0193d5308dab605f345385da6c69bb7c17201c50
Guy Sopher <guyso@wix.com>
2017-09-25 17:22:46 +0300

    add exports to index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-import Layouter from './layouter';
-import {testImages} from './images-mock.js';
-
-const items = testImages;
-
-const styleParams = {
-  isVertical: false,
-  gallerySize: 200,
-  groupSize: 3,
-  groupTypes: '1,2h,2v,3t,3b,3l,3r,3v,3h',
-  cubeImages: false,
-  collageAmount: 0.3,
-  collageDensity: 0.3,
-  minItemSize: 20,
-  layoutsVersion: 2,
-  galleryMargin: 0,
-  imageMargin: 0
+module.exports = {
+  Layouter: require('./layouter'),
+  Item: require('.item')
 };
-
-const container = {
-  galleryWidth: 1000,
-  bounds: {
-    visibleTop: 0,
-    visibleBottom: 1000,
-    renderedTop: 0,
-    renderedBottom: 10000
-  }
-};
-
-const layout = new Layouter({items, container, styleParams});
-
-console.log(layout);
