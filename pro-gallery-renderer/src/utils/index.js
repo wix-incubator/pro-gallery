b36fee75f73c77534b72803852ba7b44afe2d3b7
Guy Sopher <guyso@wix.com>
2017-11-06 13:54:26 +0200

    fix (utils): remove useless constructor

--- pro-gallery-renderer/src/utils/index.js
+++ pro-gallery-renderer/src/utils/index.js
@@ -1,10 +1,7 @@
 import {RenderUtils} from 'photography-client-lib';
 
 class Utils extends RenderUtils {
-  constructor() {
-    super();
-    this.from = 'renderUtils';
-  }
+
 }
 
 export default new Utils();
