bdc4cf7f46efc2b20cce96331815d1e6981634dc
Guy Sopher <guyso@wix.com>
2017-11-07 17:20:10 +0200

    add font icons

--- pro-gallery-renderer/src/index.js
+++ pro-gallery-renderer/src/index.js
@@ -1,5 +1,6 @@
 // import './assets/styles/consts.scss';
-// import './assets/styles/global.scss';
+import './assets/styles/global.scss';
+import './assets/styles/_progallery-svg-font-icons.scss';
 import './assets/styles/gallery.scss';
 
 import ProGallery from './components/gallery/proGallery';
