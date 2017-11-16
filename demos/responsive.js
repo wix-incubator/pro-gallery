65b01b1a7eb82a154e9872c580d8492bb1397b93
Guy Sopher <guysopher@gmail.com>
2017-11-16 11:34:16 +0200

    Update responsive.js

--- demos/responsive.js
+++ demos/responsive.js
@@ -35,7 +35,7 @@ document.body.style.padding = 0;
 const pause = document.createElement('button');
 pause.id = 'pause-button';
 pause.onclick = togglePause;
-pause.innerHTML = 'PAUSE LAYOUTER';
+pause.innerHTML = 'PAUSE INFINITE SCROLL';
 pause.style.position = 'fixed';
 pause.style.zIndex = 999;
 pause.style.top = '10px';
@@ -58,30 +58,15 @@ document.body.prepend(warning);
 
 function togglePause() {
   window.paused = !window.paused;
-  pause.innerHTML = window.paused ? 'RESUME LAYOUTER' : 'PAUSE LAYOUTER';
+  pause.innerHTML = window.paused ? 'RESUME INFINITE SCROLL' : 'PAUSE INFINITE SCROLL';
 }
 
 let windowHeight = 0;
 let windowWidth = 0;
 let windowScroll = 0;
+let layout;
 
-function placeItems() {
-
-  if (window.paused === true) {
-    return;
-  }
-
-  if (
-    Math.abs(windowHeight - window.innerHeight) < 100 &&
-    Math.abs(windowWidth - window.innerWidth) < 100 &&
-    Math.abs(windowScroll - window.scrollY) < 250
-  ) {
-    console.log('Not re-rendering, change is too small');
-    return;
-  }
-
-  console.time('Placing Items');
-
+function calcContainer() {
   windowHeight = window.innerHeight;
   windowWidth = window.innerWidth;
   windowScroll = window.scrollY;
@@ -100,79 +85,80 @@ function placeItems() {
     }
   };
 
+  return container;
+}
+
+function setItemsVisibility() {
+  if (window.paused === true) {
+    return;
+  }
+
+  if (
+    Math.abs(windowScroll - window.scrollY) < 250
+  ) {
+    console.log('Not re-rendering, change is too small');
+    return;
+  }
+
+  if (layout) {
+    layout.calcVisibilities(calcContainer().bounds);
+    createDom();
+  } else {
+    placeItems();
+  }
+
+}
+
+function placeItems() {
+
+  if (window.paused === true) {
+    return;
+  }
+
+  if (
+    Math.abs(windowHeight - window.innerHeight) < 100 &&
+    Math.abs(windowWidth - window.innerWidth) < 100
+  ) {
+    console.log('Not re-rendering, change is too small');
+    return;
+  }
+
   const layoutParams = {
     items,
     styleParams,
-    container
+    container: calcContainer()
   };
 
-  const layout = new Layouter(layoutParams);
-  let totalVisibleItems = 0;
-  let totalRenderedItems = 0;
+  layout = new Layouter(layoutParams);
 
-  parent.innerHTML = '';
-  for (let column, c = 0; column = layout.columns[c]; c++) {
-    const cDom = document.createElement('div');
-    cDom.id = 'column';
-    cDom.style.width = layout.colWidth + 'px';
-    cDom.style.float = 'left';
-    cDom.style.position = 'relative';
-    parent.append(cDom);
-
-    let isFirstRenderedGroup = true;
-    for (let group, g = 0; group = column[g]; g++) {
-
-      if (group.rendered) {
-        totalRenderedItems += group.items.length;
-
-        const gDom = document.createElement('div');
-        gDom.style.width = group.width + 'px';
-        gDom.style.height = group.height + 'px';
-        gDom.style.display = 'inline-block';
-        gDom.style.position = 'relative';
-        gDom.style.float = 'left';
-
-        if (isFirstRenderedGroup) {
-          gDom.style.marginTop = group.top + 'px';
-          isFirstRenderedGroup = false;
-        }
-
-        cDom.append(gDom);
-
-        if (!group.visible) {
-          const iDom = document.createElement('div');
-          iDom.style.width = (group.width - styleParams.itemSpacing * 2) + 'px';
-          iDom.style.height = (group.height - styleParams.itemSpacing * 2) + 'px';
-          iDom.style.position = 'absolute';
-          iDom.style.top = styleParams.itemSpacing + 'px';
-          iDom.style.left = styleParams.itemSpacing + 'px';
-          iDom.style.border = '2px solid #eee';
-          gDom.append(iDom);
-
-        } else {
-          totalVisibleItems += group.items.length;
-          for (let item, i = 0; item = group.items[i]; i++) {
-            const iDom = document.createElement('div');
-            iDom.style.width = item.width + 'px';
-            iDom.style.height = item.height + 'px';
-            iDom.style.margin = styleParams.itemSpacing + 'px';
-            iDom.style.position = item.style.position || 'absolute';
-            iDom.style.top = item.style.top + 'px';
-            iDom.style.left = item.style.left + 'px';
-            iDom.style.right = item.style.right + 'px';
-            iDom.style.bottom = item.style.bottom + 'px';
-            iDom.style.float = 'left';
-            iDom.style.backgroundColor = items[item.idx].bgColor;
-            gDom.append(iDom);
-          }
-        }
-      }
-    }
-  }
-  console.timeEnd('Placing Items');
-  console.log('Placed ' + totalRenderedItems + ' Rendered Items and ' + totalVisibleItems + ' Visible Items');
+  createDom();
 }
 
-window.addEventListener('scroll', placeItems);
+function createDom() {
+  console.time('Creating Dom');
+
+  parent.innerHTML = '';
+  for (let i = 0; i < layout.items.length; i++) {
+    const item = layout.items[i].scheme;
+    if (item.visibility.visible || item.visibility.rendered) {
+      const iDom = document.createElement('div');
+      iDom.innerHTML = i;
+      iDom.style.width = item.width + 'px';
+      iDom.style.height = item.height + 'px';
+      iDom.style.position = 'absolute';
+      iDom.style.top = item.offset.top + 'px';
+      iDom.style.left = item.offset.left + 'px';
+      if (item.visibility.visible) {
+        iDom.style.backgroundColor = items[i].bgColor;
+      } else {
+        iDom.style.backgroundColor = '#ccc';
+      }
+      parent.prepend(iDom);
+    }
+  }
+  console.timeEnd('Creating Dom');
+}
+
+window.addEventListener('scroll', setItemsVisibility);
 window.addEventListener('resize', placeItems);
 placeItems();
