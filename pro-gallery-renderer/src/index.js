ff64f8b770a3fbdd6c55e5c50371d76a16edab88
Guy Sopher <guyso@wix.com>
2017-11-27 15:57:19 +0200

    fix (index): remove GallleryDriver from index

--- pro-gallery-renderer/src/index.js
+++ pro-gallery-renderer/src/index.js
@@ -7,12 +7,10 @@ import ProGallery from './components/gallery/proGallery';
 import LoveButton from './components/item/loveButton/loveButton';
 import GalleryItem from './components/item/galleryItem';
 import {CommonItemContainer} from './components/item/commonItemContainer';
-import GalleryDriver from '../test/galleryDriver';
 
 export {
     ProGallery,
     LoveButton,
     GalleryItem,
-    CommonItemContainer,
-    GalleryDriver
+    CommonItemContainer
 };
