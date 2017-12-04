3870e6b3eb8bc8b0b9d0eda3dd9bb4fd04432dd3
Guy Sopher <guyso@wix.com>
2017-12-04 16:27:01 +0200

    fix (galleryContainer): typo (const instead of let)

--- pro-gallery-renderer/src/components/gallery/galleryContainer.js
+++ pro-gallery-renderer/src/components/gallery/galleryContainer.js
@@ -596,7 +596,7 @@ export class GalleryContainer extends React.Component {
     //new layouts
     const {gallerySize, magicLayoutSeed} = wixStyles;
 
-    const galleryLayout = selectedLayout || wixStyles.galleryLayout;
+    let galleryLayout = selectedLayout || wixStyles.galleryLayout;
 
     const emptyLayout = {
       galleryType: undefined,
