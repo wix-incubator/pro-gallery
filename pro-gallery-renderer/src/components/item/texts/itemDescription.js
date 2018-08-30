ba381b206c06e6e7ae2ed3a8a54a491e70df1a69
aviranhuga <aviranh@wix.com>
2018-08-30 14:30:38 +0300

    [pgr] feat (itemDescription.js): Text line breaks logic

--- pro-gallery-renderer/src/components/item/texts/itemDescription.js
+++ pro-gallery-renderer/src/components/item/texts/itemDescription.js
@@ -5,7 +5,11 @@ export default class ItemDescription extends React.Component {
   render() {
     const {description} = this.props;
     return (
-      <div className={'gallery-item-description'} data-hook="item-description">{description}</div>
+      <div className={'gallery-item-description'} data-hook="item-description">{
+        description.split('\n').map((i, key) => {
+          return <span key={key}>{i}<br></br></span>;
+        })
+      }</div>
     );
   }
 }
