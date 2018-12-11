4ce1f4d7d3520d60edffa67d2d051195ccf4604b
Guy Sopher <guyso@wix.com>
2018-12-11 11:20:33 +0200

    [pgif] fix (video): fix video posterUrl

--- pro-gallery-renderer/src/components/helpers/cssLayoutsHelper.js
+++ pro-gallery-renderer/src/components/helpers/cssLayoutsHelper.js
@@ -1,7 +1,7 @@
 import {createLayout} from 'pro-gallery-layouter';
 import {cssScrollHelper} from '../helpers/cssScrollHelper.js';
 
-const CDN_URL = 'https://static.wixstatic.com/media/';
+// const CDN_URL = 'https://static.wixstatic.com/media/';
 const desktopWidths = [1024, 1280, 1360, 1440, 1680, 1920];
 const mobileWidths = [320, 480, 600, 720, 800, 900];
 const popularWidths = [...mobileWidths, ...desktopWidths];
@@ -11,28 +11,29 @@ const popularWidths = [...mobileWidths, ...desktopWidths];
     - check run time and minimize layouts width
     - remove !important if possible
     - fix default width of gallery (currently 990???)
+    - check if is mobile and select the widths accordinly
 */
 
-const getImgSize = (item, dimension) => {
-  const scale = 1;
-  return Math.ceil(Math.min(
-      scale * item[dimension],
-      item[`max${dimension[0].toUpperCase() + dimension.substring(1)}`]
-    ));
-};
+// const getImgSize = (item, dimension) => {
+//   const scale = 1;
+//   return Math.ceil(Math.min(
+//       scale * item[dimension],
+//       item[`max${dimension[0].toUpperCase() + dimension.substring(1)}`]
+//     ));
+// };
 
-const getImageSrc = item => {
-  const h = getImgSize(item, 'height');
-  const w = getImgSize(item, 'width');
-  return `${CDN_URL}${item.dto.url}/v1/fit/w_${w},h_${h},al_c,q_80/file.jpg`;
-};
+// const getImageSrc = item => {
+//   const h = getImgSize(item, 'height');
+//   const w = getImgSize(item, 'width');
+//   return `${CDN_URL}${item.dto.url}/v1/fit/w_${w},h_${h},al_c,q_80/file.jpg`;
+// };
 
 const getImageStyle = item => ({
   top: item.offset.top,
   left: item.offset.left,
   width: item.width,
   height: item.height,
-  backgroundImage: `url(${getImageSrc(item)})`
+  // backgroundImage: `url(${getImageSrc(item)})`
 });
 
 const createCssFromLayouts = (layouts, styleParams) => {
