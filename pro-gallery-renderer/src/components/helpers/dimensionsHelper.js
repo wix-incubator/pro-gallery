8fba090987e3fc2a50708a2c93651f9a24dfc215
Guy Sopher <guyso@wix.com>
2018-12-16 13:15:17 +0200

    [pgr] fix (dimensionHelper): use screen width / height as default width

--- pro-gallery-renderer/src/components/helpers/dimensionsHelper.js
+++ pro-gallery-renderer/src/components/helpers/dimensionsHelper.js
@@ -56,7 +56,7 @@ class DimensionsHelper {
 
   getGalleryWidth() {
     return this.getOrPutInCache('galleryWidth', () => {
-      const domWidth = 980;//() => protectGalleryWidth(utils.isMobile() ? container.documentWidth : container.windowWidth, container); //on mobile we use the document width - which takes in account the pixel ratio fix (width more that 100% and scale down)
+      const domWidth = utils.getScreenWidth();//() => protectGalleryWidth(utils.isMobile() ? container.documentWidth : container.windowWidth, container); //on mobile we use the document width - which takes in account the pixel ratio fix (width more that 100% and scale down)
       return Math.floor((this.container.width > 0 ? this.container.width : domWidth) + this.getDimensionFix() * 2); //add margins to width and then remove them in css negative margins
     });
   }
@@ -65,7 +65,7 @@ class DimensionsHelper {
   getGalleryHeight() {
     return this.getOrPutInCache('galleryHeight', () => {
       //const offsetTop = this.styles.oneRow ? this.container.offsetTop : 0;
-      const domHeight = 500;//() => protectGalleryHeight(this.container.windowHeight, offsetTop);
+      const domHeight = utils.getScreenHeight();//() => protectGalleryHeight(this.container.windowHeight, offsetTop);
       return Math.floor((this.container.height > 0 ? this.container.height : domHeight) + this.getDimensionFix());
     });
   }
