d322401b1ba76b778330b6dadbc3b91fd972edea
aviranhuga <aviranh@wix.com>
2019-05-01 11:21:34 +0300

    [pgr] item Discription and title add if for style undefined

--- pro-gallery-renderer/src/components/item/texts/itemDescription.js
+++ pro-gallery-renderer/src/components/item/texts/itemDescription.js
@@ -4,7 +4,10 @@ export default class ItemDescription extends React.Component {
 
   render() {
     const {description, style, spanStyle} = this.props;
-    const divStyle = Object.assign(style, {'-webkit-box-orient': 'vertical'});
+    const divStyle = {'-webkit-box-orient': 'vertical'};
+    if (typeof style !== 'undefined') {
+      Object.assign(divStyle, style);
+    }
     return (
       <div className={'gallery-item-description'} data-hook="item-description" style={divStyle}>{
         description.split('\n').map((i, key) => {
--- pro-gallery-renderer/src/components/item/texts/itemTitle.js
+++ pro-gallery-renderer/src/components/item/texts/itemTitle.js
@@ -4,7 +4,10 @@ export default class ItemTitle extends React.Component {
 
   render() {
     const {title, style, spanStyle} = this.props;
-    const divStyle = Object.assign(style, {'-webkit-box-orient': 'vertical'});
+    const divStyle = {'-webkit-box-orient': 'vertical'};
+    if (typeof style !== 'undefined') {
+      Object.assign(divStyle, style);
+    }
     return (
       <div className={`gallery-item-title`} data-hook="item-title" style={divStyle}>
         <span style={spanStyle}>{title}</span>
