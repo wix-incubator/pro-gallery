399f6e9b9170f38ff2cf214c93c8e3007bf9fa38
liatVich <liatvi@wix.com>
2019-07-14 14:34:59 +0300

    Revert "[Pgr]fix(cssScrollHelper): try to fix build - eyes loading"
    This reverts commit aff618a85aaddd4535051f443b3cf450861d65b5.

--- pro-gallery-renderer/src/components/helpers/cssScrollHelper.js
+++ pro-gallery-renderer/src/components/helpers/cssScrollHelper.js
@@ -41,10 +41,8 @@ class CssScrollHelper {
     this.justBelowAndInScreenPadding = () => [5120, 0];
     this.belowScreenPadding = () => [Infinity, 0];
 
-    this.highResPadding = () =>
-      allowPreloading ? [Infinity, Infinity] : [0, 0];
-    this.lowResPadding = () =>
-      allowPreloading ? [Infinity, Infinity] : [0, 0];
+    this.highResPadding = () => (allowPreloading ? [5120, Infinity] : [0, 0]);
+    this.lowResPadding = () => (allowPreloading ? [10240, Infinity] : [0, 0]);
   }
 
   getDomId({ id }) {
@@ -182,7 +180,9 @@ class CssScrollHelper {
       if (!window.isSSR && !item.isDimensionless) {
         scrollCss +=
           createScrollSelectors(this.highResPadding(), `.image-item>canvas`) +
-          `{opacity: 1; transition: opacity 1s linear; background-image: url(${resized_url.img})}`;
+          `{opacity: 1; transition: opacity 1s linear; background-image: url(${
+            resized_url.img
+          })}`;
       }
 
       //add the blurry image
