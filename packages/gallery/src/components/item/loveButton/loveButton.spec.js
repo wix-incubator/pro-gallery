5414199ba6c30ff92ebffafac207b251c6790d01
ritabazak <rita.bazak@gmail.com>
2019-09-15 11:46:33 +0300

    fixed love button tests.

--- packages/gallery/src/components/item/loveButton/loveButton.spec.js
+++ packages/gallery/src/components/item/loveButton/loveButton.spec.js
@@ -24,26 +24,43 @@ describe('Love Button', () => {
     });
   });
 
-  it('should toggle love', () => {
-    driver.mount(LoveButton, sampleItemViewProps);
-    expect(driver.get.state().loveButtonToggledToLove).to.equal(undefined);
-    driver.find.hook('love-icon').simulate('click');
-    expect(driver.get.state().loveButtonToggledToLove).to.equal(true);
-  });
-
-  it('should increase love count when clicked', () => {
-    driver.mount(LoveButton, sampleItemViewProps);
-    expect(driver.get.state().loveButtonToggledToLove).to.equal(undefined);
-    driver.find.hook('love-icon').simulate('click');
-    expect(driver.get.state().loveButtonToggledToLove).to.equal(true);
-    expect(driver.find.hook('love-counter').length).to.equal(1);
-    expect(driver.find.hook('love-counter').text()).to.equal('1');
-  });
   it('check eventsListener called on toggle', () => {
     const stub = sinon.stub(sampleItemViewProps.actions, 'eventsListener');
     driver.mount(LoveButton, sampleItemViewProps);
     driver.find.hook('love-icon').simulate('click');
     expect(stub.calledWith(EVENTS.LOVE_BUTTON_CLICKED)).to.be.true;
     stub.restore();
+  });
+  it('should show loved when isLoved', () => {
+    Object.assign(sampleItemViewProps, {
+      isLoved: true,
+      loveCount: 1,
+    });
+    driver.mount(LoveButton, sampleItemViewProps);
+    expect(driver.find.hook('love-counter').length).to.equal(1);
+    expect(driver.find.hook('love-counter').text()).to.equal('1');
+    expect(driver.find.class('progallery-svg-font-icons-love_full').length).to.equal(1);
+    expect(driver.find.class('pro-gallery-loved').length).to.equal(1);
+    expect(driver.find.class('progallery-svg-font-icons-love_empty').length).to.equal(0);
+  });
+
+  it('should show not loved when not isLoved', () => {
+    Object.assign(sampleItemViewProps, {
+      isLoved: false,
+    });
+    driver.mount(LoveButton, sampleItemViewProps);
+    expect(driver.find.class('progallery-svg-font-icons-love_empty').length).to.equal(1);
+    expect(driver.find.class('progallery-svg-font-icons-love_full').length).to.equal(0);
+    expect(driver.find.class('pro-gallery-loved').length).to.equal(0);
+  });
+
+  it('should show correct live count', () => {
+    Object.assign(sampleItemViewProps, {
+      isLoved: false,
+      loveCount: 10,
+    });
+    driver.mount(LoveButton, sampleItemViewProps);
+    expect(driver.find.hook('love-counter').length).to.equal(1);
+    expect(driver.find.hook('love-counter').text()).to.equal('10');
   });
 });
