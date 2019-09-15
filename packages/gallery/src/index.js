6143b7620502957a1884a7a50e0eb5eb364c8469
Guy Sopher <guyso@wix.com>
2019-09-15 17:59:34 +0300

    re-add styles

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -1,6 +1,6 @@
-// import './components/styles/global.scss';
-// import './assets/styles/progallery-svg-font-icons.scss';
-// import './components/styles/gallery.scss';
+import './components/styles/global.scss';
+import './assets/styles/progallery-svg-font-icons.scss';
+import './components/styles/gallery.scss';
 
 import ProGallery from './components/gallery/proGallery';
 import LoveButton from './components/item/loveButton/loveButton';
