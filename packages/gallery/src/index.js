c1a6e25190f97723f338857e91e9075a81ed1aa5
Guy Sopher <guyso@wix.com>
2019-10-15 13:23:43 +0300

    [gallery] improve: remove css from main index.js

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -1,5 +1,3 @@
-import './components/styles/gallery.scss';
-
 import ProGallery from './components/gallery/proGallery';
 import LoveButton from './components/item/loveButton/loveButton';
 import GalleryItem from './components/item/galleryItem';
