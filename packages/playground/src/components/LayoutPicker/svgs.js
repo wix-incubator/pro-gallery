00ca4230c6784d7113c83ae07aca3825bb37c4f2
Guy Sopher <guyso@wix.com>
2019-10-23 22:06:58 +0300

    [playground] improve: change preset selector ui

--- packages/playground/src/components/LayoutPicker/svgs.js
+++ packages/playground/src/components/LayoutPicker/svgs.js
@@ -8,20 +8,20 @@ import {ReactComponent as Thumbnails} from '../../assets/layouts/thumbnails-reg.
 import {ReactComponent as ThumbnailsSel} from '../../assets/layouts/thumbnails-sel.svg';
 import {ReactComponent as Slider} from '../../assets/layouts/slider-reg.svg';
 import {ReactComponent as SliderSel} from '../../assets/layouts/slider-sel.svg';
-import {ReactComponent as Pro} from '../../assets/layouts/mix-reg.svg';
-import {ReactComponent as ProSel} from '../../assets/layouts/mix-sel.svg';
+import {ReactComponent as Pro} from '../../assets/layouts/magic-reg.svg';
+import {ReactComponent as ProSel} from '../../assets/layouts/magic-sel.svg';
 import {ReactComponent as Slideshow} from '../../assets/layouts/slideshow-reg.svg';
 import {ReactComponent as SlideshowSel} from '../../assets/layouts/slideshow-sel.svg'
 import {ReactComponent as Panorama} from '../../assets/layouts/panorama-reg.svg';
 import {ReactComponent as PanoramaSel} from '../../assets/layouts/panorama-sel.svg';
 import {ReactComponent as Column} from '../../assets/layouts/columns-reg.svg';
 import {ReactComponent as ColumnSel} from '../../assets/layouts/columns-sel.svg';
-import {ReactComponent as Magic} from '../../assets/layouts/magic-reg.svg';
-import {ReactComponent as MagicSel} from '../../assets/layouts/magic-sel.svg';
-import {ReactComponent as Bricks} from '../../assets/layouts/bricks-reg.svg';
-import {ReactComponent as BricksSel} from '../../assets/layouts/bricks-sel.svg';
-import {ReactComponent as Alternate} from '../../assets/layouts/alternate-reg.svg';
-import {ReactComponent as AlternateSel} from '../../assets/layouts/alternate-sel.svg';
+// import {ReactComponent as Magic} from '../../assets/layouts/magic-reg.svg';
+// import {ReactComponent as MagicSel} from '../../assets/layouts/magic-sel.svg';
+// import {ReactComponent as Bricks} from '../../assets/layouts/bricks-reg.svg';
+// import {ReactComponent as BricksSel} from '../../assets/layouts/bricks-sel.svg';
+// import {ReactComponent as Alternate} from '../../assets/layouts/alternate-reg.svg';
+// import {ReactComponent as AlternateSel} from '../../assets/layouts/alternate-sel.svg';
 
 export const svgs = {
   collage: {
@@ -64,24 +64,9 @@ export const svgs = {
     sel: ColumnSel,
     key: 7,
   },
-  magic: {
-    reg: Magic,
-    sel: MagicSel,
-    key: 8,
-  },
-  bricks: {
-    reg: Bricks,
-    sel: BricksSel,
-    key: 10,
-  },
-  alternate: {
-    reg: Alternate,
-    sel: AlternateSel,
-    key: 11,
-  },
   pro: { //--mix
     reg: Pro,
     sel: ProSel,
-    key: 12,
+    key: -1,
   },
 };
