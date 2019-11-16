bd03949c02877e70d011a7c26782d704f51559e4
izaacayelin <izaacaa@wix.com>
2019-11-17 01:38:42 +0200

    [tests] allowTitle it() tests

--- packages/gallery/tests/styleParams/allowTitle.spec.js
+++ packages/gallery/tests/styleParams/allowTitle.spec.js
@@ -15,15 +15,36 @@ describe('styleParam - allowTitle', () => {
     beforeEach(() => {
         driver = new GalleryDriver();
     });
+    
 
     it('should render "item-title" when "allowTitle" is "true" and item has a title', () => {
-
+        Object.assign(initialProps.styles, {
+            allowTitle: true,
+        })
+        driver.mount.proGallery(initialProps)
+        const item = driver.find.hook('item-title').at(0);
+        // first item has a title
+        expect(item).to.have.lengthOf(1);
     });
+
     it('should not render "item-title" when "allowTitle" is "true" and item does not have a title', () => {
-
-    });
-    it('should not render "item-title when "allowTitle is "false"', () => {
+        Object.assign(initialProps.styles, {
+            allowTitle: true,
+        })
+        driver.mount.proGallery(initialProps)
+        const item = driver.find.hook('item-title').at(3);
+        // last item does not have a title
+        expect(item).to.have.lengthOf(0);
         
+    });
+
+    it('should not render "item-title when "allowTitle is "false"', () => {
+        Object.assign(initialProps.styles, {
+            allowTitle: false,
+        })
+        driver.mount.proGallery(initialProps);
+        const items = driver.find.hook('item-title');
+        expect(items).to.have.lengthOf(0);
     });
 
 })
