2d7971bea2d93bc022f4892a2fe16799a5ee5f29
izaacayelin <izaacaa@wix.com>
2019-11-18 11:18:02 +0200

    [tests,contents] remoed unused imports

--- packages/gallery/contents/styleParams/allowTitle.js
+++ packages/gallery/contents/styleParams/allowTitle.js
@@ -1,11 +1,11 @@
-import { GALLERY_CONSTS, INPUT_TYPES } from './constants';
+import { INPUT_TYPES } from './constants';
 import { createOptions, always } from './utils';
 
 export default {
   title: 'Allow title',
-  description: 'Choose weather you want to show title or not',
+  description: 'Choose whether you want to show title or not',
   isRelevant: always,
   type: INPUT_TYPES.BOOLEAN,
-  default: false,
+  default: true,
   options: createOptions('allowTitle')
 }
--- packages/gallery/tests/styleParams/allowTitle.spec.js
+++ packages/gallery/tests/styleParams/allowTitle.spec.js
@@ -1,6 +1,6 @@
 import GalleryDriver from '../drivers/reactDriver'
 import { expect } from 'chai';
-import { images2, videoItems } from '../drivers/mocks/items';
+import { images2 } from '../drivers/mocks/items';
 import { styleParams, container } from '../drivers/mocks/styles'
 
 describe('styleParam - allowTitle', () => {
