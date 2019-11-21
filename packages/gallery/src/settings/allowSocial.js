3a0ef3eff67f6068f77e8b4d3f4549b3e1e5496f
izaacayelin <izaacaa@wix.com>
2019-11-21 15:53:25 +0200

    [gallery] fix (allowDownload, allowSocial): fixed settings options

--- packages/gallery/src/settings/allowSocial.js
+++ packages/gallery/src/settings/allowSocial.js
@@ -1,5 +1,5 @@
 import {  INPUT_TYPES } from './utils/constants';
-import { createOptions, always } from './utils/utils';
+import { always } from './utils/utils';
 
 export default {
   title: 'Toggle Social',
@@ -7,5 +7,4 @@ export default {
   isRelevant: always,
   type: INPUT_TYPES.BOOLEAN,
   default: false,
-  options: createOptions('allowSocial')
 }
