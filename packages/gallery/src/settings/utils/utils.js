9de53445fcf0c52d71c97011f99258afcb3e8c8a
izaacayelin <izaaca@wix.com>
2019-11-24 14:42:33 +0200

    [gallery] settings (imageMargin): added  isRelevant()

--- packages/gallery/src/settings/utils/utils.js
+++ packages/gallery/src/settings/utils/utils.js
@@ -3,3 +3,4 @@ import GALLERY_CONSTS from '../../common/constants';
 export const formatTitle = title => title.replace(/_/g, ' ').split(' ').map(word => word[0].toUpperCase() + word.substr(1).toLowerCase()).join(' ');
 export const createOptions = constName => Object.entries(GALLERY_CONSTS[constName]).map(([title, value]) => ({ value, title: formatTitle(title) }));
 export const always = () => true;
+export const imageMargin = (styleParams) => styleParams.galleryLayout !== 3 && styleParams.galleryLayout !== 5;
