cb0c06feb54ff1d966c9fe2fdc67b99ce1501244
izaacayelin <izaacaa@wix.com>
2019-12-03 11:09:23 +0200

    [gallery] tests (allowTitle e2e) removed 2e2 tests

--- packages/gallery/tests/e2e/styleParams/allowTitle.e2e.spec.js
+++ packages/gallery/tests/e2e/styleParams/allowTitle.e2e.spec.js
@@ -16,27 +16,30 @@ describe('allowTitle - e2e', () => {
   afterEach(() => {
     driver.closeBrowser();
   });
-  it('should render when "allowTitle" is "true"', async () => {
-    await driver.openPage({
-      galleryLayout: 2,
-      allowTitle: true
-    });
-    await driver.waitFor.hookToBeVisible('item-container');
-    await driver.actions.hover('item-container')[0]
-    await driver.waitFor.timer(2000);
-    const page = await driver.grab.screenshot();
-    expect(page).toMatchImageSnapshot();
-  });
-  it('should not render when "allowTitle" is "false"', async () => {
-    await driver.openPage({
-      galleryLayout: 2,
-      allowTitle: false
-    });
-    await driver.waitFor.hookToBeVisible('item-container');
-    await driver.actions.hover('item-container')[0]
-    await driver.waitFor.timer(2000);
-    const page = await driver.grab.screenshot();
-    expect(page).toMatchImageSnapshot();
-  });
+  it('Should render title', () => {
+    
+  })
+  // it('should render when "allowTitle" is "true"', async () => {
+  //   await driver.openPage({
+  //     galleryLayout: 2,
+  //     allowTitle: true
+  //   });
+  //   await driver.waitFor.hookToBeVisible('item-container');
+  //   await driver.actions.hover('item-container')[0]
+  //   await driver.waitFor.timer(2000);
+  //   const page = await driver.grab.screenshot();
+  //   expect(page).toMatchImageSnapshot();
+  // });
+  // it('should not render when "allowTitle" is "false"', async () => {
+  //   await driver.openPage({
+  //     galleryLayout: 2,
+  //     allowTitle: false
+  //   });
+  //   await driver.waitFor.hookToBeVisible('item-container');
+  //   await driver.actions.hover('item-container')[0]
+  //   await driver.waitFor.timer(2000);
+  //   const page = await driver.grab.screenshot();
+  //   expect(page).toMatchImageSnapshot();
+  // });
 
 })
