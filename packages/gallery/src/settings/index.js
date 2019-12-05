d488ab40aa6cc2d49cb721c728e0c916ec7c39d6
izaacayelin <izaacaa@wix.com>
2019-12-05 14:26:21 +0200

    [gallery] settings: edited the contents

--- packages/gallery/src/settings/index.js
+++ packages/gallery/src/settings/index.js
@@ -1,9 +1,17 @@
 import itemClick from './itemClick';
 import allowTitle from './allowTitle';
+import allowSocial from './allowSocial';
 import allowDescription from './allowDescription';
+import allowDownload from './allowDownload';
+import loveButton from './loveButton';
+import imageMargin from './imageMargin';
 
 export default {
     itemClick,
     allowTitle,
     allowDescription,
+    loveButton,
+    allowSocial,
+    allowDownload,
+    imageMargin
 }
