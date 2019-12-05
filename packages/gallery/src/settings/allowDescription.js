03d132d050e844af6576cabaf904838f32298c23
izaacayelin <izaacaa@wix.com>
2019-12-05 16:17:28 +0200

    [gallery] settings: contents

--- packages/gallery/src/settings/allowDescription.js
+++ packages/gallery/src/settings/allowDescription.js
@@ -3,8 +3,8 @@ import { always } from './utils/utils';
 
 export default {
   title: 'Show Description',
-  description: `Allow a description to appear with each item. This option can be set in all layouts.
-  Note that the placement of the texts (title and description) can also be changed in some layouts that have the option - "titlePlacement".` ,
+  description: `Allow a description to appear with each item.
+  Note that the placement of the texts (title and description) can also be changed with the option - "titlePlacement".` ,
   isRelevant: always,
   type: INPUT_TYPES.BOOLEAN,
   default: false,
