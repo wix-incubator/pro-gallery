92390008397430f8c6eaae2048f4cef823fe3e9e
izaacayelin <izaacaa@wix.com>
2019-12-05 14:33:33 +0200

    [gallery] settings: fixed typo

--- packages/gallery/src/settings/allowTitle.js
+++ packages/gallery/src/settings/allowTitle.js
@@ -3,7 +3,7 @@ import { always } from './utils/utils';
 
 export default {
   title: 'Show Title',
-  description: `Show the title in the item info. This style param can be set in all layouts.
+  description: `Show the title in the item info section. This style param can be set in all layouts.
   Notice that the placement of the texts(title and description) can be set too(see Style Param: "titlePlacement").`,
   isRelevant: always,
   type: INPUT_TYPES.BOOLEAN,
