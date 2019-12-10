a38107bf7629c3bc37b6a41594c01876ee63d7cb
izaacayelin <izaacaa@wix.com>
2019-12-11 00:12:05 +0200

    [gallery] tests(hoveringBehaviour): added e2e tests

--- packages/gallery/src/settings/hoveringBehaviour.js
+++ packages/gallery/src/settings/hoveringBehaviour.js
@@ -7,8 +7,6 @@ export default {
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.infoBehaviourOnHover.APPEARS,
   options: createOptions('hoveringBehaviour'),
-  description: `Determines whether the hover container appear or disappear when hovering over items. Note that
-  this option is not the only one that deals with the hover event. When "titlePlacement" option is anything but "SHOW_ON_HOVER",
-  it will take priority over "hoveringBehaviour".
+  description: `Determines whether the hover container appear or disappear when hovering over items.
   `,
 }
---
+++ packages/gallery/tests/e2e/styleParams/hoveringBehaviour.e2e.spec.js
@@ -0,0 +1,56 @@
+import GalleryDriver from '../../drivers/pptrDriver';
+import { toMatchImageSnapshot} from 'jest-image-snapshot';
+import { GALLERY_CONSTS } from '../../../src/settings/utils/constants';
+
+jest.setTimeout(30000)
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('hoveringBehaviour - e2e', () => {
+  let driver;
+  
+  beforeEach(async () => {
+    driver = new GalleryDriver();
+    await driver.launchBrowser();
+  });
+
+  afterEach(() => {
+    driver.closeBrowser();
+  });
+  it('should show hover container on hover event when "hoveringBehaviour" is "APPEAR', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      allowTitle: true,
+      hoveringBehaviour: GALLERY_CONSTS.infoBehaviourOnHover.APPEARS
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.actions.hover('item-container')[0]
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('#pro-gallery-container');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should make hover state disapear on hover (reverse-hover) when "hoveringBehaviour" is "DISAPPEARS', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      allowTitle: true,
+      hoveringBehaviour: GALLERY_CONSTS.infoBehaviourOnHover.DISAPPEARS
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.actions.hover('item-container')[0]
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('#pro-gallery-container');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should set all items in constant hover state when "hoveringBehaviour" is "NO_CHANGE"', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      allowTitle: true,
+      hoveringBehaviour: GALLERY_CONSTS.infoBehaviourOnHover.NO_CHANGE
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.actions.hover('item-container')[0]
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('#pro-gallery-container');
+    expect(page).toMatchImageSnapshot();
+  });
+})
