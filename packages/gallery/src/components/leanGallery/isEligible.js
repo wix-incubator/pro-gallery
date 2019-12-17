2d63690cd31f43d338dddfff87e07ec661d0408e
Guy Sopher <guyso@wix.com>
2019-12-17 15:27:44 +0200

    add example

--- packages/gallery/src/components/leanGallery/isEligible.js
+++ packages/gallery/src/components/leanGallery/isEligible.js
@@ -1,10 +1,12 @@
 import consts from '../../common/constants/index';
 import { addLayoutStyles } from '../helpers/layoutHelper';
 
+//example: http://pro-gallery.surge.sh/?titlePlacement=DONT_SHOW&itemClick=nothing&allowTitle=true&allowHover=false&galleryLayout=2&allowLeanGallery=true
+
 export default ({items, styles, options}) => {
 
     styles = {...styles, ...options};
-    
+
     const allowLeanGallery = !!styles.allowLeanGallery;
     
     if (!allowLeanGallery) {
