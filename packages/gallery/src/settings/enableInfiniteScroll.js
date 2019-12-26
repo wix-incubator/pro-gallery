18af9e1d179a1483d0034e5890b646e785703c2f
izaacayelin <izaacaa@wix.com>
2019-12-26 15:20:52 +0200

    [gallery] settings(enableInfiniteScroll): added to settings

---
+++ packages/gallery/src/settings/enableInfiniteScroll.js
@@ -0,0 +1,11 @@
+import { INPUT_TYPES } from './utils/constants';
+import { enableInfiniteScroll } from './utils/utils';
+
+export default {
+  title: 'Use Infinite Scroll',
+  description: `Choose Whether you want to have infinite scroll option or not. When this option is set to false the gallery will have
+  a "Load More". Note that the consumer will have to add the new items when the gallery requests more ("NEED_MORE_ITEMS" event will be emited).`,
+  isRelevant: enableInfiniteScroll,
+  type: INPUT_TYPES.BOOLEAN,
+  default: true,
+}
--- packages/gallery/src/settings/utils/utils.js
+++ packages/gallery/src/settings/utils/utils.js
@@ -7,3 +7,4 @@ export const always = () => true;
 export const imageMargin = (styleParams) => !isInPreset(styleParams,'imageMargin');
 export const hoveringBehaviour = (styleParams)  => !isInPreset(styleParams,'hoveringBehaviour')
 export const allowHover = (styleParams) => !isInPreset(styleParams,'allowHover');
+export const enableInfiniteScroll = (styleParams) => !isInPreset(styleParams,'enableInfiniteScroll') && !styleParams.oneRow;
