9bfb98a0db7aadb6af9da283a9576a9de6c2f975
izaacayelin <izaacaa@wix.com>
2020-01-01 23:41:27 +0200

    [gallery] presets

--- packages/gallery/src/components/gallery/presets/alternateGallery.js
+++ packages/gallery/src/components/gallery/presets/alternateGallery.js
@@ -5,6 +5,7 @@ import LAYOUTS from '../../../common/constants/layout';
 
 export const fixedStyles = {
   galleryLayout: LAYOUTS.ALTERNATE,
+  cubeImages: true
 }
 export default class alternateGallery extends React.Component {
 
