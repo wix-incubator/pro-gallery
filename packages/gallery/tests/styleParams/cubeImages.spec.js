53aec36d1635067de82174a1b778d3d68e4283bb
izaacayelin <izaacaa@wix.com>
2020-01-09 10:46:47 +0200

    [gallery] removed repeated tests

--- packages/gallery/tests/styleParams/cubeImages.spec.js
+++ packages/gallery/tests/styleParams/cubeImages.spec.js
@@ -25,21 +25,7 @@ describe('styleParam - cubeImages', () => {
     })
     driver.mount.proGallery(initialProps);
     const itemWrappers = driver.find.selector('.cube-type-fill');
-    console.log(itemWrappers.length);
     //expect to find items that have "cube-type-fill" class
-    expect(itemWrappers.length).to.be.greaterThan(0)
-    driver.detach.proGallery();
-  });
-  it('should be able to set "cubeType"(fit) when "cubeImages" is "true"', () => {
-    Object.assign(initialProps.styles, {
-      galleryLayout: -1,
-      cubeImages: true,
-      cubeType: CUBE_TYPE.FIT,
-    })
-    driver.mount.proGallery(initialProps);
-    const itemWrappers = driver.find.selector('.cube-type-fit');
-    console.log(itemWrappers.length);
-    //expect to find items that have "cube-type-fit" class
     expect(itemWrappers.length).to.be.greaterThan(0)
     driver.detach.proGallery();
   });
@@ -51,21 +37,7 @@ describe('styleParam - cubeImages', () => {
     })
     driver.mount.proGallery(initialProps);
     const itemWrappers = driver.find.selector('.cube-type-fill');
-    console.log(itemWrappers.length);
     //expect to not find items with "cube-type-fill" class
-    expect(itemWrappers.length).to.eq(0);
-    driver.detach.proGallery();
-  });
-  it('should not be able to set "cubeType"(fit) when "cubeImages" is "false"', () => {
-    Object.assign(initialProps.styles, {
-      galleryLayout: -1,
-      cubeImages: false,
-      cubeType: CUBE_TYPE.FIT,
-    })
-    driver.mount.proGallery(initialProps);
-    const itemWrappers = driver.find.selector('.cube-type-fit');
-    console.log(itemWrappers.length);
-    //expect to not find items with "cube-type-fit" class
     expect(itemWrappers.length).to.eq(0);
     driver.detach.proGallery();
   });
