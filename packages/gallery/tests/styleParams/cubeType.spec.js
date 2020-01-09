739c450845d71d06da815d6507358d0675070481
izaacayelin <izaacaa@wix.com>
2020-01-09 10:40:31 +0200

    [gallery] added test and textItems

--- packages/gallery/tests/styleParams/cubeType.spec.js
+++ packages/gallery/tests/styleParams/cubeType.spec.js
@@ -1,6 +1,6 @@
 import GalleryDriver from '../drivers/reactDriver'
 import { expect } from 'chai';
-import { images2 } from '../drivers/mocks/items';
+import { images2, textItems } from '../drivers/mocks/items';
 import { styleParams, container } from '../drivers/mocks/styles';
 import GALLERY_CONSTS from '../../src/common/constants';
 
@@ -48,6 +48,32 @@ describe('styleParam - cubeType', () => {
     driver.mount.proGallery(initialProps)
     const items = driver.find.selector('.grid-fit');
     expect(items.length).to.be.greaterThan(0)
+    driver.detach.proGallery();
+  });
+  it('should set "backgroundColor" to "transparent" on text items when "cubeType" is "fill"', () => {
+    initialProps.items = textItems;
+    Object.assign(initialProps.styles, {
+      galleryLayout: -1,
+      cubeImages:true,
+      cubeType: GALLERY_CONSTS.cubeType.CROP
+    })
+    driver.mount.proGallery(initialProps)
+    const textItem = driver.find.hook('item-wrapper').at(0);
+    const { backgroundColor } = textItem.props().style;
+    expect(backgroundColor).to.eq('transparent')
+    driver.detach.proGallery();
+  });
+  it('should set "backgroundColor" to "inherit" on text items when "cubeType" is "fit"', () => {
+    initialProps.items = textItems;
+    Object.assign(initialProps.styles, {
+      galleryLayout: -1,
+      cubeImages:true,
+      cubeType: GALLERY_CONSTS.cubeType.FIT
+    })
+    driver.mount.proGallery(initialProps)
+    const textItem = driver.find.hook('item-wrapper').at(0);
+    const { backgroundColor } = textItem.props().style;
+    expect(backgroundColor).to.eq('inherit');
     driver.detach.proGallery();
   });
 })
