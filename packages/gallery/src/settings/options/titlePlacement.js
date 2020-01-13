4f350caf7e291beef9122b4267dd6e0807787feb
izaacayelin <izaacaa@wix.com>
2020-01-13 17:19:02 +0200

    [gallery] added options file

---
+++ packages/gallery/src/settings/options/titlePlacement.js
@@ -0,0 +1,13 @@
+import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
+import { createOptions } from '../utils/utils';
+
+export default {
+  title: 'Texts Placement',
+  isRelevant: (styleParams) => false,
+  type: INPUT_TYPES.OPTIONS,
+  default: GALLERY_CONSTS.placements.SHOW_ON_HOVER,
+  options: createOptions('placements'),
+  description: `Choose the the placement of the texts (title and description) relative to the items in the gallery. Note that
+  this option also deals with the hover effects and may overide "hoveringBehaviour" when set to anything but "SHOW_ON_HOVER".
+  `,
+}
