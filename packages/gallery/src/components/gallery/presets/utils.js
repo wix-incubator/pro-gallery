b3c017e9ba905f47386d87ba2ff2343fea63cb8d
Guy Sopher <guyso@wix.com>
2020-01-14 14:16:45 +0200

    progress

---
+++ packages/gallery/src/components/gallery/presets/utils.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import consts from '../../../common/constants';
+
+const basePropTypes = {
+  orientaion: PropTypes.oneOf(['rows', 'cols']),
+  rtl: PropTypes.bool,
+  spacing: PropTypes.number,
+  scrollDirection: PropTypes.oneOf(['vertical', 'horizontal']),
+  itemSize: PropTypes.number,
+  info: PropTypes.oneOf(['above', 'below', 'hover', 'none']),
+  play: PropTypes.oneOf(['auto', 'hover', 'onClick']),
+  overlayAnimation: PropTypes.oneOf(consts.overlayAnimations),
+  imageHoverAnimation: PropTypes.oneOf(consts.imageHoverAnimations),
+  scrollAnimation: PropTypes.oneOf(consts.scrollAnimations),
+  hoveringBehaviour: PropTypes.oneOf(consts.hoveringBehaviours),
+  cropType,
+  cropRatio,
+  loop: PropTypes.bool, //slideshowLoop
+  autoSlide: PropTypes.number, //isAutoSlideshow && autoSlideshowInterval
+  itemsPerCol: numberOfImagesPerCol
+
+}
+
+export const layoutPropTypes = props => {
+  return props.reduce((resPropTypes, prop) => resPropTypes[prop] = basePropTypes[prop], {});
+}
+
+export const propsToStyles = props => {
+  return {
+    imageMargin: props.spacing,
+    isRTL: props.rtl,
+    isVertical: props.orientation === 'cols',
+    oneRow: props.scrollDirection === 'horizontal',
+    gallerySizeType: props.itemSize > 0 ? 'px' : 'smart',
+    gallerySizePx: props.itemSize,
+    allowHover: props.info === 'hover',
+    videoPlay: props.play,
+    overlayAnimation: props.overlayAnimation,
+    imageHoverAnimation: props.imageHoverAnimation,
+    scrollAnimation: props.scrollAnimation,
+    hoveringBehaviour: props.hoveringBehaviour,
+
+  }
+}
