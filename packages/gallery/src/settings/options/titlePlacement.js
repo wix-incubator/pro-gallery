860a0b6a2b0b00738039b17703f297c674f8b328
izaacayelin <izaacaa@wix.com>
2020-01-16 14:26:06 +0200

    [gallery] options

--- packages/gallery/src/settings/options/titlePlacement.js
+++ packages/gallery/src/settings/options/titlePlacement.js
@@ -5,8 +5,8 @@ export default {
   title: 'Texts Placement',
   isRelevant: (styleParams) => (styleParams.isVertical &&
     styleParams.groupSize === 1 && 
-    styleParams.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL) || 
-    (styleParams.isSlider || styleParams.isColumns),
+    styleParams.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL) && 
+    !styleParams.isSlider && !styleParams.isColumns,
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.placements.SHOW_ON_HOVER,
   options: createOptions('placements'),
