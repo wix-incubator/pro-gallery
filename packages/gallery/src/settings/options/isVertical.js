373fa939007192583fbc5d71ba9ff4d7a247b2e7
izaacayelin <izaacaa@wix.com>
2020-01-21 11:13:30 +0200

    [gallery]

--- packages/gallery/src/settings/options/isVertical.js
+++ packages/gallery/src/settings/options/isVertical.js
@@ -1,10 +1,12 @@
 import { INPUT_TYPES } from '../utils/constants';
+import { createOptions } from '../utils/utils';
 
 export default {
   title: 'Layout Orientation',
   description: `Choose between a vertical to horizontal oriented gallery. Note that this option may affect
   other (e.g: "titlePlacement" - you will not be able to change this option when "isVertical" is "false")`,
   isRelevant: (styleParams) => !styleParams.oneRow,
-  type: INPUT_TYPES.BOOLEAN,
-  default: 10,
+  options: createOptions('isVertical'),
+  type: INPUT_TYPES.OPTIONS,
+  default: false,
 }
