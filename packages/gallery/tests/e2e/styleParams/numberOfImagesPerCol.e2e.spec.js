6218d3b8f9208d48b3b5559f22ef26b3e25e262b
izaacayelin <izaacaa@wix.com>
2020-02-05 12:53:10 +0200

    [gallery] tests

---
+++ packages/gallery/tests/e2e/styleParams/numberOfImagesPerCol.e2e.spec.js
@@ -0,0 +1,59 @@
+import GalleryDriver from '../../drivers/pptrDriver';
+import {toMatchImageSnapshot} from '../../drivers/matchers';
+import GALLERY_CONSTS from '../../../src/common/constants';
+
+jest.setTimeout(30000)
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('numberOfImagesPerCol - e2e', () => {
+  let driver;
+  
+  beforeEach(async () => {
+    driver = new GalleryDriver();
+    await driver.launchBrowser();
+  });
+
+  afterEach(() => {
+    driver.closeBrowser();
+  });
+  it('should set 1 image per a column', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      numberOfImagesPerCol:1,
+      isGrid: true,
+      oneRow: true,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.HORIZONTAL,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should set 2 image per a column', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      numberOfImagesPerCol:2,
+      isGrid: true,
+      oneRow: true,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.HORIZONTAL,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should set 3 image per a column', async () => {
+    await driver.openPage({
+      galleryLayout: 2,
+      numberOfImagesPerCol:3,
+      isGrid: true,
+      oneRow: true,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.HORIZONTAL,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+})
