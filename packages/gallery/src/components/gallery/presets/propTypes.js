29595985960c0c45476bd90940bfb06b89756af2
Guy Sopher <guyso@wix.com>
2020-02-10 17:46:06 +0200

    refactor

--- packages/gallery/src/components/gallery/presets/propTypes.js
+++ packages/gallery/src/components/gallery/presets/propTypes.js
@@ -14,23 +14,6 @@ export const basePropTypes = {
   resizeMediaUrl: PropTypes.function,
 };
 
-export const LAYOUT_PROPS = {
-  ORIENTATION: 'orientation',
-  RTL: 'rtl',
-  SPACING: 'spacing',
-  SCROLL_DIRECTION: 'scrollDirection',
-  ITEM_SIZE: 'itemSize',
-  INFO: 'info',
-  PLAY: 'play',
-  OVERLAY_ANIMATION: 'overlayAnimation',
-  IMAGE_OVER_ANIMATION: 'imageHoverAnimation',
-  SCROLL_ANIMATION: 'scrollAnimation',
-  HOVERING_BEHAVIOUR: 'hoveringBehaviour',
-  LOOP: 'loop',
-  AUTO_SLIDE: 'autoSlide',
-  ITEMS_PER_COL: 'itemsPerCol',
-}
-
 export const baseLayoutPropTypes = {
   orientaion: PropTypes.oneOf(['rows', 'cols']),
   rtl: PropTypes.bool,
@@ -43,34 +26,32 @@ export const baseLayoutPropTypes = {
   imageHoverAnimation: PropTypes.oneOf(consts.imageHoverAnimations),
   scrollAnimation: PropTypes.oneOf(consts.scrollAnimations),
   hoveringBehaviour: PropTypes.oneOf(consts.hoveringBehaviours),
-  // cropType,
-  // cropRatio,
   loop: PropTypes.bool, //slideshowLoop
   autoSlide: PropTypes.number, //isAutoSlideshow && autoSlideshowInterval
   itemsPerCol: PropTypes.number
+  // cropType,
+  // cropRatio,
 }
 
-export const layoutPropTypes = layoutProps => {
-  return {
-    ...basePropTypes,
-    ...layoutProps.reduce((resPropTypes, prop) => resPropTypes[prop] = baseLayoutPropTypes[prop], {})
-  }
-}
+export const LAYOUT_PROPS = Object.keys(baseLayoutPropTypes).reduce((layoutProps, prop) => ({...layoutProps, [prop]:prop}), {});
 
-export const propsToStyles = props => {
-  return {
-    ...props.styles,
-    ...(props.spacing && {imageMargin: props.spacing}),
-    ...(props.rtl && {isRTL: props.rtl}),
-    ...(props.orientation && {isVertical: props.orientation === 'cols'}),
-    ...(props.scrollDirection && {oneRow: props.scrollDirection === 'horizontal'}),
-    ...(props.itemSize && {gallerySizeType: props.itemSize > 0 ? 'px' : 'smart'}),
-    ...(props.itemSize && {gallerySizePx: props.itemSize}),
-    ...(props.info && {allowHover: props.info === 'hover'}),
-    ...(props.play && {videoPlay: props.play}),
-    ...(props.overlayAnimation && {overlayAnimation: props.overlayAnimation}),
-    ...(props.imageHoverAnimation && {imageHoverAnimation: props.imageHoverAnimation}),
-    ...(props.scrollAnimation && {scrollAnimation: props.scrollAnimation}),
-    ...(props.hoveringBehaviour && {hoveringBehaviour: props.hoveringBehaviour}),
-  }
-}
+export const layoutPropTypes = layoutProps => ({
+  ...basePropTypes,
+  ...layoutProps.reduce((resPropTypes, prop) => ({...resPropTypes, [prop]: baseLayoutPropTypes[prop]}), {})
+})
+
+export const propsToStyles = props => ({
+  ...(props.spacing && {imageMargin: props.spacing}),
+  ...(props.rtl && {isRTL: props.rtl}),
+  ...(props.orientation && {isVertical: props.orientation === 'cols'}),
+  ...(props.scrollDirection && {oneRow: props.scrollDirection === 'horizontal'}),
+  ...(props.itemSize && {gallerySizeType: props.itemSize > 0 ? 'px' : 'smart'}),
+  ...(props.itemSize && {gallerySizePx: props.itemSize}),
+  ...(props.info && {allowHover: props.info === 'hover'}),
+  ...(props.play && {videoPlay: props.play}),
+  ...(props.overlayAnimation && {overlayAnimation: props.overlayAnimation}),
+  ...(props.imageHoverAnimation && {imageHoverAnimation: props.imageHoverAnimation}),
+  ...(props.scrollAnimation && {scrollAnimation: props.scrollAnimation}),
+  ...(props.hoveringBehaviour && {hoveringBehaviour: props.hoveringBehaviour}),
+  ...props.styles,
+})
