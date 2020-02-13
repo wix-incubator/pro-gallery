88a365d7b3531e31cb3ba08e31776e406a89034d
izaacayelin <izaacaa@wix.com>
2020-02-13 10:41:59 +0200

    [gallery] options

--- packages/gallery/src/settings/options/videoPlay.js
+++ packages/gallery/src/settings/options/videoPlay.js
@@ -3,7 +3,7 @@ import { createOptions } from '../utils/utils';
 
 export default {
   title: 'Playing Options',
-  description: `Choose when video items will start playing: On hover, click or autoplay`,
+  description: `Choose when video items will start playing: On hover, on click or autoplay`,
   isRelevant: (styleParams) => true,
   options: createOptions('videoPlay'),
   type: INPUT_TYPES.OPTIONS,
