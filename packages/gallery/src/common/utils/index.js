636d854de6f631b9cb58ea8cf00815888dfbe17d
izaacayelin <izaaca@wix.com>
2020-02-16 10:16:12 +0200

    [gallery] fixed isiOS function

--- packages/gallery/src/common/utils/index.js
+++ packages/gallery/src/common/utils/index.js
@@ -329,7 +329,9 @@ class Utils {
   isiOS() {
     return this.getOrPutFromCache('isiOS', () => {
       try {
-        return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+        return (/iPad|iPhone|iPod/.test(navigator.userAgent) ||
+        (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1))
+        && !window.MSStream;
       } catch (e) {
         return false;
       }
