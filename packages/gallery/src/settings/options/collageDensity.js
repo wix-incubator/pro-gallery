b40b7c5d722d18b2970b9b38a489f0e95716682f
izaacayelin <izaaca@wix.com>
2020-02-19 14:23:53 +0200

    [gallery] tests and options

---
+++ packages/gallery/src/settings/options/collageDensity.js
@@ -0,0 +1,10 @@
+import { INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Collage Density',
+  description: `Determines how dense the collaging and grouping of images will be. Note that in order for this option to take any effect,
+  the option "groupSize" needs to be set to more than 1`,
+  isRelevant: (styleParams) => styleParams.groupSize > 1 && styleParams.groupsPerStrip > 1,
+  type: INPUT_TYPES.NUMBER,
+  default: 0.8,
+}
