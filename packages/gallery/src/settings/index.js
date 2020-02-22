2a7285568a168923a6846248e69e37177f15f5f8
izaacayelin <izaaca@wix.com>
2020-02-22 23:29:58 +0200

    [gallery] tests and options

--- packages/gallery/src/settings/index.js
+++ packages/gallery/src/settings/index.js
@@ -30,6 +30,7 @@ import textBoxBorderColor from './options/textBoxBorderColor';
 import textBoxBorderRadius from './options/textBoxBorderRadius';
 import textBoxBorderWidth from './options/textBoxBorderWidth';
 import textBoxWidth from './options/textBoxWidth';
+import thumbnailSize from './options/thumbnailSize';
 
 export default {
     itemClick,
@@ -64,4 +65,5 @@ export default {
     textBoxBorderRadius,
     textBoxBorderWidth,
     textBoxWidth,
+    thumbnailSize,
 }
---
+++ packages/gallery/src/settings/options/thumbnailSize.js
@@ -0,0 +1,12 @@
+import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Thumbnail Size',
+  isRelevant: (styleParams)  => true,
+  type: INPUT_TYPES.NUMBER,
+  default: 120,
+  min: 80,
+  max: 300,
+  description: `Set the size (width and height) of each of the thumbnails in the Thumbnail layout.
+  `,
+}
