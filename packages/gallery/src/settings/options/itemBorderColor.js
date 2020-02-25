0c15025f51bf71cd562ec74fe03e677980127bcd
izaacayelin <izaaca@wix.com>
2020-02-25 12:27:38 +0200

    [gallery] fixed isRelevant

--- packages/gallery/src/settings/options/itemBorderColor.js
+++ packages/gallery/src/settings/options/itemBorderColor.js
@@ -3,7 +3,7 @@ import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
 export default {
   title: 'Item Border Color',
   description: `Set the border color for each item in the gallery.`,
-  isRelevant: (styleParams) => styleParams.hoveringBehaviour !== GALLERY_CONSTS.infoBehaviourOnHover.NEVER_SHOW,
+  isRelevant: (styleParams) => styleParams.cubeType !== GALLERY_CONSTS.cubeType.FIT,
   type: INPUT_TYPES.NUMBER,
   default: 0,
 }
