50769655fb638ca25b5142a311116576de3a7872
izaacayelin <izaaca@wix.com>
2020-02-25 12:56:41 +0200

    [gallery] isRelevant

--- packages/gallery/src/settings/options/imageInfoType.js
+++ packages/gallery/src/settings/options/imageInfoType.js
@@ -7,8 +7,8 @@ export default {
   description: `Choose the layout you want for your texts, you can choose to separate the texts and the items
   so you can style them separately.`,
   isRelevant: (styleParams) => (styleParams.allowTitle || styleParams.allowDescription) &&
-  styleParams.titlePlacement !== GALLERY_CONSTS.placements.SHOW_ON_HOVER && 
-  styleParams.groupSize === 1 && styleParams.isVertical,
+  styleParams.groupSize === 1 && styleParams.isVertical && 
+  styleParams.titlePlacement !== GALLERY_CONSTS.placements.SHOW_ON_HOVER,
   options: createOptions('infoType'),
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.infoType.NO_BACKGROUND,
