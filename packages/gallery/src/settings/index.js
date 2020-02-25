0bdbb6e11dbcc67c9644e3ccb00eb9cf84e55e0f
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-02-25 17:49:27 +0200

    [gallery] tests (groupSize): added tests and options

--- packages/gallery/src/settings/index.js
+++ packages/gallery/src/settings/index.js
@@ -32,6 +32,7 @@ import textBoxBorderWidth from './options/textBoxBorderWidth';
 import textBoxWidth from './options/textBoxWidth';
 import textBoxWidthPercent from './options/textBoxWidthPercent';
 import imageInfoType from './options/imageInfoType';
+import itemBorderWidth from './options/itemBorderWidth';
 
 export default {
     itemClick,
@@ -68,4 +69,5 @@ export default {
     textBoxWidth,
     textBoxWidthPercent,
     imageInfoType,
+    itemBorderWidth,
 }
