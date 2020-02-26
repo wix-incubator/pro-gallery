5946ab3a40e44c2e792f89ae1b271873d9a470a1
izaacayelin <izaaca@wix.com>
2020-02-26 17:35:40 +0200

    [gallery] fix: (enableinfiniteScroll.js): fixed isrelevant

--- packages/gallery/src/settings/options/enableInfiniteScroll.js
+++ packages/gallery/src/settings/options/enableInfiniteScroll.js
@@ -4,7 +4,7 @@ export default {
   title: 'Use Infinite Scroll',
   description: `Choose Whether you want to have infinite scroll option or not. When this option is set to false the gallery will have
   a "Load More". Note that the consumer will have to add the new items when the gallery requests more ("NEED_MORE_ITEMS" event will be emited).`,
-  isRelevant: (styleParams) => styleParams.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL,
+  isRelevant: (styleParams) => !styleParams.oneRow,
   type: INPUT_TYPES.BOOLEAN,
   default: true,
 }
