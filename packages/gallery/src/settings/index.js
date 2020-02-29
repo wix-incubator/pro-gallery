2897b2bc9041f33e6a024fdf488189c956310bde
izaacayelin <izaaca@wix.com>
2020-02-29 18:11:55 +0200

    [gallery] tests and options

--- packages/gallery/src/settings/index.js
+++ packages/gallery/src/settings/index.js
@@ -33,6 +33,7 @@ import textBoxWidth from './options/textBoxWidth';
 import imageInfoType from './options/imageInfoType';
 import groupSize from './options/groupSize';
 import collageDensity from './options/collageDensity';
+import hasThumbnails from './options/hasThumbnails';
 
 export default {
     itemClick,
@@ -70,4 +71,5 @@ export default {
     imageInfoType,
     groupSize,
     collageDensity,
+    hasThumbnails
 }
