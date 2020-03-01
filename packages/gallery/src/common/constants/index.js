ec16c26c226468856528cf383f97a9a07f368657
izaacayelin <izaaca@wix.com>
2020-03-01 11:59:48 +0200

    [gallery] tests

--- packages/gallery/src/common/constants/index.js
+++ packages/gallery/src/common/constants/index.js
@@ -25,6 +25,7 @@ import dimensions from './dimensions';
 import gallerySizeType from './gallerySizeType';
 import isVertical from './isVertical';
 import galleryTextAlign from './galleryTextAlign.js';
+import layoutDirection from './layoutDirection';
 
 export default {
   events,
@@ -53,5 +54,6 @@ export default {
   dimensions,
   gallerySizeType,
   isVertical,
-  galleryTextAlign
+  galleryTextAlign,
+  layoutDirection,
 };
---
+++ packages/gallery/tests/styleParams/isRTL.spec.js
@@ -0,0 +1,60 @@
+import GalleryDriver from '../drivers/reactDriver'
+import { expect } from 'chai';
+import { images2 } from '../drivers/mocks/items';
+import { styleParams, container } from '../drivers/mocks/styles';
+import GALLERY_CONSTS from '../../src/common/constants';
+
+describe('styleParam - isRTL', () => {
+
+  let driver;
+  const initialProps = {
+    container,
+    items: images2,
+    styles: styleParams
+  }
+
+  beforeEach(() => {
+    driver = new GalleryDriver();
+  });
+
+  it('should not set "rtl" class to the gallery container', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout: GALLERY_CONSTS.layout.EMPTY,
+      isRTL: GALLERY_CONSTS.layoutDirection.LEFT_TO_RIGHT,
+    })
+    driver.mount.proGallery(initialProps);
+    const galleryContainer = driver.find.selector('#pro-gallery-container');
+    expect(galleryContainer.hasClass('rtl')).to.be.false;
+    driver.detach.proGallery();
+  });
+  it('should set "rtl" class to the gallery container', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout: GALLERY_CONSTS.layout.EMPTY,
+      isRTL: GALLERY_CONSTS.layoutDirection.RIGHT_TO_LEFT,
+    })
+    driver.mount.proGallery(initialProps);
+    const galleryContainer = driver.find.selector('#pro-gallery-container');
+    expect(galleryContainer.hasClass('rtl')).to.be.true;
+    driver.detach.proGallery();
+  });
+  it('should start keyboard navigation from left to right', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout: GALLERY_CONSTS.layout.GRID,
+      isRTL: GALLERY_CONSTS.layoutDirection.LEFT_TO_RIGHT,
+    })
+    driver.mount.proGallery(initialProps);
+    const galleryContainer = driver.find.selector('#pro-gallery-container');
+    galleryContainer.getDOMNode().focus();
+    // galleryContainer.simulate('keypress', {key: 'Tab'});
+    // galleryContainer.simulate('keypress', {key: 'Tab'});
+    // driver.wrapper.simulate('keypress', {key: 'Tab'});
+    // driver.wrapper.simulate('keydown', {keyCode: 9});
+    // driver.wrapper.simulate('keydown', {key: 'Tab',keyCode: 9, which: 9});
+    // galleryContainer.simulate('keydown', {key: 'Tab',keyCode: 9, which: 9});
+    console.log(document.activeElement.tagName);
+    
+    expect()
+    driver.detach.proGallery();
+  });
+
+})
