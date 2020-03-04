418ddba08a16d431bb871d564a700689a1139729
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-03-04 12:43:54 +0200

    [gallery] tests (hasThumbnails): tests and options

--- packages/gallery/src/components/gallery/presets/panoramaGallery.js
+++ packages/gallery/src/components/gallery/presets/panoramaGallery.js
@@ -18,7 +18,6 @@ export const fixedStyles = {
   gallerySize: () => dimensionsHelper.getGalleryWidth(),
   oneRow: false,
   fixedColumns: 1,
-  hasThumbnails: false,
   enableScroll: true,
   isGrid: false,
   isColumns: false,
---
+++ packages/gallery/src/settings/options/hasThumbnails.js
@@ -0,0 +1,11 @@
+import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Slider Thumbnails',
+  isRelevant: (styleParams)  => styleParams.oneRow,
+  type: INPUT_TYPES.BOOLEAN,
+  default: false,
+  description: `Choose if you want to have slider thumbnails in the gallery. Note that this option is relevant only
+  to galleries that render the slideShowView component.
+  `,
+}
