1b92b073a5c2499916fbaae8630c406f848c4c19
izaacayelin <izaaca@wix.com>
2020-03-14 20:28:00 +0200

    [gallery] fixed isRelevant

--- packages/gallery/src/settings/options/itemShadowBlur.js
+++ packages/gallery/src/settings/options/itemShadowBlur.js
@@ -2,8 +2,10 @@ import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
 
 export default {
   title: 'Item Shadow Blur',
-  isRelevant: (styleParams)  => styleParams.itemEnableShadow && !styleParams.oneRow && 
-  (!styleParams.cubeImages || !styleParams.cubeType !== GALLERY_CONSTS.cubeType.FIT),
+  isRelevant: (styleParams) => styleParams.itemEnableShadow && !styleParams.oneRow &&
+    (!styleParams.cubeImages || !styleParams.cubeType !== GALLERY_CONSTS.cubeType.FIT) &&
+    styleParams.imageInfoType === GALLERY_CONSTS.infoType.ATTACHED_BACKGROUND ||
+    styleParams.titlePlacement === GALLERY_CONSTS.placements.SHOW_ON_HOVER,
   type: INPUT_TYPES.NUMBER,
   default: 20,
 }
