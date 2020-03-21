7b1b450dc725b04a4636e2c87db17e1c0a668ad3
izaacayelin <izaaca@wix.com>
2020-03-21 17:42:31 +0200

    [gallery] tests (titleDescriptionSpace.js): fixed wrong default value that fails tests

--- packages/gallery/src/settings/options/titleDescriptionSpace.js
+++ packages/gallery/src/settings/options/titleDescriptionSpace.js
@@ -4,7 +4,7 @@ export default {
   title: 'Title Description Spacing',
   isRelevant: (styleParams)  => styleParams.allowTitle && styleParams.allowDescription ,
   type: INPUT_TYPES.NUMBER,
-  default: 0,
+  default: 6,
   min: 0,
   max: 50,
   description: `Set the spacing between the title and the description.
