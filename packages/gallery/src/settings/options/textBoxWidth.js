cb13f4681940c78c201b1f2bb10c532599b6c5d6
izaacayelin <izaaca@wix.com>
2020-03-23 16:53:35 +0200

    Revert "[gallery] fixed typo"
    This reverts commit f388d05bef9eb54c2d1db28b75d46dcf2ed36a22.

--- packages/gallery/src/settings/options/textBoxWidth.js
+++ packages/gallery/src/settings/options/textBoxWidth.js
@@ -5,7 +5,7 @@ export default {
   description: `Set the text box width when on the right side or on the left side.`,
   isRelevant: (styleParams) => (styleParams.isVertical &&
     styleParams.groupSize === 1 &&
-    !styleParams.oneRow) &&
+    !styleParams.oneRow &&
     styleParams.titlePlacement === GALLERY_CONSTS.placements.MANUAL &&
     styleParams.calculateTextBoxWidthMode === GALLERY_CONSTS.textBoxWidthCalculationOptions.MANUAL &&
     (styleParams.titlePlacement === GALLERY_CONSTS.placements.SHOW_ON_THE_LEFT ||
