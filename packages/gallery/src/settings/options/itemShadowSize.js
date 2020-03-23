47c06a3b3178b4dc3ad71db8829f290075dda2ff
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-03-23 18:20:34 +0200

    [gallery] tests (itemShadow): added test and options

---
+++ packages/gallery/src/settings/options/itemShadowSize.js
@@ -0,0 +1,11 @@
+import { GALLERY_CONSTS, INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Item Shadow Size',
+  isRelevant: (styleParams) => styleParams.itemEnableShadow && !styleParams.oneRow &&
+    !(styleParams.cubeImages && styleParams.cubeType === GALLERY_CONSTS.cubeType.FIT) &&
+    (styleParams.imageInfoType === GALLERY_CONSTS.infoType.ATTACHED_BACKGROUND ||
+    styleParams.titlePlacement === GALLERY_CONSTS.placements.SHOW_ON_HOVER),
+  type: INPUT_TYPES.NUMBER,
+  default: 10,
+}
