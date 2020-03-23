fb692ec815d12c45fab8af8e0c1f0ad1ce18103d
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-03-23 17:44:33 +0200

    [gallery] tests (imageLoadingMode ): added tests and options

---
+++ packages/gallery/tests/styleParams/imageLoadingMode.spec.js
@@ -0,0 +1,61 @@
+import GalleryDriver from '../drivers/reactDriver'
+import { expect } from 'chai';
+import { images2 } from '../drivers/mocks/items';
+import { styleParams, container } from '../drivers/mocks/styles';
+import GALLERY_CONSTS from '../../src/common/constants';
+import GalleryItem from '../../src/components/item/galleryItem';
+import sinon from 'sinon';
+
+describe('styleParam - imageLoadingMode', () => {
+
+  let driver;
+  let imageStub;
+  const initialProps = {
+    container,
+    items: images2.slice(0,1),
+    styles: styleParams
+  }
+
+  beforeEach(() => {
+    driver = new GalleryDriver();
+  });
+
+  it('should preload blury image', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout:  GALLERY_CONSTS.layout.GRID,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.VERTICAL,
+      oneRow: false,
+      imageLoadingMode: GALLERY_CONSTS.loadingMode.BLUR,
+    })
+    imageStub = sinon.stub(GalleryItem.prototype, 'createUrl');
+    driver.mount.proGallery(initialProps);
+    expect(imageStub.withArgs('resized','thumb').called).to.be.true;
+    imageStub.restore();
+    driver.detach.proGallery();
+  });
+  it('should preload pixel image (MAIN_COLOR)', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout:  GALLERY_CONSTS.layout.GRID,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.VERTICAL,
+      oneRow: false,
+      imageLoadingMode: GALLERY_CONSTS.loadingMode.MAIN_COLOR,
+    })
+    imageStub = sinon.stub(GalleryItem.prototype, 'createUrl');
+    driver.mount.proGallery(initialProps);
+    expect(imageStub.withArgs('pixel','img').called).to.be.true;
+    imageStub.restore();
+    driver.detach.proGallery();
+  });
+  it('should preload color background (MAIN_COLOR)', () => {
+    Object.assign(initialProps.styles, {
+      galleryLayout:  GALLERY_CONSTS.layout.GRID,
+      scrollDirection: GALLERY_CONSTS.scrollDirection.VERTICAL,
+      oneRow: false,
+      imageLoadingMode: GALLERY_CONSTS.loadingMode.COLOR,
+    })
+    driver.mount.proGallery(initialProps);
+    const item = driver.find.hook('image-item').at(0);
+    expect(item.hasClass('load-with-color')).to.be.true;
+    driver.detach.proGallery();
+  });
+})
