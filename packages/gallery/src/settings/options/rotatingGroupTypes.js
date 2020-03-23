45b91c45d2124e45b2a5ad492c588fcc90791fa1
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-03-23 18:09:13 +0200

    [gallery] tests (rotatingGroupTypes): added tests and options

---
+++ packages/gallery/src/settings/options/rotatingGroupTypes.js
@@ -0,0 +1,12 @@
+import { INPUT_TYPES } from '../utils/constants';
+import { createOptions } from '../utils/utils';
+
+export default {
+  title: 'Rotating Group Types',
+  description: `Choose the order of group types to appear in rotation in a collage gallery. Note that
+  the groupSize must support the groups specified (if groupSize set to 2, you cannot use groups of 3)`,
+  isRelevant: (styleParams) => styleParams.groupSize > 1,
+  type: INPUT_TYPES.MULTISELECT,
+  options: createOptions('groupTypes'),
+  default: '',
+}
