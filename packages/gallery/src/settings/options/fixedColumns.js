835866ecfb0061a71a1765af5ee7a19c28aefec9
izaacayelin <izaaca@wix.com>
2020-03-23 15:26:26 +0200

    [gallery] renamed title

--- packages/gallery/src/settings/options/fixedColumns.js
+++ packages/gallery/src/settings/options/fixedColumns.js
@@ -1,7 +1,7 @@
 import { INPUT_TYPES } from '../utils/constants';
 
 export default {
-  title: 'Fixed Columns',
+  title: 'Number Of Columns',
   description: `Set a fixed number of columns in the gallery. Note that this option relies on the options isVertical (set to "true")
   and oneRow (set to "false").`,
   isRelevant: (styleParams) => !styleParams.oneRow && styleParams.isVertical,
