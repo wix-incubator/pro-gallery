11ec7a96ee30b4856e674d9e380a988ac14b544c
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-03-23 18:02:41 +0200

    [gallery] tests (fixedColumns): added tests and options

---
+++ packages/gallery/tests/e2e/styleParams/fixedColumns.e2e.spec.js
@@ -0,0 +1,53 @@
+import GalleryDriver from '../../drivers/pptrDriver';
+import {toMatchImageSnapshot} from '../../drivers/matchers';
+import GALLERY_CONSTS from '../../../src/common/constants';
+
+jest.setTimeout(30000)
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('fixedColumns - e2e', () => {
+  let driver;
+  
+  beforeEach(async () => {
+    driver = new GalleryDriver();
+    await driver.launchBrowser();
+  });
+
+  afterEach(() => {
+    driver.closeBrowser();
+  });
+  it('should set fixedColimns of "1"', async () => {
+    await driver.openPage({
+      galleryLayout: GALLERY_CONSTS.layout.EMPTY,
+      groupSize:1,
+      isVertical:true,
+      fixedColumns: 1,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should set fixedColimns of "2"', async () => {
+    await driver.openPage({
+      galleryLayout: GALLERY_CONSTS.layout.EMPTY,
+      groupSize:1,
+      isVertical:true,
+      fixedColumns: 2,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+  it('should set fixedColimns of "3"', async () => {
+    await driver.openPage({
+      galleryLayout: GALLERY_CONSTS.layout.EMPTY,
+      groupSize:1,
+      isVertical:true,
+      fixedColumns: 3,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+})
