8929a510634958d36066a7de4cf418bf10c7ac70
Yonatan Hattav <yhattav@gmail.com>
2020-04-13 11:25:50 +0300

    [gallery] add video placeholder item option - metaData.isVideoPlaceholder

--- packages/gallery/src/components/item/itemView.js
+++ packages/gallery/src/components/item/itemView.js
@@ -658,7 +658,11 @@ class ItemView extends GalleryComponent {
       case 'image':
       case 'picture':
       default:
+        if (this.props.isVideoPlaceholder) {
+          itemInner = this.getVideoItemPlaceholder(imageDimensions, itemHover);
+        } else {
         itemInner = [this.getImageItem(imageDimensions), itemHover];
+        }
     }
 
     if (styleParams.isSlideshow) {
