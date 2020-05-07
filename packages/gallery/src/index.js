7b6570f589b3375f10f4205448c47a314189488a
Guy Sopher <guyso@wix.com>
2020-05-07 12:15:12 +0300

    [gallery] fix: typo

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -4,7 +4,7 @@ export { default as ProGallery } from './components/gallery/index';
 export { default as LoveButton } from './components/item/loveButton/loveButton';
 export { default as gallerySettings } from './settings/merged';
 export { default as defaultStyles}  from './common/defaultStyles';
-export { deafult as LeanGallery } from './components/gallery/leanGallery/leanGallery';
+export { default as LeanGallery } from './components/gallery/leanGallery/leanGallery';
 export { default as isEligibleForLeanGallery, notEligibleReasons } from './components/gallery/leanGallery/isEligible';
 export { default as ExpandableProGallery } from './components/gallery/presets/expandableGallery';
 export { cssScrollHelper } from './components/helpers/cssScrollHelper';
