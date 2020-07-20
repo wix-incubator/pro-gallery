d1a99b7dbd0417c27a542c27f1bf6a72da9d3648
Guy Sopher <guyso@wix.com>
2020-07-20 15:38:24 +0300

    extend pro-gallery

--- packages/gallery/src/components/gallery/proGallery/proBlueprintsGallery.js
+++ packages/gallery/src/components/gallery/proGallery/proBlueprintsGallery.js
@@ -1,44 +1,8 @@
-import '../../../common/utils/polyfills';
-
 import React from 'react';
 import Gallery from './galleryContainerExtraNew.js';
-import utils from '../../../common/utils';
-import { viewModeWrapper } from '../../../common/window/viewModeWrapper';
-import { GalleryComponent } from '../../galleryComponent';
+import ProGallery from './proGallery';
 
-import '../../../versionLogger';
-
-export default class ProGallery extends GalleryComponent {
-  constructor(props) {
-    super();
-    this.init(props);
-    if (utils.isLocal() && !utils.isTest()) {
-      console.log('PRO BLUEPRINTS GALLERY DEV');
-    }
-  }
-
-  init(props) {
-    //this should be some sort of HOC
-    if (typeof props.viewMode !== 'undefined') {
-      viewModeWrapper.setViewMode(props.viewMode);
-    }
-    if (typeof props.formFactor !== 'undefined') {
-      viewModeWrapper.setFormFactor(props.formFactor);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    //this should be some sort of HOC
-    if (this.props.viewMode !== nextProps.viewMode) {
-      utils.dumpCache();
-      viewModeWrapper.setViewMode(nextProps.viewMode);
-    }
-    if (this.props.formFactor !== nextProps.formFactor) {
-      utils.dumpCache();
-      viewModeWrapper.setFormFactor(nextProps.formFactor);
-    }
-  }
-
+export default class ProBlueprintsGallery extends ProGallery {
   render() {
     return (
       <div id={`pro-gallery-${this.props.domId}`} className="pro-gallery">
