81bbb42fb2d21bf6b6a714b5f7998d54cafce344
Yonatan Hattav <yhattav@gmail.com>
2020-07-23 21:23:16 +0300

    [gallery] fix blueprintsGallery entry

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -1,7 +1,7 @@
 import './components/styles/gallery.scss';
 
 export { default as ProGallery } from './components/gallery/index';
-export { default as ProBlueprintsGallery } from './components/gallery/proGallery/proBlueprintsGallery';
+export { default as ProBlueprintsGallery } from './components/gallery/blueprintsIndex';
 export { default as gallerySettings } from './settings/merged';
 export { default as LeanGallery } from './components/gallery/leanGallery/leanGallery';
 export { default as isEligibleForLeanGallery, notEligibleReasons } from './components/gallery/leanGallery/isEligible';
