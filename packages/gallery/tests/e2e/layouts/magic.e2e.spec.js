e25f26fa2a707b546f58efa26062c603195361b0
ritabazak <rita.bazak@gmail.com>
2020-08-09 18:04:42 +0300

    [gallery]: fixed lats commit

--- packages/gallery/tests/e2e/layouts/magic.e2e.spec.js
+++ packages/gallery/tests/e2e/layouts/magic.e2e.spec.js
@@ -38,28 +38,28 @@ describe('magic - e2e', () => {
     const page = await driver.grab.elemScreenshot('.pro-gallery');
     expect(page).toMatchImageSnapshot();
   });
-  // it('magic - scrollDirection = vertical, magicLayoutSeed = 2 (horizontal)', async () => {
-  //   await driver.openPage({
-  //     galleryLayout: GALLERY_CONSTS.layout.MAGIC,
-  //     magicLayoutSeed: 2,
-  //     scrollDirection:GALLERY_CONSTS.scrollDirection.VERTICAL
-  //   });
-  //   // await driver.waitFor.hookToBeVisible('item-container');
-  //   await driver.waitFor.timer(200);
-  //   const page = await driver.grab.screenshot();
-  //   expect(page).toMatchImageSnapshot();
-  // });
-  //
-  // it('magic - scrollDirection = horizontal, magicLayoutSeed = 2 (horizontal)', async () => {
-  //   await driver.openPage({
-  //     galleryLayout: GALLERY_CONSTS.layout.MAGIC,
-  //     magicLayoutSeed: 2,
-  //     scrollDirection:GALLERY_CONSTS.scrollDirection.HORIZONTAL
-  //   });
-  //   // await driver.waitFor.hookToBeVisible('item-container');
-  //   await driver.waitFor.timer(200);
-  //   const page = await driver.grab.screenshot();
-  //   expect(page).toMatchImageSnapshot();
-  // });
+  it('magic - scrollDirection = vertical, magicLayoutSeed = 2 (horizontal)', async () => {
+    await driver.openPage({
+      galleryLayout: GALLERY_CONSTS.layout.MAGIC,
+      magicLayoutSeed: 2,
+      scrollDirection:GALLERY_CONSTS.scrollDirection.VERTICAL
+    });
+    // await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.screenshot();
+    expect(page).toMatchImageSnapshot();
+  });
+
+  it('magic - scrollDirection = horizontal, magicLayoutSeed = 2 (horizontal)', async () => {
+    await driver.openPage({
+      galleryLayout: GALLERY_CONSTS.layout.MAGIC,
+      magicLayoutSeed: 2,
+      scrollDirection:GALLERY_CONSTS.scrollDirection.HORIZONTAL
+    });
+    // await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.screenshot();
+    expect(page).toMatchImageSnapshot();
+  });
 
 })
