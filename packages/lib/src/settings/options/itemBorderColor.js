f484af62584452a7e0fbe2cbbf7276cfaa428c8b
Guy Sopher <guyso@wix.com>
2020-09-10 17:05:32 +0300

    [playground] fix (settings): re-add disabled design settings

--- packages/lib/src/settings/options/itemBorderColor.js
+++ packages/lib/src/settings/options/itemBorderColor.js
@@ -5,6 +5,6 @@ export default {
   title: 'Item Border Color',
   description: `Set the border color for each item in the gallery.`,
   isRelevant: (styleParams) => styleParams.cubeType !== GALLERY_CONSTS.cubeType.FIT,
-  type: INPUT_TYPES.NUMBER,
+  type: INPUT_TYPES.COLOR_PICKER,
   default: 0,
 }
