0a0da34b0dd8a25bf99970cd45eb0f56fbee733b
Guy Sopher <guyso@wix.com>
2020-09-23 21:29:30 +0300

    export layoutFixer

--- packages/gallery/src/components/gallery/proGallery/layoutFixer.js
+++ packages/gallery/src/components/gallery/proGallery/layoutFixer.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import layoutFixerStr from '../../../common/utils/layoutFixerStr';
 import { isPrerenderMode } from 'pro-gallery-lib';
 
 const logString = `[LAYOUT FIXER] SSR script (log string)`;
 
 export const LayoutFixer = (props) => {
     // console.log('[LAYOUT FIXER] rendering', isPrerenderMode(), props);
-    return (isPrerenderMode()) ? (
+    return (isPrerenderMode) ? (
         <>
             <script dangerouslySetInnerHTML={{__html: `console.log("${logString}")`}}></script>
-            <script dangerouslySetInnerHTML={{__html: '(' + layoutFixerStr + ')()'}}></script>
+            <script src={props.baseUrl + "/layoutFixer.bundle.min.js"}></script>
             <layout-fixer 
                 parentId={props.parentId}
                 items={JSON.stringify(props.items)}
--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -4,6 +4,7 @@ export { default as ProGallery } from './components/gallery/index';
 export { default as ProBlueprintsGallery } from './components/gallery/blueprintsIndex';
 export { default as LayoutingProGallery } from './components/gallery/layoutingIndex';
 export { default as LeanGallery } from './components/gallery/leanGallery/leanGallery';
+export { default as LayoutFixer } from './components/gallery/proGallery/layoutFixer';
 export { default as isEligibleForLeanGallery, notEligibleReasons } from './components/gallery/leanGallery/isEligible';
 export { cssScrollHelper } from './components/helpers/cssScrollHelper';
 export { default as addLayoutStyles } from './components/helpers/layoutHelper';
