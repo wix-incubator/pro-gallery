2c080a77b551782a20bd41923b7678c104eafa04
Guy Sopher <guyso@wix.com>
2020-10-01 16:01:25 +0300

    [gallery] fix (layoutFixer): use isPrerenderMode properly

--- packages/gallery/src/components/layoutFixer/layoutFixer.js
+++ packages/gallery/src/components/layoutFixer/layoutFixer.js
@@ -3,7 +3,7 @@ import { isPrerenderMode } from 'pro-gallery-lib';
 
 export const LayoutFixer = (props) => {
     // console.log('[LAYOUT FIXER] rendering', isPrerenderMode(), props);
-    return (isPrerenderMode) ? (
+    return (isPrerenderMode()) ? (
         <>
             <script dangerouslySetInnerHTML={{__html: `console.log("[LAYOUT FIXER] Start (first script loaded)")`}}></script>
             <script dangerouslySetInnerHTML={{__html: `console.time("[LAYOUT FIXER] Done")`}}></script>
