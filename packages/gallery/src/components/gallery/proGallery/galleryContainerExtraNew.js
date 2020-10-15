1a4fe40a0811c018211b6ecee064ca4604502006
Guy Sopher <guyso@wix.com>
2020-10-15 12:30:57 +0300

    [gallery] improve (layoutFixer): preload the layout fixer and use opacity:0 always

--- packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
@@ -404,9 +404,9 @@ export class GalleryContainer extends React.Component {
   }
 
   createDynamicStyles({overlayBackground}) {
-    const allowSSROpacity = isPrerenderMode() && !!this.props.settings.allowSSROpacity;
+    const useSSROpacity = isPrerenderMode();
     this.dynamicStyles = `
-      ${!allowSSROpacity ? '' : `#pro-gallery-${this.props.domId} .gallery-item-container { opacity: 0 }`}
+      ${!useSSROpacity ? '' : `#pro-gallery-${this.props.domId} .gallery-item-container { opacity: 0 }`}
       ${!overlayBackground ? '' : `#pro-gallery-${this.props.domId} .gallery-item-hover::before { background-color: ${overlayBackground} !important}`}
     `.trim();
   }
