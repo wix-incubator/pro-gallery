a53880f6d03545211645e4663856b1293170b95e
Guy Sopher <guyso@wix.com>
2020-10-15 12:34:35 +0300

    [gallery] improve (layoutFixer): add async to script to prioritise it

--- packages/gallery/src/components/layoutFixer/layoutFixer.js
+++ packages/gallery/src/components/layoutFixer/layoutFixer.js
@@ -8,7 +8,7 @@ export const LayoutFixer = (props) => {
             <link rel="preload" href={props.layoutFixerBundleUrl} as="script"/>
             <script dangerouslySetInnerHTML={{__html: `console.log("[LAYOUT FIXER] Start (first script loaded)")`}}></script>
             <script dangerouslySetInnerHTML={{__html: `console.time("[LAYOUT FIXER] Done")`}}></script>
-            <script src={props.layoutFixerBundleUrl}></script>
+            <script async type="module" src={props.layoutFixerBundleUrl}></script>
             <layout-fixer 
                 parentId={props.parentId}
                 items={JSON.stringify(props.items)}
