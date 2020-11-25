7f2273b074c16da96911ba99f0e1b194453a8573
izaacayelin <izaaca@wix.com>
2020-11-25 15:11:55 +0200

    [gallery] fix lint errors

--- packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainerExtraNew.js
@@ -221,14 +221,14 @@ export class GalleryContainer extends React.Component {
   getVisibleItems(items, container) {
     const { gotFirstScrollEvent } = this.state;
     const scrollY = window.scrollY;
-    const {galleryHeight, scrollBase, galleryWidth} = container;
+    const { galleryHeight, scrollBase, galleryWidth } = container;
     if (
-      (utils.isSSR() && !this.props.settings.renderVisibleItemsInSsr) || 
-      isSEOMode() || 
-      isEditMode() || 
-      isPreviewMode() ||       
-      gotFirstScrollEvent || 
-      scrollY > 0 || 
+      (utils.isSSR() && !this.props.settings.renderVisibleItemsInSsr) ||
+      isSEOMode() ||
+      isEditMode() ||
+      isPreviewMode() ||
+      gotFirstScrollEvent ||
+      scrollY > 0 ||
       this.props.currentIdx > 0
     ) {
       return items;
