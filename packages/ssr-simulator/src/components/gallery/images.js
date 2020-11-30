0da506e274137bedada2355898e2c1a6fcc72b46
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2020-11-30 13:40:21 +0200

    [main+gallery+ssr-simulator] tests: configure tests to run different localhost for ssr-simulator

--- packages/ssr-simulator/src/components/gallery/images.js
+++ packages/ssr-simulator/src/components/gallery/images.js
@@ -3024,6 +3024,209 @@ const testVideos = [
   },
 ];
 
+const monochromeImages = [
+  {
+    itemId: "b1125adc-e169-41d1-95e6-898d314e5160",
+    metaData: {
+      height: 262,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_9c2cb153205c4f80b68525b9a23f2093~mv2.jpg",
+  },
+  {
+    itemId: "86f9f1f1-9297-417a-9f3d-165fd97c0a86",
+    metaData: {
+      height: 165,
+      width: 210,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_b5fa9b7e1dd14daba1a3efd3d4fc6610~mv2.jpg",
+  },
+  {
+    itemId: "1759fdd2-4960-4912-8a47-7258212c2079",
+    metaData: {
+      height: 141,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_1129aba81ff6469d817497dff8b389a6~mv2.jpg",
+  },
+  {
+    itemId: "6c2ee599-0c42-400d-9e61-43bfc113d564",
+    metaData: {
+      height: 200,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_bd32a3b80ac14407bae46c625bdca4bc~mv2.jpg",
+  },
+  {
+    itemId: "13c77804-ecb4-4a89-ac22-5d805673baab",
+    metaData: {
+      height: 87,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_651ac74c4af548a2b24c94f8fa3da8ed~mv2.jpg",
+  },
+  {
+    itemId: "9b91913e-764b-49f7-976a-22eda2a424f2",
+    metaData: {
+      height: 262,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_0efac50a0a704cb59c10174dac754d98~mv2.jpg",
+  },
+  {
+    itemId: "41310b3b-a8a2-45d3-a924-ddde1a22d34f",
+    metaData: {
+      height: 165,
+      width: 210,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_55eb39eccc044085ae0462a3e349ef1e~mv2.jpg",
+  },
+  {
+    itemId: "e6b9f247-3a5e-4f21-9fcd-d25008d1a0d8",
+    metaData: {
+      height: 87,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_4e81ef6811d443f29b5fd3c9f51de4a2~mv2.jpg",
+  },
+  {
+    itemId: "8ade484f-d55e-432b-8582-bd40dc779df7",
+    metaData: {
+      height: 141,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_dd4819fde6074c7eb4829d6cc55fef1a~mv2.jpg",
+  },
+  {
+    itemId: "f1c68420-afbd-47a9-a6c2-7fcfa3d84202",
+    metaData: {
+      height: 200,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_13863fc21ef94b3c91137d29fde38c15~mv2.jpg",
+  },
+  {
+    itemId: "b2b5e91b-e9d7-4e2b-9535-8eb9930d3070",
+    metaData: {
+      height: 262,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_23afa29547f540e4862eee64f4ef8389~mv2.jpg",
+  },
+  {
+    itemId: "8ec2470b-9aa5-4886-8c39-c2e6eb816657",
+    metaData: {
+      height: 165,
+      width: 210,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_0e647d5f8fe54cb3b2844e923e0ee190~mv2.jpg",
+  },
+  {
+    itemId: "2b388966-446f-4b5c-b026-c5aa717cc5ff",
+    metaData: {
+      height: 141,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_5dba255afc3b41a690792b17509f3386~mv2.jpg",
+  },
+  {
+    itemId: "9bea76a8-3034-4eee-8f5e-193532c71a85",
+    metaData: {
+      height: 87,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_0503c30583ad440982c9f4f359b4ef28~mv2.jpg",
+  },
+  {
+    itemId: "b3c41d54-0901-4afb-b4a5-c44868eb8976",
+    metaData: {
+      height: 200,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_40bce04edbc44922ae7981a7b7c2c5b4~mv2.jpg",
+  },
+  {
+    itemId: "7a6ddec8-fbce-486b-ada0-bf299e8df27e",
+    metaData: {
+      height: 262,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_f3fda1f1252c491cb15c008fffdb9ca4~mv2.jpg",
+  },
+  {
+    itemId: "8323cbcb-b98b-4885-a9e3-54bd8f99837f",
+    metaData: {
+      height: 165,
+      width: 210,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_c139e44218a848299d70657e786a80e4~mv2.jpg",
+  },
+  {
+    itemId: "558bb90f-b5f8-4fc9-b278-f6046d474b31",
+    metaData: {
+      height: 87,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_30cd827f829a4990bc56f0f0ad37d12c~mv2.jpg",
+  },
+  {
+    itemId: "b0c09def-d0c9-4bce-9dcf-88d9d1f27b3c",
+    metaData: {
+      height: 141,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_994233b50c62417a852185964788aab3~mv2.jpg",
+  },
+  {
+    itemId: "b8cffad0-9e45-40ae-b6bb-0c5691a32bc7",
+    metaData: {
+      height: 200,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_88a405fef79247798efda1a3ec14a062~mv2.jpg",
+  },
+  {
+    itemId: "aafa11ba-6210-4691-b78e-79c102c555ae",
+    metaData: {
+      height: 262,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_523117db617741c9a581d4fea639d315~mv2.jpg",
+  },
+  {
+    itemId: "4ee0934d-0f54-4e5a-a159-5d837c41ffa8",
+    metaData: {
+      height: 165,
+      width: 210,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_25e44fc0c2be4836b31aaf39cad8751e~mv2.jpg",
+  },
+  {
+    itemId: "afc8c1cc-80a4-4894-82f3-0560b9a388c7",
+    metaData: {
+      height: 200,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_40c00f18760043a79453270c87ac7010~mv2.jpg",
+  },
+  {
+    itemId: "175e5b97-6c1f-4f5e-b57d-a0a7c45b6ef6",
+    metaData: {
+      height: 87,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_040f554bab2f45f795201fb193da4735~mv2.jpg",
+  },
+  {
+    itemId: "62752b60-1b0d-432e-ae68-d5943e631eee",
+    metaData: {
+      height: 141,
+      width: 200,
+    },
+    mediaUrl: "https://static.wixstatic.com/media/0d72ac_8fc8ccb92a3b4301a77151202dccecf1~mv2.jpg",
+  }
+]
+
 const testItems = [...testImages, ...testVideos];
 
-export { testImages, testVideos, testItems };
+export { testImages, testVideos, testItems, monochromeImages };
