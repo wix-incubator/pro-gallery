0076f5a496e19ab22ddc9cdf5d325c40ab51014c
Yakov Haiilo <yakovh@wix.com>
2020-12-03 12:01:51 +0200

    [gallery] test: add e2e test to gallerySizeType SP

---
+++ packages/gallery/tests/e2e/styleParams/gallerySizeType.e2e.spec.js
@@ -0,0 +1,40 @@
+import GalleryDriver from '../../drivers/pptrDriver';
+import { toMatchImageSnapshot } from '../../drivers/matchers';
+import { GALLERY_CONSTS } from 'pro-gallery-lib';
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('gallerySizeType - e2e', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = new GalleryDriver();
+    await driver.openPage();
+  });
+
+  afterAll(async () => {
+    await driver.closePage();
+  });
+
+  it('when "gallerySizeType" set to "PIXELS" the width of each item should be close to the "gallerySizePx" destination', async () => {
+    await driver.navigate({
+      galleryLayout: GALLERY_CONSTS.layout.GRID,
+      gallerySizeType: GALLERY_CONSTS.gallerySizeType.PIXELS,
+      gallerySizePx: 600,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+
+  it('when "gallerySizeType" set to "RATIO" the width of each item should be relative to the gallery width. ("gallerySizeRatio" represents the target width of each item in percentage)', async () => {
+    await driver.navigate({
+      galleryLayout: GALLERY_CONSTS.layout.GRID,
+      gallerySizeType: GALLERY_CONSTS.gallerySizeType.RATIO,
+      gallerySizeRatio: 25,
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+});
