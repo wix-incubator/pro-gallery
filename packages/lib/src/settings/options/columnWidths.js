93aa1b38446b8fa00e4a79869ab80b998930b5a5
Guy Sopher <guyso@wix.com>
2020-12-06 11:32:05 +0200

    [layouts] feature: add (and fix) the columnsWidths feature to the playground

---
+++ packages/lib/src/settings/options/columnWidths.js
@@ -0,0 +1,9 @@
+import { INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Fixed Column Widths',
+  description: `Set the width of each column. The real width will be proportional to the width of the gallery`,
+  isRelevant: (styleParams) => styleParams.isVertical && styleParams.gridStyle === 1,
+  type: INPUT_TYPES.TEXT,
+  default: '',
+};
