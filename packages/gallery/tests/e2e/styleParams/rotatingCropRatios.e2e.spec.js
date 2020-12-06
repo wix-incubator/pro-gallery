d687e1e368c5455a45d8f031bc74cacc69720af5
Yakov Haiilo <yakovh@wix.com>
2020-12-06 17:10:50 +0200

    added e2e test to rotatingCropRatios SP

---
+++ packages/gallery/tests/e2e/styleParams/rotatingCropRatios.e2e.spec.js
@@ -0,0 +1,30 @@
+import { GALLERY_CONSTS } from 'pro-gallery-lib';
+import GalleryDriver from '../../drivers/pptrDriver';
+import { toMatchImageSnapshot } from '../../drivers/matchers';
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('rotatingCropRatios - e2e', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = new GalleryDriver();
+    await driver.openPage();
+  });
+
+  afterAll(async () => {
+    await driver.closePage();
+  });
+
+  it('should create a gallery with the correct types of crop ratios', async () => {
+    await driver.navigate({
+      galleryLayout: GALLERY_CONSTS.layout.GRID,
+      cubeImages: true,
+      rotatingCropRatios: '1,2,3',
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    await driver.waitFor.timer(200);
+    const page = await driver.grab.elemScreenshot('#pro-gallery-container');
+    expect(page).toMatchImageSnapshot();
+  });
+});
