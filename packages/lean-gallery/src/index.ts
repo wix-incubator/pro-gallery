83fe0e8888624d22433cdb2f71bf6ce55ed1062b
ido-postelnik <idop@wix.com>
2020-12-08 17:04:43 +0200

    [lean-gallery] Initial commit for new lean-gallery pacakge

---
+++ packages/lean-gallery/src/index.ts
@@ -0,0 +1,7 @@
+export { default as LeanGallery } from './leanGallery/leanGallery';
+
+export { formatLeanGalleryStyles } from './leanGallery/leanGallery';
+
+export { default as isEligibleForLeanGallery } from './leanGallery/isEligible';
+
+export { notEligibleReasons } from './leanGallery/isEligible';
