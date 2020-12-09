3ff621511a4d46de46e571853eb3e26291784193
ido-postelnik <idop@wix.com>
2020-12-09 14:39:43 +0200

    [main + gallery + playground + lean-gallery] Remove leanGallery usage and imports from "Gallery" package

--- packages/lean-gallery/src/index.ts
+++ packages/lean-gallery/src/index.ts
@@ -1,7 +1,4 @@
 export { default as LeanGallery } from './leanGallery/leanGallery';
-
 export { formatLeanGalleryStyles } from './leanGallery/leanGallery';
-
 export { default as isEligibleForLeanGallery } from './leanGallery/isEligible';
-
 export { notEligibleReasons } from './leanGallery/isEligible';
