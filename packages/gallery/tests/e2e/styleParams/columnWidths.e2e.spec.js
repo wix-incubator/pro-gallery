a53e9d671b49da7fc2cea8c02a9752f20a7c26ca
Yakov Haiilo <yakovh@wix.com>
2020-12-13 11:36:06 +0200

    [gallery] tests: added e2e test to columnWidths SP

---
+++ packages/gallery/tests/e2e/styleParams/columnWidths.e2e.spec.js
@@ -0,0 +1,28 @@
+import GalleryDriver from '../../drivers/pptrDriver';
+import { toMatchImageSnapshot } from '../../drivers/matchers';
+import { GALLERY_CONSTS } from 'pro-gallery-lib';
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('columnWidths - e2e', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = new GalleryDriver();
+    await driver.openPage();
+  });
+
+  afterAll(async () => {
+    await driver.closePage();
+  });
+  it('should set the width of each column according to the values in "columnWidths".(proportional to the width of the gallery)', async () => {
+    await driver.navigate({
+      galleryLayout: GALLERY_CONSTS.layout.GRID,
+      gridStyle: GALLERY_CONSTS.gridStyle.SET_ITEMS_PER_ROW,
+      columnWidths: '1,2,4',
+    });
+    await driver.waitFor.hookToBeVisible('item-container');
+    const page = await driver.grab.elemScreenshot('.pro-gallery');
+    expect(page).toMatchImageSnapshot();
+  });
+});
