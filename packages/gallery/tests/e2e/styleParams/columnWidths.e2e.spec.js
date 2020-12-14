866d0a7998ec45ea7152ed89b126d17e460e11e4
Yakov Haiilo <yakovh@wix.com>
2020-12-14 17:50:10 +0200

    ״

--- packages/gallery/tests/e2e/styleParams/columnWidths.e2e.spec.js
+++ packages/gallery/tests/e2e/styleParams/columnWidths.e2e.spec.js
@@ -15,11 +15,12 @@ describe('columnWidths - e2e', () => {
   afterAll(async () => {
     await driver.closePage();
   });
-  it('should set the width of each column according to the values in "columnWidths".(proportional to the width of the gallery)', async () => {
+  it('should set the width of each column according to the values in "columnWidths" in repeating order', async () => {
     await driver.navigate({
       galleryLayout: GALLERY_CONSTS.layout.GRID,
       gridStyle: GALLERY_CONSTS.gridStyle.SET_ITEMS_PER_ROW,
       columnWidths: '1,2,4',
+      numberOfImagesPerRow: 4,
     });
     await driver.waitFor.hookToBeVisible('item-container');
     const page = await driver.grab.elemScreenshot('.pro-gallery');
