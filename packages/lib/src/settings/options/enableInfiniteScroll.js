3c60b8e61c53ec3d22c7e55dbe3dc471d1344241
Yakov Haiilo <yakovh@wix.com>
2020-12-17 11:42:59 +0200

    [playground + lib] improve: change allowInfiniteScroll to enableLoadMoreButton

--- packages/lib/src/settings/options/enableInfiniteScroll.js
+++ packages/lib/src/settings/options/enableInfiniteScroll.js
@@ -1,8 +1,8 @@
 import { INPUT_TYPES } from '../utils/constants';
 
 export default {
-  title: 'Use Infinite Scroll',
-  description: `Choose Whether you want to have infinite scroll option or not. When this option is set to false the gallery will have
+  title: 'Enable load more button',
+  description: `Choose Whether you want to have infinite scroll option or not. When this option is set to true the gallery will have
   a "Load More". Note that the consumer will have to add the new items when the gallery requests more ("NEED_MORE_ITEMS" event will be emited).`,
   isRelevant: (styleParams) => !styleParams.oneRow,
   type: INPUT_TYPES.BOOLEAN,
