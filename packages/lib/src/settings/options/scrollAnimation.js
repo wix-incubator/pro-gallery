279d720480139182d86951ccd5af7193f0fefc89
Yakov Haiilo <yakovh@wix.com>
2020-12-24 13:31:54 +0200

    edited scroll Animation description

--- packages/lib/src/settings/options/scrollAnimation.js
+++ packages/lib/src/settings/options/scrollAnimation.js
@@ -6,7 +6,7 @@ export default {
   title: 'Scroll Animation',
   description: `Choose the type of animation to be used when items appear while scrolling verticaly through the gallery`,
   isRelevantDescription:
-    'Set a Vertical gallery ("Scroll Direction" as "Vertical") or set a Horizontal gallery ("Scroll Direction" as "Horizontal") and "Slide Animation" to "Scroll".',
+    'To enable "Scroll Animation" either set a Vertical gallery ("Scroll Direction" as "Vertical")\nor set a Horizontal gallery ("Scroll Direction" as "Horizontal") with "Slide Animation" set to "Scroll".',
   isRelevant: (sp) =>
     !sp.oneRow ||
     (sp.oneRow && sp.slideAnimation === GALLERY_CONSTS.slideAnimations.SCROLL),
