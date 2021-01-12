0651bd00620d63d3f0efb7f95ae1c51f45c9d571
ido-postelnik <idop@wix.com>
2021-01-12 12:51:28 +0200

    [pgl] Update DESIGNED_PRESET_3

--- packages/lib/src/core/presets/designedPresets/designedPreset_3.js
+++ packages/lib/src/core/presets/designedPresets/designedPreset_3.js
@@ -3,14 +3,11 @@ import { calcTargetItemSize } from '../../helpers/layoutHelper';
 
 export const fixedStyles = {
   galleryLayout: LAYOUTS.DESIGNED_PRESET_3,
-  rotatingGroupTypes: '2h',
+  groupSize: 1,
   cubeImages: true,
-  cubeRatio: '50%/100%',
-  smartCrop: true,
-  rotatingCropRatios: '3:4,4:3',
-  imageMargin: 50,
-  galleryMargin: 50,
-  gridStyle: 1,
+  cubeRatio: '100%/50%',
+  rotatingCropRatios: '25%/100%,50%/100%',
+  scrollAnimation: 'FADE_IN',
   scrollDirection: 1,
 };
 
