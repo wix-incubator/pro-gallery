19c90adb58b336348637d73a1d1d8beeece0c09d
HanWax <hannahw@wix.com>
2021-01-27 20:21:57 +0200

    Clear form fields after submission

--- packages/playground/src/components/SideBar/community.js
+++ packages/playground/src/components/SideBar/community.js
@@ -16,14 +16,14 @@ function Community() {
                 <h3>Submit your layout to the community</h3>
                 <Form layout="vertical">
                     <Form.Item label="Title" labelAlign="left">
-                        <Input onChange={e => set_layoutItem({...layoutItem, title: e.target.value})}/>
+                        <Input value={layoutItem.title} onChange={e => set_layoutItem({...layoutItem, title: e.target.value})}/>
                     </Form.Item>
                     <Form.Item label="Description" labelAlign="left" help="describe your layout">
-                        <Input.TextArea onChange={e => set_layoutItem({...layoutItem, description: e.target.value})}/>
+                        <Input.TextArea value={layoutItem.description} onChange={e => set_layoutItem({...layoutItem, description: e.target.value})}/>
                     </Form.Item>
                     <Form.Item label="Tags" labelAlign="left"
                                help="tag your beautiful layout so you can inspire others. Commas separated values, please.">
-                        <Input onChange={e => set_layoutItem({
+                        <Input value={layoutItem.tags} onChange={e => set_layoutItem({
                             ...layoutItem,
                             tags: e.target.value
                         })}/>
