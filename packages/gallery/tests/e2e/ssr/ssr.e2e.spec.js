ab53ec805940003d74a61916ff8d6642033d3372
Yonatan Hattav <yhattav@gmail.com>
2021-02-08 11:05:11 +0200

    linter wow..

--- packages/gallery/tests/e2e/ssr/ssr.e2e.spec.js
+++ packages/gallery/tests/e2e/ssr/ssr.e2e.spec.js
@@ -15,7 +15,7 @@ describe('ssr - e2e', () => {
     await driver.closePage();
   });
   it('alternate layout - scrollDirection = vertical', async () => {
-    await driver.navigate({disableSSROpacity: true });
+    await driver.navigate({ disableSSROpacity: true });
     await driver.waitFor.hookToBeVisible('item-container');
     await driver.waitFor.timer(10000);
     const page = await driver.grab.partialScreenshot();
