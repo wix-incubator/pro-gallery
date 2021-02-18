c2379b36dc5ea2dfd02647206c39aee5c77be7ba
ritabazak <rita.bazak@gmail.com>
2021-02-18 12:14:13 +0200

    [gallery] added ItemView to index.ts exports

--- packages/gallery/src/index.ts
+++ packages/gallery/src/index.ts
@@ -2,6 +2,7 @@ export { default as ProGallery } from './components/gallery/index';
 export { default as ProGalleryRenderer } from './components/gallery/rendererIndex';
 export { cssScrollHelper } from './components/helpers/cssScrollHelper';
 export { default as addLayoutStyles } from './components/helpers/layoutHelper';
+export { default as ItemView } from './components/item/itemView';
 
 export { GALLERY_CONSTS } from 'pro-gallery-lib';
 export { addPresetStyles } from 'pro-gallery-lib';
