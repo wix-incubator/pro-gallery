db6700400aa9516f024c9e6c14f5f5d3254f3e51
Guy Sopher <guyso@wix.com>
2021-03-14 15:56:58 +0200

    [gallery] fix (imageRenderer): use img tag when only one src is available

--- packages/gallery/src/components/item/imageRenderer.js
+++ packages/gallery/src/components/item/imageRenderer.js
@@ -4,14 +4,13 @@ const ImageRenderer = (props) => {
   if (typeof ImageRenderer.customImageRenderer === 'function') {
     return ImageRenderer.customImageRenderer(props);
   } else if (typeof props.src === 'string') {
-    return (
-      <picture id={`picture_${props.id}`} key={`picture_${props.id}`}>
-        <img alt={props.alt} {...props} />
-      </picture>
-    );
+    return <img alt={props.alt} {...props} />;
   } else if (typeof props.src === 'object') {
     return (
-      <picture id={`multi_picture_${props.id}`} key={`multi_picture_${props.id}`}>
+      <picture
+        id={`multi_picture_${props.id}`}
+        key={`multi_picture_${props.id}`}
+      >
         {props.src.map((src) => (
           <source srcSet={src.dpr || src.url} type={`image/${src.type}`} />
         ))}
