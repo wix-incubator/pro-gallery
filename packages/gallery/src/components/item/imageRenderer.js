a83b8d33866abb3061ea8e1392f7fc3c3e844d85
Guy Sopher <guyso@wix.com>
2021-03-15 11:59:38 +0200

    [gallery] improve (imageRenderer): add a multi url picture element support

--- packages/gallery/src/components/item/imageRenderer.js
+++ packages/gallery/src/components/item/imageRenderer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const ImageRenderer = (imageProps) => {
+const ImageRenderer = (props) => {
   if (typeof ImageRenderer.customImageRenderer === 'function') {
-    return ImageRenderer.customImageRenderer(imageProps);
-  } else {
-    return <img {...imageProps} />;
+    return ImageRenderer.customImageRenderer(props);
+  } else if (typeof props.src === 'string') {
+    return <img alt={props.alt} {...props} />;
+  } else if (typeof props.src === 'object') {
+    return (
+      <picture
+        id={`multi_picture_${props.id}`}
+        key={`multi_picture_${props.id}`}
+      >
+        {props.src.map((src) => (
+          <source srcSet={src.dpr || src.url} type={`image/${src.type}`} />
+        ))}
+        <img alt={props.alt} {...props} src={props.src[0].url} />
+      </picture>
+    );
   }
 };
 
