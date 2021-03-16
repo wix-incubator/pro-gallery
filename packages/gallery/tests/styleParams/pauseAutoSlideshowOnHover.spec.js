074c9235f34bbd05e6f8c3a6c5fe18d5f7007c43
Yakov Haiilo <yakovh@wix.com>
2021-03-16 19:51:49 +0200

    test refactor

--- packages/gallery/tests/styleParams/pauseAutoSlideshowOnHover.spec.js
+++ packages/gallery/tests/styleParams/pauseAutoSlideshowOnHover.spec.js
@@ -13,47 +13,26 @@ describe('styleParam - pauseAutoSlideshowOnHover', () => {
   beforeEach(() => {
     driver = new GalleryDriver();
     initialProps = driver.props.galleryView();
-    Object.assign(initialProps.styleParams, {
-      oneRow: true,
-      isAutoSlideshow: true,
-      autoSlideshowInterval: 1,
-      galleryLayout: GALLERY_CONSTS.layout.SLIDESHOW,
-    });
     clock = sinon.useFakeTimers();
   });
 
   afterEach(() => {
     clock.restore();
   });
-  it('startAutoSlideshow should not be called when Hovering over the gallery', () => {
+  it('shuld not auto slide when Hovering over the gallery', () => {
     Object.assign(initialProps.styleParams, {
+      oneRow: true,
+      isAutoSlideshow: true,
+      autoSlideshowInterval: 1,
+      galleryLayout: GALLERY_CONSTS.layout.SLIDESHOW,
       pauseAutoSlideshowOnHover: true,
     });
     galleryViewProps = driver.props.galleryView(initialProps);
     const stub = sinon.stub(SlideshowView.prototype, 'next');
     driver.mount(SlideshowView, galleryViewProps);
     driver.set.props({ isGalleryContainerInHover: true });
+    clock.tick(1200);
     expect(stub.called).to.equal(false);
-    clock.tick(900);
-    expect(stub.called).to.equal(false);
-    clock.tick(300);
-    expect(stub.called).to.equal(false);
-    stub.restore();
-  });
-
-  it('startAutoSlideshow should be called when the gallery is not hovering', () => {
-    Object.assign(initialProps.styleParams, {
-      pauseAutoSlideshowOnHover: true,
-    });
-    galleryViewProps = driver.props.galleryView(initialProps);
-    const stub = sinon.stub(SlideshowView.prototype, 'next');
-    driver.mount(SlideshowView, galleryViewProps);
-    driver.set.props({ isGalleryContainerInHover: false });
-    expect(stub.called).to.equal(false);
-    clock.tick(900);
-    expect(stub.called).to.equal(false);
-    clock.tick(300);
-    expect(stub.called).to.equal(true);
     stub.restore();
   });
 });
