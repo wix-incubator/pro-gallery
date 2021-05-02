e51945e211db6df54136b9d7604553cb1abde039
izaacayelin <izaaca@wix.com>
2021-05-02 16:42:49 +0300

    [gallery] improved isRelevat function

--- packages/lib/src/settings/options/shouldIndexDirectShareLinkInSEO.js
+++ packages/lib/src/settings/options/shouldIndexDirectShareLinkInSEO.js
@@ -5,8 +5,10 @@ export default {
   title: 'Search engines indexing for direct share links',
   description: `Choose whether you want the items with share link to be indexed by crawlers in SEO or not`,
   isRelevant: (sp) =>
-    sp.itemClick === GALLERY_CONSTS.itemClick.FULLSCREEN ||
-    sp.itemClick === GALLERY_CONSTS.itemClick.EXPAND,
+    [
+      GALLERY_CONSTS.itemClick.FULLSCREEN,
+      GALLERY_CONSTS.itemClick.EXPAND,
+    ].includes(sp.itemClick),
   isRelevantDescription:
     'Click Action (itemClick) must be set to "fullscreen" or "expand"',
   type: INPUT_TYPES.BOOLEAN,
