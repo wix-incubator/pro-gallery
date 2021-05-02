cd1ea3bbfb07e737f13ae379f9c1642a16a2f9fb
yakovhaiilo <57101084+yakovhaiilo@users.noreply.github.com>
2021-05-02 17:08:15 +0300

    [gallery] improve: changed 'ITEM_FOCUS' and 'ITEM_LOST_FOCUS'  parameters

--- packages/gallery/src/components/item/itemView.js
+++ packages/gallery/src/components/item/itemView.js
@@ -118,17 +118,20 @@ class ItemView extends GalleryComponent {
         this.props.idx
       );
     }
-    this.props.actions.eventsListener(GALLERY_CONSTS.events.ITEM_FOCUSED, {
-      styleParams: this.props.styleParams,
-      itemStyle: this.props.style,
-    });
+    this.props.actions.eventsListener(
+      GALLERY_CONSTS.events.ITEM_FOCUSED,
+      this.props
+    );
   }
 
   onBlur() {
     if (this.props.styleParams.isAccessible) {
       this.props.actions.eventsListener(GALLERY_CONSTS.events.HOVER_SET, -1);
     }
-    this.props.actions.eventsListener(GALLERY_CONSTS.events.ITEM_LOST_FOCUS);
+    this.props.actions.eventsListener(
+      GALLERY_CONSTS.events.ITEM_LOST_FOCUS,
+      this.props
+    );
   }
 
   onContainerKeyDown(e) {
