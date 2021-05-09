2e124c24fd7e333bc85be8d3a986a0e9169d8a79
Nir Naor <nirnaori@gmail.com>
2021-05-09 10:33:24 +0300

    Docs - Copy pasted code now works out of the box (#309)
    * Using real items avaiable online.
    
    * new readme

--- packages/playground/src/components/CodePanel/CodePanel.js
+++ packages/playground/src/components/CodePanel/CodePanel.js
@@ -73,7 +73,54 @@ function getCode(options) {
   export function Gallery() {
 
     // Add your images here...
-    const items = [];
+    const items = [
+            { // Image item:
+                    itemId: 'sample-id',
+                    mediaUrl: 'https://i.picsum.photos/id/674/200/300.jpg?hmac=kS3VQkm7AuZdYJGUABZGmnNj_3KtZ6Twgb5Qb9ITssY',
+                    metaData: {
+                            type: 'image',
+                            height: 200,
+                            width: 100,
+                            title: 'sample-title',
+                            description: 'sample-description',
+                            focalPoint: [0, 0],
+                            link: {
+                                    url: 'http://example.com',
+                                    target: '_blank'
+                            },
+                    }
+            },
+            { // Another Image item:
+                    itemId: 'differentItem',
+                    mediaUrl: 'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk',
+                    metaData: {
+                            type: 'image',
+                            height: 200,
+                            width: 100,
+                            title: 'sample-title',
+                            description: 'sample-description',
+                            focalPoint: [0, 0],
+                            link: {
+                                    url: 'http://example.com',
+                                    target: '_blank'
+                            },
+                    }
+            },
+            { // HTML item:
+                    itemId: 'htmlItem',
+                    html: "<div style='width: 300px; height: 200px; background:pink;'>I am a text block</div>",
+                    metadata: {
+                            type: "text",
+                            height: 200,
+                            width: 300,
+                            title: 'sample-title',
+                            description: 'sample-description',
+                            backgroundColor: 'pink'
+                    },
+
+            },
+    ]
+
 
     // The options of the gallery (from the playground current state)
     const options = {
