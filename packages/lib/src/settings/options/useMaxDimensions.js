489978fc67ac20eca38612b1fb4678d22d44e0ee
izaacayelin <izaaca@wix.com>
2021-06-13 10:28:33 +0300

    fix lint errors

--- packages/lib/src/settings/options/useMaxDimensions.js
+++ packages/lib/src/settings/options/useMaxDimensions.js
@@ -1,11 +1,10 @@
 import { INPUT_TYPES } from '../utils/constants';
-import { default as GALLERY_CONSTS } from '../../common/constants';
 
 export default {
   title: 'Use max dimensions if needed',
   description: `The gallery will strech small images to fit the item's container, when set to true
   the gallery will use the original image dimensions if it is too small for the container`,
-  isRelevant: (styleParams) => true,
+  isRelevant: () => true,
   isRelevantDescription: 'Always true',
   type: INPUT_TYPES.BOOLEAN,
   default: false,
