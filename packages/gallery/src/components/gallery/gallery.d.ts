433b179c810f3a5cb3ec0ef1874e39991910e282
Nir Naor <nirnaori@gmail.com>
2021-06-29 16:25:35 +0300

    Adding styleParams to interfaces file.

--- packages/gallery/src/components/gallery/gallery.d.ts
+++ packages/gallery/src/components/gallery/gallery.d.ts
@@ -7,3 +7,17 @@ export interface GalleryProps {
 export interface GalleryState {
   blueprint: any;
 }
+
+export type PlayOn = "hover" | "auto" | "onClick"
+export interface Video  {
+    playOn: PlayOn
+}
+
+export interface Item {
+    video: Video
+}
+
+export interface StyleParams {
+    item : Item
+}
+
