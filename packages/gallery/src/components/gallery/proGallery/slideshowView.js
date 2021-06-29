03e12ea83bc35c8b9e61f0535e284fa88b2b290b
Yakov Haiilo <yakovh@wix.com>
2021-06-29 15:50:27 +0300

    Increasing removeArrowsIfNeeded call

--- packages/gallery/src/components/gallery/proGallery/slideshowView.js
+++ packages/gallery/src/components/gallery/proGallery/slideshowView.js
@@ -1443,7 +1443,7 @@ class SlideshowView extends GalleryComponent {
           hideRightArrow: !!nextHideRight,
         });
       }
-    }, 50);
+    }, 100);
   }
   navigationOutHandler() {
     //TODO remove after full refactor release
