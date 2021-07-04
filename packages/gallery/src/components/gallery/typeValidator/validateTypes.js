7cf82c3a63d450478656f2b0da51189d7d2c306a
Nir Naor <nirnaori@gmail.com>
2021-07-04 14:38:43 +0300

    fixed my first type error

--- packages/gallery/src/components/gallery/typeValidator/validateTypes.js
+++ packages/gallery/src/components/gallery/typeValidator/validateTypes.js
@@ -6,8 +6,8 @@ import typeErrorsUI from './typeErrorsUI';
 function validate(data) {
   const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
   const validate = ajv.compile(schema);
-  const valid = validate(data);
-  return valid ? [] : validate.errors;
+  validate(data);
+  return validate.errors || [];
 }
 
 export default { validate, typeErrorsUI };
