0296bb71f27c3a3fb119539e89384812805eba1b
Nir Naor <nirnaori@gmail.com>
2021-07-05 10:40:10 +0300

    wip

--- packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
+++ packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
@@ -3,8 +3,9 @@ import Ajv from 'ajv';
 import schema from './schema';
 
 import typeErrorsUI from './typeErrorsUI';
+import { StyleParams } from '../gallery.d';
 
-function validate(data: any) {
+function validate(data: StyleParams) {
   const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
   const validateFunc = ajv.compile(schema);
   validateFunc(data);
