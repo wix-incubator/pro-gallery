b73b7a04c73cfce3ac63d7de4b24a24ff65d62eb
Nir Naor <nirnaori@gmail.com>
2021-07-21 20:25:54 +0300

    chagne production code

--- packages/gallery/src/components/gallery/index.tsx
+++ packages/gallery/src/components/gallery/index.tsx
@@ -90,7 +90,7 @@ export default class BaseGallery extends React.Component<
   }
 
   render() {
-    console.log('Change production code3');
+    console.log('Change production code5');
     const { blueprint, typeErrors } = this.state;
 
     if (typeErrors) {
