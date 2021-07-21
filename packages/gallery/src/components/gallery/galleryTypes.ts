4dc51ab7efcffe1914bec647ff4a3f6087579e70
ritabazak <rita.bazak@gmail.com>
2021-07-21 20:24:32 +0300

    fix

--- packages/gallery/src/components/gallery/galleryTypes.ts
+++ packages/gallery/src/components/gallery/galleryTypes.ts
@@ -1,16 +1,16 @@
 import { PhotoItem, VideoItem, TextItem } from '../../common/interfaces/Item';
 
 type ViewMode = 'SITE' | 'EDIT' | 'PREVIEW' | 'SEO';
-type DeviceType = 'desktop' | 'mobile' | 'tablet';
+type FormFactor = 'desktop' | 'mobile' | 'tablet';
 
 export interface GalleryProps {
   id?: string;
   totalItemsCount?: number;
   items: (PhotoItem | VideoItem | TextItem)[];
-  activeIndex?: number;
+  currentIdx?: number;
   isPrerenderMode?: boolean;
   viewMode?: ViewMode;
-  deviceType?: DeviceType;
+  formFactor?: FormFactor;
   [key: string]: any;
 }
 
