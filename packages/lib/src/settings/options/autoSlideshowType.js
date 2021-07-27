8114989fe72cb2f82c72fc7a8f52782f1965277d
Yakov Haiilo <yakovh@wix.com>
2021-07-27 12:33:56 +0300

    improve autoSlideshowType

--- packages/lib/src/settings/options/autoSlideshowType.js
+++ packages/lib/src/settings/options/autoSlideshowType.js
@@ -6,11 +6,12 @@ export default {
   title: 'Auto slide Behaviour',
   isRelevant: (sp) =>
     sp.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL &&
+    sp.groupSize === 1 &&
     sp.isAutoSlideshow,
   isRelevantDescription:
     'Set a Horizontal gallery ("Scroll Direction" as "Horizontal") and set "Auto Slide" to "true".',
   type: INPUT_TYPES.OPTIONS,
-  default: GALLERY_CONSTS.autoSlideshowType.INTERVAL,
+  default: GALLERY_CONSTS.autoSlideshowTypes.INTERVAL,
   options: createOptions('autoSlideshowTypes'),
   description: `Choose the type of auto-scrolling to be used when navigation between items, "interval" (time between navigation) or "continues" (Continuous scrolling).`,
 };
