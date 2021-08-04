bb279814cc12678de956f24ba51b7404e84f7cf7
Yonatan Hattav <yhattav@gmail.com>
2021-08-04 11:53:37 +0300

    remove the functions from the lib and add them in the playground

--- packages/playground/src/constants/styleParams.js
+++ packages/playground/src/constants/styleParams.js
@@ -4,8 +4,6 @@ import {
   NEW_PRESETS,
   defaultStyles,
   galleryOptions,
-  flattenObject,
-  assignByString,
 } from 'pro-gallery-lib';
 
 const defaultStyleParams = defaultStyles;
@@ -96,6 +94,44 @@ export const getStyleParamsFromUrl = () => {
     return {};
   }
 };
+function assignByString(
+  Obj,
+  string,
+  value,
+) {
+  let _obj = { ...Obj };
+  let keyArr = string.split('_');
+  let assignedProperty = keyArr.pop();
+  let pointer = _obj;
+  keyArr.forEach((key) => {
+    if (!pointer[key]) pointer[key] = {};
+    pointer = pointer[key];
+  });
+  pointer[assignedProperty] = value;
+  return _obj;
+}
+
+function flattenObject(ob) {
+  var toReturn = {};
+
+  for (var i in ob) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!ob.hasOwnProperty(i)) continue;
+
+    if (typeof ob[i] == 'object' && ob[i] !== null) {
+      var flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (!flatObject.hasOwnProperty(x)) continue;
+
+        toReturn[i + '.' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+}
 
 export const setStyleParamsInUrl = (styleParams) => {
   const flatSP = flattenObject(styleParams);
