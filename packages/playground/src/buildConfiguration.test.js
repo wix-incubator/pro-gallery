823ac638a9711e2dd2230a498318981f75c4308d
Nir Naor <nirnaori@gmail.com>
2021-08-08 15:18:37 +0300

    testing lerna version

--- packages/playground/src/buildConfiguration.test.js
+++ packages/playground/src/buildConfiguration.test.js
@@ -1,14 +1,17 @@
 import fs from 'fs'
 import path from 'path'
+
 it('should make sure that only lerna manages the internal version numbers (relevant for lerna Fixed/Locked mode)', () => {
+  const getVersion = f => JSON.parse(fs.readFileSync(f)).version
   const pacakgesDir = path.join(__dirname, '../../')
+
   const uniqVersionNumbers = fs
     .readdirSync(pacakgesDir)
-    .map(p => path.join(pacakgesDir, p))
-    .map(p => path.join(p, 'package.json'))
-    .map(f => fs.readFileSync(f))
-    .map(JSON.parse)
-    .map(j => j.version)
+    .map(p => path.join(pacakgesDir, p, 'package.json'))
+    .map(getVersion)
     .reduce((s, v) => s.add(v), new Set())
+
   expect(uniqVersionNumbers.size).toEqual(1)
+  expect(uniqVersionNumbers).toContain(getVersion(path.join(pacakgesDir, '../lerna.json')))
 })
+
