8d0e6229ec35f6e07a541975af7c63405d2b79e4
Nir Naor <nirnaori@gmail.com>
2021-08-08 16:38:45 +0300

    Testing all relevant dependencies versions.

--- packages/playground/src/buildConfiguration.test.js
+++ packages/playground/src/buildConfiguration.test.js
@@ -1,17 +1,38 @@
 import fs from 'fs'
 import path from 'path'
 
-it('should make sure that only lerna manages the internal version numbers (relevant for lerna Fixed/Locked mode)', () => {
-  const getVersion = f => JSON.parse(fs.readFileSync(f)).version
-  const pacakgesDir = path.join(__dirname, '../../')
+const json = f => JSON.parse(fs.readFileSync(f))
+const pacakgesDir = path.join(__dirname, '../../')
+const pJsonPath = p => path.join(pacakgesDir, p, 'package.json')
+const getVersion = f => json(f).version
+const lernaVersion = getVersion(path.join(pacakgesDir, '../lerna.json'))
+const packageFolderNames = fs.readdirSync(pacakgesDir)
 
-  const uniqVersionNumbers = fs
-    .readdirSync(pacakgesDir)
-    .map(p => path.join(pacakgesDir, p, 'package.json'))
+it('should make sure that only lerna manages the internal version numbers (relevant for lerna Fixed/Locked mode)', () => {
+  const uniqVersionNumbers = packageFolderNames
+    .map(pJsonPath)
     .map(getVersion)
     .reduce((s, v) => s.add(v), new Set())
 
   expect(uniqVersionNumbers.size).toEqual(1)
-  expect(uniqVersionNumbers).toContain(getVersion(path.join(pacakgesDir, '../lerna.json')))
+  expect(uniqVersionNumbers).toContain(lernaVersion)
 })
 
+it('should make sure versions are consumed correctly by dependants', () => {
+  const packageNames = packageFolderNames
+    .map(pJsonPath)
+    .map(json)
+    .map(x => x.name)
+
+  packageFolderNames
+    .map(pJsonPath)
+    .map(json)
+    .forEach(assertJsonConsumingMonoRepoDepsCorrectly)
+
+  function assertJsonConsumingMonoRepoDepsCorrectly(json) {
+    Object
+      .entries(json.dependencies || {})
+      .filter(e => packageNames.includes(e[0]))
+      .forEach(e => expect(e[1]).toMatch(lernaVersion))
+  }
+})
