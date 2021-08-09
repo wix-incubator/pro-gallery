42c111e20b78f488868043dc05808860164c011c
Yakov Haiilo <yakovh@wix.com>
2021-08-09 13:18:56 +0300

    refactor

--- packages/gallery/src/components/helpers/scrollHelper.js
+++ packages/gallery/src/components/helpers/scrollHelper.js
@@ -57,11 +57,7 @@ export function scrollToItemImp(scrollParams) {
     if (scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL) {
       //set scroll to place the item in the middle of the component
       const diff = (galleryWidth - item.width - imageMargin) / 2;
-      if (diff > 0) {
-        to -= diff;
-      } else if (diff < 0) {
-        to += diff * -1;
-      }
+      to -= diff;
       to = Math.max(0, to);
       to = Math.min(to, totalWidth - galleryWidth + scrollMarginCorrection);
       to *= rtlFix;
@@ -146,11 +142,7 @@ export function scrollToGroupImp(scrollParams) {
     if (scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL) {
       //set scroll to place the group in the middle of the component
       const diff = (galleryWidth - group.width - imageMargin) / 2;
-      if (diff > 0) {
-        to -= diff;
-      } else if (diff < 0) {
-        to += diff * -1;
-      }
+      to -= diff;
       to = Math.max(0, to);
       to = Math.min(to, totalWidth - galleryWidth + scrollMarginCorrection);
       to *= rtlFix;
