e8500f2b265988b6efd2203cf0c20219a288efbc
Nir Naor <nirnaori@gmail.com>
2021-08-09 14:28:55 +0300

    Adding test.

---
+++ packages/blueprints/test/blueprints.spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+import {BlueprintsManager} from '../src/index'
+it('should create a blueprints manager', () => {
+	const id = 'my id'
+	const bpm = new BlueprintsManager({id});
+	expect(bpm.id).equal(`${id}'s blueprintsManager`)
+})
