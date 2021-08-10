fcd7f7fab8860b48893fd1d548851a5263208c75
Yonatan Hattav <yhattav@gmail.com>
2021-08-10 12:06:08 +0300

    [playground/lib] Added stylesUtils and flatToNested functionality (#468)

---
+++ packages/lib/src/core/helpers/stylesUtils.js
@@ -0,0 +1,43 @@
+function assignByString(Obj, string, value) {
+  let _obj = { ...Obj };
+  let keyArr = string.split('_');
+  let assignedProperty = keyArr.pop();
+  let pointer = _obj;
+  keyArr.forEach((key) => {
+    if (!pointer[key]) pointer[key] = {};
+    pointer = pointer[key];
+  });
+  pointer[assignedProperty] = value;
+  return _obj;
+}
+
+function flattenObject(ob) {
+  var toReturn = {};
+
+  for (var i in ob) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!ob.hasOwnProperty(i)) continue;
+
+    if (typeof ob[i] == 'object' && ob[i] !== null) {
+      var flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (!flatObject.hasOwnProperty(x)) continue;
+
+        toReturn[i + '_' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+}
+
+function flatToNested(ob) {
+  return Object.entries(ob).reduce(
+    (obj, [styleParam, value]) => assignByString(obj, styleParam, value),
+    {}
+  );
+}
+
+export { flattenObject, assignByString, flatToNested };
