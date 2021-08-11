d2c423d58671ffea63dd0bf9f1627d77a909cdc2
Nir Naor <nirnaori@gmail.com>
2021-08-11 12:10:42 +0300

    Adding missing proGallery.

--- packages/gallery/src/components/gallery/index.tsx
+++ packages/gallery/src/components/gallery/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BlueprintsManager, GALLERY_CONSTS, utils } from 'pro-gallery-lib';
+import ProGallery from './proGallery/proGallery';
 // import { Dimensions } from '../../common/interfaces/Dimensions';
 
 import { GalleryProps, GalleryState } from './galleryTypes';
