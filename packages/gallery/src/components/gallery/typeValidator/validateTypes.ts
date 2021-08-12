2e2c5160e529b9af021ce68745985dc3c759cf20
Nir Naor <nirnaori@gmail.com>
2021-08-12 13:08:17 +0300

    lint and ignore generated file.

--- packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
+++ packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
@@ -1,6 +1,6 @@
 import typeErrorsUI from './typeErrorsUI';
 import { StyleParams } from '../styles.d';
-const validateFunc = require('./standaloneValidateCode')
+const validateFunc = require('./standaloneValidateCode');
 
 function validate(data?: StyleParams) {
   validateFunc(data);
