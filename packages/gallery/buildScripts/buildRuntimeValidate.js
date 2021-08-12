ca743c348fe6a50c040f924af521e90f31705c1d
Nir Naor <nirnaori@gmail.com>
2021-08-12 12:20:35 +0300

    Using the standalone code.

--- packages/gallery/buildScripts/buildRuntimeValidate.js
+++ packages/gallery/buildScripts/buildRuntimeValidate.js
@@ -1,47 +1,56 @@
+const path = require("path");
+const fs = require("fs");
 const Ajv = require("ajv") // version >= v7.0.0
 // import Ajv from "ajv" // version >= v7.0.0
 const ajv = new Ajv({messages: true, verbose: true, code: {source: true}}) // this option is required to generate standalone code
 const standaloneCode = require("ajv/dist/standalone").default
 
-const schema = {
-  $id: "https://example.com/object.json",
-  type: "object",
-  properties: {
-    foo: {
-      type: "string",
-      pattern: "^[a-z]+$",
-    },
-  },
+// const schema = {
+//   $id: "https://example.com/object.json",
+//   type: "object",
+//   properties: {
+//     foo: {
+//       type: "string",
+//       pattern: "^[a-z]+$",
+//     },
+//   },
+// }
+const getSchema = require('./generateJSONSchemaFromTypes')
+function buildValidationFunction(schema) {
+  ajv.addSchema(schema, 'testSchema')
+
+  // 1. generate module with a single default export (CommonJS and ESM compatible):
+  const validate = ajv.compile(schema, 'testSchema')
+  ajv.errorsText(validate.errors)
+
+  let moduleCode = standaloneCode(ajv, validate)
+  return moduleCode
+
+  // 2. pass map of schema IDs to generate multiple exports,
+  // it avoids code duplication if schemas are mutually recursive or have some share elements:
+  // let moduleCode = standaloneCode(ajv, {
+  //   validateObject: "https://example.com/object.json",
+  // })
+
+  // // 3. or generate module with all schemas added to the instance (excluding meta-schemas),
+  // // export names would use schema IDs (or keys passed to addSchema method):
+  // let moduleCode = standaloneCode(ajv)
+
+  // now you can
+  // write module code to file
+  // const fs = require("fs")
+
+  // ... or require module from string
 }
-ajv.addSchema(schema, 'testSchema')
-
-// 1. generate module with a single default export (CommonJS and ESM compatible):
-const validate = ajv.compile(schema, 'testSchema')
-ajv.errorsText(validate.errors)
-
-let moduleCode = standaloneCode(ajv, validate)
-
-// 2. pass map of schema IDs to generate multiple exports,
-// it avoids code duplication if schemas are mutually recursive or have some share elements:
-// let moduleCode = standaloneCode(ajv, {
-//   validateObject: "https://example.com/object.json",
-// })
-
-// // 3. or generate module with all schemas added to the instance (excluding meta-schemas),
-// // export names would use schema IDs (or keys passed to addSchema method):
-// let moduleCode = standaloneCode(ajv)
-
-// now you can
-// write module code to file
-// const fs = require("fs")
-// const path = require("path")
-// fs.writeFileSync(path.join(__dirname, "/validate.js"), moduleCode)
-
-// ... or require module from string
-const requireFromString = require("require-from-string")
-const standaloneValidate = requireFromString(moduleCode) // for a single default export
 
 
-const valid = standaloneValidate({foo: 5})
-console.log(valid)
-console.log(standaloneValidate.errors)
+const moduleCode = buildValidationFunction(getSchema())
+const typeValidatorDir = path.join(__dirname, '../src/components/gallery/typeValidator')
+fs.writeFileSync(path.join(typeValidatorDir,'/standaloneValidateCode.js'), moduleCode, {encoding: 'utf-8'})
+
+// const requireFromString = require("require-from-string")
+// const standaloneValidate = requireFromString(moduleCode) // for a single default export
+// // return standaloneValidate
+// const valid = standaloneValidate({foo: 5})
+// console.log(valid)
+// console.log(standaloneValidate.errors)
--- packages/gallery/buildScripts/generateJSONSchemaFromTypes.js
+++ packages/gallery/buildScripts/generateJSONSchemaFromTypes.js
@@ -1,29 +1,34 @@
+module.exports = getSchema
 const path = require("path");
 const fs = require("fs");
 
 const TJS = require("typescript-json-schema")
-
-// optionally pass argument to schema generator
-const settings = {
+function getSchema() {
+  // optionally pass argument to schema generator
+  const settings = {
     required: true,
-};
+  };
 
-// optionally pass ts compiler options
-const compilerOptions = {
+  // optionally pass ts compiler options
+  const compilerOptions = {
     strictNullChecks: true,
-};
+  };
 
-// optionally pass a base path
-// const basePath = "./my-dir";
+  // optionally pass a base path
+  // const basePath = "./my-dir";
 
-const program = TJS.getProgramFromFiles(
-  [path.join(__dirname, '../src/components/gallery/styles.d.ts')],
-  compilerOptions,
-  // basePath
-);
+  const program = TJS.getProgramFromFiles(
+    [path.join(__dirname, '../src/components/gallery/styles.d.ts')],
+    compilerOptions,
+    // basePath
+  );
 
-// We can either get the schema for one file and one type...
-const schema = TJS.generateSchema(program, "StyleParams", settings);
-const schemaJSON = JSON.stringify(schema, null, 4)
-const template = `export default ${schemaJSON}`
-fs.writeFileSync(path.join(__dirname, '../src/components/gallery/typeValidator/schema.js'), template, {encoding: 'utf-8'})
+  // We can either get the schema for one file and one type...
+  const schema = TJS.generateSchema(program, "StyleParams", settings);
+  return schema
+}
+// const schema = getSchema()
+// const schemaJSON = JSON.stringify(schema, null, 4)
+// const template = `export default ${schemaJSON}`
+// const typeValidatorDir = path.join(__dirname, '../src/components/gallery/typeValidator')
+// fs.writeFileSync(path.join(typeValidatorDir,'/schema.js'), template, {encoding: 'utf-8'})
