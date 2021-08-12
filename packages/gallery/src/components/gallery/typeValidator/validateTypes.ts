1ce4ca890803c82b65fbbbd28b7dc6f0bf9bfdcd
Nir Naor <nirnaori@gmail.com>
2021-08-12 12:57:57 +0300

    data is optional.

--- packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
+++ packages/gallery/src/components/gallery/typeValidator/validateTypes.ts
@@ -2,7 +2,7 @@ import typeErrorsUI from './typeErrorsUI';
 import { StyleParams } from '../styles.d';
 const validateFunc = require('./standaloneValidateCode')
 
-function validate(data: StyleParams) {
+function validate(data?: StyleParams) {
   validateFunc(data);
   return validateFunc.errors || [];
 }
