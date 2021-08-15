7bf8ef240e6a4bc1ad5a5f7c9b6b92373dcfe5b6
Nir Naor <nirnaori@gmail.com>
2021-08-15 15:25:08 +0300

    wip

--- packages/gallery/src/components/gallery/index.tsx
+++ packages/gallery/src/components/gallery/index.tsx
@@ -16,6 +16,7 @@ export default class BaseGallery extends React.Component<
 
   constructor(props: GalleryProps) {
     super(props);
+    console.log('nir');
     this.isUsingCustomInfoElements = this.isUsingCustomInfoElements.bind(this);
     this.blueprintsManager = new BlueprintsManager({ id: 'layoutingGallery' });
     this.blueprintsManager.init({
