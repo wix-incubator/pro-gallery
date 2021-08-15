67ab772371a27d071e90037b670b753ad49bec68
Nir Naor <nirnaori@gmail.com>
2021-08-15 15:32:20 +0300

    wip

--- packages/lean-gallery/src/index.ts
+++ packages/lean-gallery/src/index.ts
@@ -1,4 +1,5 @@
 export { default as LeanGallery } from './leanGallery/leanGallery';
+console.log('nir');
 export { formatLeanGalleryStyles } from './leanGallery/leanGallery';
 export { default as isEligibleForLeanGallery } from './leanGallery/isEligible';
 export { notEligibleReasons } from './leanGallery/isEligible';
