cb9d36957090e94e830e797f56204436f2d14484
Nir Naor <nirnaori@gmail.com>
2021-08-17 12:15:32 +0300

    lint.

--- packages/gallery/buildScripts/buildRuntimeValidateFunctionFromTypes.js
+++ packages/gallery/buildScripts/buildRuntimeValidateFunctionFromTypes.js
@@ -7,9 +7,12 @@ const getSchemaFromTypes = require('./generateJSONSchemaFromTypes');
 
 function writeES5StandaloneValidateMethod() {
   const code = buildValidationFunction(getSchemaFromTypes());
-  const moduleCode = `/* eslint-disable */ ${code} /* eslint-enable */`;
+  // const moduleCode = `/* eslint-disable */ ${code} /* eslint-enable */`;
   const tempFilePath = path.join(__dirname, 'temp.js');
-  fs.writeFileSync(tempFilePath, `module.exports=${moduleCode}`);
+  fs.writeFileSync(
+    tempFilePath,
+    `/* eslint-disable */ module.exports=${code}/* eslint-enable */`
+  );
   const typeValidatorDir = path.join(
     __dirname,
     '../src/components/gallery/typeValidator'
