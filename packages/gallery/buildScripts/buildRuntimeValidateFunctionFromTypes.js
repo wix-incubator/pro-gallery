fc03ea1244ab6849aeff1d93f21b745e1e1a4cee
Nir Naor <nirnaori@gmail.com>
2021-08-17 11:32:04 +0300

    using browserify to build a standalone validate function

--- packages/gallery/buildScripts/buildRuntimeValidateFunctionFromTypes.js
+++ packages/gallery/buildScripts/buildRuntimeValidateFunctionFromTypes.js
@@ -1,26 +1,47 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
+const { execSync } = require('child_process');
 
-const { transformSync } =require("@babel/core")
-const Ajv = require("ajv")
+// const { transformSync } =require("@babel/core")
+const Ajv = require('ajv');
 
-const getSchemaFromTypes = require('./generateJSONSchemaFromTypes')
+const getSchemaFromTypes = require('./generateJSONSchemaFromTypes');
 
 function writeES5StandaloneValidateMethod() {
-  const moduleCode = buildValidationFunction(getSchemaFromTypes())
-  const {code} = transformSync(moduleCode, {});
-  const es5CompatibleCode = `/* eslint-disable */ ${code} /* eslint-enable */`
-  const typeValidatorDir = path.join(__dirname, '../src/components/gallery/typeValidator')
-  fs.writeFileSync(path.join(typeValidatorDir,'/standaloneValidateCode.js'), es5CompatibleCode, {encoding: 'utf-8'})
+  const moduleCode = buildValidationFunction(getSchemaFromTypes());
+  // const {code} = transformSync(moduleCode, {});
+  // const es5CompatibleCode = `/* eslint-disable */ ${code} /* eslint-enable */`
+  const tempFilePath = path.join(__dirname, 'temp.js');
+  fs.writeFileSync(tempFilePath, `module.exports=${moduleCode}`);
+  // fs.writeFileSync(tempFilePath, moduleCode)
+  const typeValidatorDir = path.join(
+    __dirname,
+    '../src/components/gallery/typeValidator'
+  );
+  const standaloneValidateCodePath = path.join(
+    typeValidatorDir,
+    '/standaloneValidateCode.js'
+  );
+  // fs.writeFileSync(standaloneValidateCodePath, `module.exports=${moduleCode}`)
+  // const browserifyBundle = path.join(typeValidatorDir,'/browserify.js')
+  const browserifyBundle = standaloneValidateCodePath;
+  execSync(
+    `node node_modules/.bin/browserify -t [ babelify --presets [ @babel/preset-env ] ] --standalone nirnaor ${tempFilePath} > ${browserifyBundle}`
+  );
+  // fs.writeFileSync(standaloneValidateCodePath, moduleCode, {encoding: 'utf-8'})
 }
 
 function buildValidationFunction(schema) {
-  const ajv = new Ajv({messages: true, verbose: true, code: {source: true, es5: true}})
-  const standaloneCode = require("ajv/dist/standalone").default
-  ajv.addSchema(schema, 'testSchema')
-  const validate = ajv.compile(schema, 'testSchema')
-  let moduleCode = standaloneCode(ajv, validate)
-  return moduleCode
+  const ajv = new Ajv({
+    messages: true,
+    verbose: true,
+    code: { source: true, es5: true },
+  });
+  const standaloneCode = require('ajv/dist/standalone').default;
+  ajv.addSchema(schema, 'testSchema');
+  const validate = ajv.compile(schema, 'testSchema');
+  let moduleCode = standaloneCode(ajv, validate);
+  return moduleCode;
 }
 
-writeES5StandaloneValidateMethod()
+writeES5StandaloneValidateMethod();
