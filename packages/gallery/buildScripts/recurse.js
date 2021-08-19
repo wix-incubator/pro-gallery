52c84581dd2a91a67e1827fbad772fa508e18193
Nir Naor <nirnaori@gmail.com>
2021-08-19 16:49:36 +0300

    wip.

--- packages/gallery/buildScripts/recurse.js
+++ packages/gallery/buildScripts/recurse.js
@@ -4,38 +4,39 @@ const fs = require('fs');
 const _ = require('lodash');
 const { assignByString } = require('pro-gallery-lib');
 
-let final = {};
-let route = [];
-
 function build(schema) {
+  let final = {};
+  let route = [];
   const properties = schema.properties;
   _.forEach(properties, (refObj, key) => {
     traverse(schema, refObj.$ref, key);
   });
-}
 
-function traverse(schema, refName, key) {
-  // console.log(`key: ${key}`)
-  route.push(key);
-  // console.log(route)
+  function traverse(schema, refName, key) {
+    // console.log(`key: ${key}`)
+    route.push(key);
+    // console.log(route)
 
-  const obj = schema.definitions[refName.split('#/definitions/').pop()];
-  if (_.isUndefined(obj.properties)) buildObject();
-  else {
-    _.forEach(obj.properties, (refObj, key) =>
-      traverse(schema, refObj.$ref, key)
-    );
+    const obj = schema.definitions[refName.split('#/definitions/').pop()];
+    if (_.isUndefined(obj.properties)) buildObject();
+    else {
+      _.forEach(obj.properties, (refObj, key) =>
+        traverse(schema, refObj.$ref, key)
+      );
+    }
   }
-}
 
-function buildObject() {
-  const routeString = route.join('_');
-  final = Object.assign({}, assignByString(final, routeString, routeString));
-  route = [];
+  function buildObject() {
+    const routeString = route.join('_');
+    final = Object.assign({}, assignByString(final, routeString, routeString));
+    route = [];
+  }
+
+  return final;
 }
 
 const schema = fs.readFileSync(path.join(__dirname, 'schema.json'), {
   encoding: 'utf8',
 });
-build(JSON.parse(schema));
-console.log(final);
+const result = build(JSON.parse(schema));
+console.log(result);
