dfc220d68736f223843d252954083ede258e5a12
Yonatan Hattav <yhattav@gmail.com>
2021-08-19 11:19:58 +0300

    add import of stylingParams

--- packages/gallery/src/components/gallery/styles.d.ts
+++ packages/gallery/src/components/gallery/styles.d.ts
@@ -1,5 +1,6 @@
 import LayoutParams from './layoutParams.d';
 import BehaviourParams from './behaviourParams.d';
+import StylingParams from './stylingParams.d';
 
 export interface StyleParams {
   layoutParams: LayoutParams;
