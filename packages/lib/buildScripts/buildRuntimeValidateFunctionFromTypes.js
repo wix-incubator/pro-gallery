e681f2991f70e354da5935c4eba73ad11ec9df68
Nir Naor <nirnaori@gmail.com>
2021-08-23 10:24:37 +0300

    Generate validateTypes in lib build script.

--- packages/lib/buildScripts/buildRuntimeValidateFunctionFromTypes.js
+++ packages/lib/buildScripts/buildRuntimeValidateFunctionFromTypes.js
@@ -3,13 +3,19 @@ const fs = require('fs');
 
 const writeES5StandaloneValidateMethod = require('./writeStandaloneValidate');
 function start() {
-  const galleryFolder = path.join(__dirname, '../src/components/gallery');
+  const galleryFolder = path.join(
+    __dirname,
+    '../../gallery/src/components/gallery'
+  );
   const sourceTypesFile = path.join(galleryFolder, 'galleryTypes.ts');
   const tempFile = path.join(__dirname, 'temp.js');
   const targetFile = path.join(
-    galleryFolder,
-    'typeValidator/validateTypes/standaloneValidateCode.js'
+    __dirname,
+    '../src/common/validateTypes/standaloneValidateCode.js'
   );
+
+  [sourceTypesFile, targetFile].forEach(raiseIfNotExist);
+
   writeES5StandaloneValidateMethod({
     sourceTypesFile,
     targetFile,
@@ -20,3 +26,10 @@ function start() {
   });
 }
 start();
+
+function raiseIfNotExist(fileOrDirPath) {
+  if (fs.existsSync(fileOrDirPath) === false)
+    throw new Error(
+      `Cannot generate validate function. this path does not exist: ${fileOrDirPath}`
+    );
+}
