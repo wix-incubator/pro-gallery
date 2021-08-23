d8eb6a3b5257deb301d148189231ef6d5f029e3b
ritabazak <rita.bazak@gmail.com>
2021-08-23 12:53:16 +0300

    RendererContainer

--- packages/gallery/src/common/interfaces/galleryRendererTypes.ts
+++ packages/gallery/src/common/interfaces/galleryRendererTypes.ts
@@ -1,7 +1,7 @@
-export interface GalleryRendererProps {
-  container: any;
-  items: any;
+import { GalleryProps } from './galleryTypes';
+import { RendererContainer } from './RendererContainer';
+
+export interface GalleryRendererProps extends GalleryProps {
+  container: RendererContainer;
   structure: any;
-  styles: any;
-  [key: string]: any;
 }
