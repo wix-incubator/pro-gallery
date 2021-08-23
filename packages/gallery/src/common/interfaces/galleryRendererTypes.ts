14d262dfad2f13301716007844bda4985d8a16a0
ritabazak <rita.bazak@gmail.com>
2021-08-23 15:29:06 +0300

    added Structure interface

--- packages/gallery/src/common/interfaces/galleryRendererTypes.ts
+++ packages/gallery/src/common/interfaces/galleryRendererTypes.ts
@@ -1,7 +1,16 @@
 import { GalleryProps } from './galleryTypes';
 import { RendererContainer } from './RendererContainer';
 
+export interface Structure {
+  items: any;
+  groups: any;
+  strips: any;
+  columns: any;
+  height: number;
+  width: number;
+}
+
 export interface GalleryRendererProps extends GalleryProps {
   container: RendererContainer;
-  structure: any;
+  structure: Structure;
 }
