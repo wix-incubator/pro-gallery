01d81cb7db8ecad6341c7c1dce4533d7a300bcd0
Nir Naor <nirnaori@gmail.com>
2021-08-23 11:12:46 +0300

    Generate validateTypes in lib build script. (#550)
    * Generate validateTypes in lib build script.
    
    * Raising on missing targetDir and not targetFile.
    
    * remove standaloneCode from gitignore.
    
    * Delete standaloneValidateCode.
    
    * Ignore standaloneValidateCode.

---
+++ packages/lib/buildScripts/buildRuntimeValidateFunctionFromTypes.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+const writeES5StandaloneValidateMethod = require('./writeStandaloneValidate');
+function start() {
+  const galleryFolder = path.join(
+    __dirname,
+    '../../gallery/src/components/gallery'
+  );
+  const sourceTypesFile = path.join(galleryFolder, 'galleryTypes.ts');
+  const tempFile = path.join(__dirname, 'temp.js');
+  const targetDir = path.join(__dirname, '../src/common/validateTypes/');
+  const targetFile = path.join(targetDir, 'standaloneValidateCode.js');
+
+  [sourceTypesFile, targetDir].forEach(raiseIfNotExist);
+
+  writeES5StandaloneValidateMethod({
+    sourceTypesFile,
+    targetFile,
+    tempFile,
+    writeFileSync: fs.writeFileSync,
+    createWriteStream: fs.createWriteStream,
+    rmSync: fs.rmSync,
+  });
+}
+start();
+
+function raiseIfNotExist(fileOrDirPath) {
+  if (fs.existsSync(fileOrDirPath) === false)
+    throw new Error(
+      `Cannot generate validate function. this path does not exist: ${fileOrDirPath}`
+    );
+}
