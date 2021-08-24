62e8b3279b07e6c36b346e35d11936f69f2b6433
Nir Naor <nirnaori@gmail.com>
2021-08-24 11:30:37 +0300

    GalleryProps instead of any.

--- packages/gallery/src/components/gallery/index.tsx
+++ packages/gallery/src/components/gallery/index.tsx
@@ -3,7 +3,6 @@ import { GALLERY_CONSTS, utils } from 'pro-gallery-lib';
 import { BlueprintsManager } from 'pro-gallery-blueprints';
 import ProGallery from './proGallery/proGallery';
 
-/// <reference types="pro-gallery-lib" />
 import { GalleryProps, GalleryState } from 'pro-gallery-lib';
 import shouldValidate from './typeValidator/shouldValidate';
 
@@ -14,7 +13,7 @@ export default class Gallery extends React.Component<
   private blueprintsManager: any;
   private galleryProps: any;
 
-  constructor(props: any) {
+  constructor(props: GalleryProps) {
     super(props);
     this.isUsingCustomInfoElements = this.isUsingCustomInfoElements.bind(this);
     this.blueprintsManager = new BlueprintsManager({ id: 'layoutingGallery' });
