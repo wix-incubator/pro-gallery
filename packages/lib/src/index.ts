c74b5b6f672ac0ff164b8803da3233c0be04c5e5
Nir Naor <nirnaori@gmail.com>
2021-08-24 11:24:08 +0300

    Moving interfaces to lib (#554)
    * Moving interfaces to lib
    
    * Adding GalleryProps and GalleryState.
    
    * REMOVING FAILING TEST
    
    * moving galleryRendererTypes to lib.
    
    * Revert "REMOVING FAILING TEST"
    
    This reverts commit 4cf4bcc7f7d28627f87a583024216a771bdddbc3.
    
    * not using galleryProps and galleryState for now.
    
    * Removing parallel.
    
    * Revert "not using galleryProps and galleryState for now."
    
    This reverts commit 56eb729df181f489f833d8c1a4e876f47119e613.
    
    * Revert "Revert "not using galleryProps and galleryState for now.""
    
    This reverts commit 0000356733f923dae2a0f2c8c5060f30ad4269cb.
    
    * Adding similiar type declaration to lib.
    
    * more duplicated declaration.
    
    * using the old types.

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -35,3 +35,12 @@ export { isDeviceTypeTouch } from './common/window/viewModeWrapper';
 
 export { default as galleryOptions } from './settings/options';
 export { INPUT_TYPES } from './settings/utils/constants';
+
+export { Item, PhotoItem, VideoItem, TextItem } from './common/interfaces/Item';
+export { Container } from './common/interfaces/Container';
+export { EventsListener } from './common/interfaces/EventsListener';
+export { CreateMediaUrl } from './common/interfaces/CreateMediaUrl';
+export { CustomComponents } from './common/interfaces/CustomComponents';
+export { ScrollingElement } from './common/interfaces/ScrollingElement';
+export { GalleryProps, GalleryState } from './common/interfaces/galleryTypes';
+export { GalleryRendererProps } from './common/interfaces/galleryRendererTypes';
