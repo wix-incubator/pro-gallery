8088e3e7746b0add452ea37a3b2be7b18ffc153d
Yoav Vaknin <yoavv@wix.com>
2021-08-31 11:28:04 +0300

    Refactor the styleparam test to reuse code

--- packages/gallery/tests/styleParams/arrowsVerticalPosition.spec.js
+++ packages/gallery/tests/styleParams/arrowsVerticalPosition.spec.js
@@ -26,15 +26,23 @@ describe('styleParam - arrowsVerticalPosition', () => {
     driver.detach.proGallery();
   });
 
+  async function mountGallery(props) {
+    driver.mount.proGallery(props);
+    await driver.update();
+  }
+
+  async function mountGalleryAndGetArrows(props) {
+    await mountGallery(props); // mounting gallery
+    return driver.find.selector('.nav-arrows-container'); // Getting the arrows
+  }
+
   it('Should not render the arrows when not enough info space', async () => {
     // Exact style params relevant to this sub-test
     initialProps.options = mergeNestedObjects(initialProps.options, {
       slideshowInfoSize: 38, // info height is 39 when default, so slideshowInfoSize < arrowsSize
       arrowsVerticalPosition: 'INFO_CENTER',
     });
-    driver.mount.proGallery(initialProps);
-    await driver.update();
-    const navArrows = driver.find.selector('.nav-arrows-container');
+    const navArrows = await mountGalleryAndGetArrows(initialProps);
     expect(navArrows).to.have.lengthOf(0);
   });
 
@@ -43,9 +51,7 @@ describe('styleParam - arrowsVerticalPosition', () => {
     initialProps.options = mergeNestedObjects(initialProps.options, {
       arrowsVerticalPosition: 'INFO_CENTER',
     });
-    driver.mount.proGallery(initialProps);
-    await driver.update();
-    const navArrows = driver.find.selector('.nav-arrows-container');
+    const navArrows = await mountGalleryAndGetArrows(initialProps);
     const galleryContainer = driver.find.selector('#pro-gallery-container');
     const { height: galleryHeight } = galleryContainer.props().style;
     const { top } = navArrows.props().style;
@@ -60,9 +66,7 @@ describe('styleParam - arrowsVerticalPosition', () => {
     initialProps.options = mergeNestedObjects(initialProps.options, {
       arrowsVerticalPosition: 'ITEM_CENTER',
     });
-    driver.mount.proGallery(initialProps);
-    await driver.update();
-    const navArrows = driver.find.selector('.nav-arrows-container');
+    const navArrows = await mountGalleryAndGetArrows(initialProps);
     const { top } = navArrows.props().style;
     expect(top.replace(/\s/g, '')).to.eq(`calc(50%+0px-19.5px-0px)`);
   });
@@ -72,9 +76,7 @@ describe('styleParam - arrowsVerticalPosition', () => {
     initialProps.options = mergeNestedObjects(initialProps.options, {
       arrowsVerticalPosition: 'IMAGE_CENTER',
     });
-    driver.mount.proGallery(initialProps);
-    await driver.update();
-    const navArrows = driver.find.selector('.nav-arrows-container');
+    const navArrows = await mountGalleryAndGetArrows(initialProps);
     const { top } = navArrows.props().style;
     const expectedInfoSpace = initialProps.options.slideshowInfoSize / 2;
     expect(top.replace(/\s/g, '')).to.eq(
