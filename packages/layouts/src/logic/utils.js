d60383a3e91deda5271fea39e6ca367c1a0aa3ea
Yonatan Hattav <yhattav@gmail.com>
2021-08-31 17:28:25 +0300

    remove unneeded code

--- packages/layouts/src/logic/utils.js
+++ packages/layouts/src/logic/utils.js
@@ -72,52 +72,6 @@ class Utils {
     return this._hash2int[str];
   }
 
-  assignByString(Obj, string, value) {
-    let _obj = { ...Obj };
-    let keyArr = string.split('_');
-    let assignedProperty = keyArr.pop();
-    let pointer = _obj;
-    keyArr.forEach((key) => {
-      if (!pointer[key]) pointer[key] = {};
-      pointer = pointer[key];
-    });
-    pointer[assignedProperty] = value;
-    return _obj;
-  }
-
-  flattenObject(ob) {
-    var toReturn = {};
-
-    for (var i in ob) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (!ob.hasOwnProperty(i)) continue;
-
-      if (
-        typeof ob[i] == 'object' &&
-        ob[i] !== null &&
-        Object.keys(ob[i]).length > 0
-      ) {
-        var flatObject = this.flattenObject(ob[i]);
-        for (var x in flatObject) {
-          // eslint-disable-next-line no-prototype-builtins
-          if (!flatObject.hasOwnProperty(x)) continue;
-
-          toReturn[i + '_' + x] = flatObject[x];
-        }
-      } else {
-        toReturn[i] = ob[i];
-      }
-    }
-    return toReturn;
-  }
-
-  flatToNested(ob) {
-    return Object.entries(ob).reduce(
-      (obj, [styleParam, value]) => this.assignByString(obj, styleParam, value),
-      {}
-    );
-  }
-
   addDefaultStyleParams(styleParams) {
     //default styleParams
     const defaultLayouterSP = {
