7ddd3cd6356d9170683815d066053f70e16c77ce
Yakov Hailo <yakovh@wix.com>
2021-09-01 17:18:48 +0300

    improve isMeaningfulString tests

--- packages/lib/test/utils.spec.js
+++ packages/lib/test/utils.spec.js
@@ -3,13 +3,13 @@ import utils from '../src/common/utils/index';
 
 describe('utils isMeaningfulString', () => {
   it('should return false if is not a meaningful string', () => {
-    [undefined, '', '  '].map((value) => {
-      expect(utils.isMeaningfulString(value)).to.equal(false);
-    });
+    expect([undefined, '', '  '].some(utils.isMeaningfulString)).to.equal(
+      false
+    );
   });
   it('should return true if is a meaningful string', () => {
-    ['yakov', ' yakov h  '].map((value) => {
-      expect(utils.isMeaningfulString(value)).to.equal(true);
-    });
+    expect(['yakov', ' yakov h  '].some(utils.isMeaningfulString)).to.equal(
+      true
+    );
   });
 });
