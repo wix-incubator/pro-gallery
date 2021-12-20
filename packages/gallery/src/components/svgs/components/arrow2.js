d5ddac2edf244a2cdac54c250828c8b999cec0a5
izaacayelin <izaaca@wix.com>
2021-12-20 14:22:39 +0200

    arrows refactor

---
+++ packages/gallery/src/components/svgs/components/arrow2.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* tslint:disable */
+import React from 'react';
+const Arrow2 = ({style}) => (
+<svg style={style} width="40px" height="33px" viewBox="0 0 40 33" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Artboard-Copy-93" transform="translate(-28.000000, -32.000000)" fill="#000000" fill-rule="nonzero">
+            <polygon id="4" points="51.5515789 32 67.6 48.0484211 51.5515789 64.0968421 46.9663158 59.5115789 54.9880947 51.4881326 28 51.4897902 28 44.9245271 55.30728 44.9243284 46.9663158 36.5852632"></polygon>
+        </g>
+    </g>
+</svg>
+);
+export default Arrow2;
+/* tslint:enable */
+/* eslint-enable */
--- packages/gallery/src/components/svgs/index.js
+++ packages/gallery/src/components/svgs/index.js
@@ -5,5 +5,6 @@ export {default as play_background} from './components/play_background';
 export {default as play_triangle} from './components/play_triangle';
 export {default as play} from './components/play';
 export {default as x} from './components/x';
+export {default as ARROWS_DATA } from './arrowsIconsData';
 /* tslint:enable */
 /* eslint-enable */
