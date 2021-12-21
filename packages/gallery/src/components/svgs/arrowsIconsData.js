327bdae0968a25c7428028c36b76cf88ce52390d
izaacayelin <izaaca@wix.com>
2021-12-21 11:32:05 +0200

    delete test arrow

--- packages/gallery/src/components/svgs/arrowsIconsData.js
+++ packages/gallery/src/components/svgs/arrowsIconsData.js
@@ -1,16 +1,10 @@
 import DefaultArrow from './components/defaultArrow';
-import Arrow2 from './components/arrow2';
 
 const ARROWS_DATA = {
   DEFAULT_ARROW: {
     SvgComp: DefaultArrow,
     width: 23,
     height: 39,
-  },
-  ARROW_2: {
-    SvgComp: Arrow2,
-    width: 40,
-    height: 33,
   },
 };
 export default ARROWS_DATA;
