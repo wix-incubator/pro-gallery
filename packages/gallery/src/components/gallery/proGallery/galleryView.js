81f698ae6ed3212b9de5ebcf18b2affbc216dc58
Yonatan Hattav <yhattav@gmail.com>
2022-01-26 12:40:44 +0200

    fix id on margin container

--- packages/gallery/src/components/gallery/proGallery/galleryView.js
+++ packages/gallery/src/components/gallery/proGallery/galleryView.js
@@ -180,7 +180,7 @@ class GalleryView extends React.Component {
         onKeyDown={this.handleKeys}
       >
         <div
-          id="pro-gallery-margin-container"
+          id={'pro-gallery-margin-container-' + this.props.id}
           style={{
             margin: options.layoutParams.gallerySpacing + 'px',
             height: galleryHeight,
