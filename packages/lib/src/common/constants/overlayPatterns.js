7b2b721003084a990575cd68f2b6110097650649
Guy Sopher <guyso@wix.com>
2022-01-26 15:33:20 +0200

    Add files

---
+++ packages/lib/src/common/constants/overlayPatterns.js
@@ -0,0 +1,12 @@
+const OVERLAY_PATTERNS = {
+  COLOR: 'COLOR',
+  WEAVE: 'weave',
+  STARS: 'stars',
+  RAINBOW: 'rainbow',
+  CARBON: 'carbon',
+  CUBE: 'cube',
+  TABLECLOTH: 'tablecloth',
+  HONEYCOMB: 'honeycomb',
+};
+
+export default OVERLAY_PATTERNS;
