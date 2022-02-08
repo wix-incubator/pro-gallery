6f5bd7a34744bd858b82f535fedc99e2c8cda4c2
Yonatan Hattav <yhattav@gmail.com>
2022-02-08 14:54:22 +0100

    Fix overflow horizontal (#744)

--- packages/gallery/src/components/gallery/proGallery/slideshowView.js
+++ packages/gallery/src/components/gallery/proGallery/slideshowView.js
@@ -1034,7 +1034,7 @@ class SlideshowView extends React.Component {
       const columnStyle = {
         width: this.props.isPrerenderMode ? '100%' : column.width,
         height: this.getDimensions().height,
-        overflow: 'visible',
+        overflowY: 'visible',
       };
       return (
         <div
