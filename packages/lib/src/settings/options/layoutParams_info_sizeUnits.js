345c871464b6383ed2ba7ff65d03ec0f1388f6c9
shmuelhizmi <shmuelhizmi1@gmail.com>
2022-03-01 14:49:45 +0200

    remove usage of width parm inside sizeUnits

--- packages/lib/src/settings/options/layoutParams_info_sizeUnits.js
+++ packages/lib/src/settings/options/layoutParams_info_sizeUnits.js
@@ -1,12 +1,15 @@
 import { INPUT_TYPES } from '../utils/constants';
 import { default as GALLERY_CONSTS } from '../../common/constants';
 import { createOptions } from '../utils/utils';
-import width from './layoutParams_info_width';
 
 export default {
   title: 'Text Box Width Units',
   description: `Set the text box width in pixels or as a percent from the column width and height.`,
-  isRelevant: (options) => width.isRelevant(options),
+  isRelevant: (options) =>
+    options.isVertical &&
+    options.groupSize === 1 &&
+    options.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL &&
+    GALLERY_CONSTS.hasExternalHorizontalPlacement(options.titlePlacement),
   type: INPUT_TYPES.OPTIONS,
   options: createOptions('textBoxWidthCalculationOptions'),
   default: GALLERY_CONSTS.textBoxWidthCalculationOptions.PERCENT,
