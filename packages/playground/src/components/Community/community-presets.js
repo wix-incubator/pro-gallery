d77f3c57b1e3ffcd164e257f3c4dea964335556f
izaacayelin <izaaca@wix.com>
2022-03-09 15:15:39 +0200

    migration

--- packages/playground/src/components/Community/community-presets.js
+++ packages/playground/src/components/Community/community-presets.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Icon as LegacyIcon } from '@ant-design/compatible';
 import { Card, Button } from 'antd';
-import { ArrowUpOutline, ArrowDownOutline } from '@ant-design/icons';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import { GalleryPreview } from './galleryPreview';
 import { getAll, like } from '../../data';
 import { formatValue } from "../../utils/utils";
@@ -55,8 +55,8 @@ function CommunityPresets() {
       <Card
         cover={<a href={item.href}><GalleryPreview item={item} /></a>}
         actions={[
-          <Button type="link" disabled={isDownvoted(item)} size="large" onClick={() => onClickLike(item, -1)} icon={<LegacyIcon type={ArrowDownOutline.name} />}></Button>,
-          <Button type="link" disabled={isUpvoted(item)} size="large" onClick={() => onClickLike(item, 1)} icon={<LegacyIcon type={ArrowUpOutline.name} />}></Button>,
+          <Button type="link" disabled={isDownvoted(item)} size="large" onClick={() => onClickLike(item, -1)} icon={<LegacyIcon type={ArrowDownOutlined.name} />}></Button>,
+          <Button type="link" disabled={isUpvoted(item)} size="large" onClick={() => onClickLike(item, 1)} icon={<LegacyIcon type={ArrowUpOutlined.name} />}></Button>,
           <Button type="link" size="large"><LegacyIcon type="star" theme={isLiked(item) ? 'filled' : 'outlined'}/>  {item.likes}</Button>,
         ]}
         style={{
