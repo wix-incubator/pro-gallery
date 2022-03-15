0e9e7072d6c75efe5785f187f4dde70cdb06fbb4
Yakov Hailo <yakovh@wix.com>
2022-03-15 17:42:49 +0200

    improve isRelevent for crop ratios

--- packages/lib/src/settings/options/layoutParams_crop_ratios.js
+++ packages/lib/src/settings/options/layoutParams_crop_ratios.js
@@ -9,7 +9,10 @@ export default {
   description: `Crop each image according to the corresponding crop ratio as indicated in this string. This will create a pattern of cropped images`,
   isRelevantDescription: 'Set "Crop Images" to "true".',
   isRelevant: (options) =>
-    options.cubeImages && options.cubeType === GALLERY_CONSTS.cubeType.CROP,
+    options.cubeImages &&
+    (!options.cropOnlyFill ||
+      (options.cropOnlyFill &&
+        options.cubeType === GALLERY_CONSTS.cubeType.CROP)),
   type: INPUT_TYPES.NUMBER,
   default: [1],
 };
