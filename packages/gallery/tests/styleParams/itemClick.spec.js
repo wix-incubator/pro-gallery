3d80a1ffc967cc9a29ab6fd55c426ae1ec4fcf41
izaacayelin <izaaca@wix.com>
2022-04-25 12:42:20 +0300

    remove unneeded test selectors

--- packages/gallery/tests/styleParams/itemClick.spec.js
+++ packages/gallery/tests/styleParams/itemClick.spec.js
@@ -128,11 +128,9 @@ describe('options - itemClick', () => {
       driver.mount.proGallery(initialProps);
       await driver.update();
       const item = driver.find
-        .selector(
-          '#pro-gallery-container-default-dom-id a',
-          '#pro-gallery-container-default-dom-id div'
-        )
+        .selector('#pro-gallery-container-default-dom-id a')
         .at(0);
+      console.log(item);
       expect(item.props().href).to.not.be.undefined;
       driver.detach.proGallery();
     });
@@ -143,10 +141,7 @@ describe('options - itemClick', () => {
       driver.mount.proGallery(initialProps);
       await driver.update();
       const item = driver.find
-        .selector(
-          '#pro-gallery-container-default-dom-id a',
-          '#pro-gallery-container-default-dom-id div'
-        )
+        .selector('#pro-gallery-container-default-dom-id div')
         .at(0);
       expect(item.props().href).to.be.undefined;
       driver.detach.proGallery();
