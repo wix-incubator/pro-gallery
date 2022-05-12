999070e274b8f4da8daed5d93392d3a1118eb525
yuvalab <82800222+yuvalab@users.noreply.github.com>
2022-05-12 13:57:30 +0300

    Revert "floor targetHeight/Width (#831)" (#836)

--- packages/layouts/src/logic/item.js
+++ packages/layouts/src/logic/item.js
@@ -407,8 +407,6 @@ export class Item {
       }
     }
 
-    targetWidth = Math.floor(targetWidth);
-    targetHeight = Math.floor(targetHeight);
     let fixVals = {
       fixTop: (this.height - targetHeight) / 2,
       fixLeft: (this.width - targetWidth) / 2,
