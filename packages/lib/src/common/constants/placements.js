9363a5fcf8c2bd6bea0922f4384037ae33ba3874
Yonatan Hattav <yhattav@gmail.com>
2022-05-25 18:55:07 +0300

    vertical pos

--- packages/lib/src/common/constants/placements.js
+++ packages/lib/src/common/constants/placements.js
@@ -8,24 +8,42 @@ const PLACEMENTS = {
   ALTERNATE_VERTICAL: 'ALTERNATE_VERTICAL',
 };
 
+const NEW_PLACEMENTS = {
+  OVERLAY: 'OVERLAY',
+  ABOVE: 'ABOVE',
+  BELOW: 'BELOW',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+  ALTERNATE_HORIZONTALLY: 'ALTERNATE_HORIZONTALLY',
+  ALTERNATE_VERTICALLY: 'ALTERNATE_VERTICALLY',
+};
+
 const hasHoverPlacement = (placement) =>
-  String(placement).indexOf(PLACEMENTS.SHOW_ON_HOVER) >= 0;
+  String(placement).includes(PLACEMENTS.SHOW_ON_HOVER) ||
+  String(placement).includes(NEW_PLACEMENTS.OVERLAY);
 const hasExternalAbovePlacement = (placement, idx) =>
-  String(placement).indexOf(PLACEMENTS.SHOW_ABOVE) >= 0 ||
+  String(placement).includes(PLACEMENTS.SHOW_ABOVE) ||
+  String(placement).includes(NEW_PLACEMENTS.ABOVE) ||
   (idx % 2 === 0 &&
-    String(placement).indexOf(PLACEMENTS.ALTERNATE_VERTICAL) >= 0);
+    (String(placement).includes(PLACEMENTS.ALTERNATE_VERTICAL) ||
+      String(placement).includes(NEW_PLACEMENTS.ALTERNATE_VERTICALLY)));
 const hasExternalBelowPlacement = (placement, idx) =>
-  String(placement).indexOf(PLACEMENTS.SHOW_BELOW) >= 0 ||
+  String(placement).includes(PLACEMENTS.SHOW_BELOW) ||
+  String(placement).includes(NEW_PLACEMENTS.BELOW) ||
   (idx % 2 === 1 &&
-    String(placement).indexOf(PLACEMENTS.ALTERNATE_VERTICAL) >= 0);
+    (String(placement).includes(PLACEMENTS.ALTERNATE_VERTICAL) ||
+      String(placement).includes(NEW_PLACEMENTS.ALTERNATE_VERTICALLY)));
 const hasExternalRightPlacement = (placement, idx) =>
-  String(placement).indexOf(PLACEMENTS.SHOW_ON_THE_RIGHT) >= 0 ||
+  String(placement).includes(PLACEMENTS.SHOW_ON_THE_RIGHT) ||
+  String(placement).includes(NEW_PLACEMENTS.RIGHT) ||
   (idx % 2 === 0 &&
-    String(placement).indexOf(PLACEMENTS.ALTERNATE_HORIZONTAL) >= 0);
+    String(placement).includes(PLACEMENTS.ALTERNATE_HORIZONTAL)) ||
+  String(placement).includes(NEW_PLACEMENTS.ALTERNATE_HORIZONTALLY);
 const hasExternalLeftPlacement = (placement, idx) =>
-  String(placement).indexOf(PLACEMENTS.SHOW_ON_THE_LEFT) >= 0 ||
+  String(placement).includes(PLACEMENTS.SHOW_ON_THE_LEFT) ||
   (idx % 2 === 1 &&
-    String(placement).indexOf(PLACEMENTS.ALTERNATE_HORIZONTAL) >= 0);
+    String(placement).includes(PLACEMENTS.ALTERNATE_HORIZONTAL)) ||
+  String(placement).includes(NEW_PLACEMENTS.ALTERNATE_HORIZONTALLY);
 const hasExternalVerticalPlacement = (placement) =>
   hasExternalAbovePlacement(placement, 0) ||
   hasExternalBelowPlacement(placement, 1);
@@ -42,17 +60,22 @@ const isExternalHorizontalPlacement = (placement) =>
   !hasExternalVerticalPlacement(placement) &&
   !hasHoverPlacement(placement);
 const isExternalAbovePlacement = (placement) =>
-  String(placement) === PLACEMENTS.SHOW_ABOVE;
+  String(placement) === PLACEMENTS.SHOW_ABOVE ||
+  String(placement) === NEW_PLACEMENTS.ABOVE;
 const isExternalBelowPlacement = (placement) =>
-  String(placement) === PLACEMENTS.SHOW_BELOW;
+  String(placement) === PLACEMENTS.SHOW_BELOW ||
+  String(placement) === NEW_PLACEMENTS.BELOW;
 const isExternalAboveOrBelowSinglePlacement = (placement) =>
   isExternalAbovePlacement(placement) || isExternalBelowPlacement(placement);
 const isHoverPlacement = (placement) =>
-  String(placement) === PLACEMENTS.SHOW_ON_HOVER;
+  String(placement) === PLACEMENTS.SHOW_ON_HOVER ||
+  String(placement) === NEW_PLACEMENTS.OVERLAY;
 const isExternalRightPlacement = (placement) =>
-  String(placement) === PLACEMENTS.SHOW_ON_THE_RIGHT;
+  String(placement) === PLACEMENTS.SHOW_ON_THE_RIGHT ||
+  String(placement) === NEW_PLACEMENTS.RIGHT;
 const isExternalLeftPlacement = (placement) =>
-  String(placement) === PLACEMENTS.SHOW_ON_THE_LEFT;
+  String(placement) === PLACEMENTS.SHOW_ON_THE_LEFT ||
+  String(placement) === NEW_PLACEMENTS.LEFT;
 
 const isConstantVerticalPlacement = (placement, isSlideshow) => {
   const placementArray = placement.split(',');
