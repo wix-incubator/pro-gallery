304467e5d46a4b02d40d8e16ad9c8a30f9c42f19
Yuval Abramovich <yuvalab@wix.com>
2022-08-08 14:24:23 +0300

    working on is relevant

--- packages/lib/src/settings/options/itemClick.js
+++ packages/lib/src/settings/options/itemClick.js
@@ -4,8 +4,11 @@ import { default as GALLERY_CONSTS } from '../../common/constants';
 
 export default {
   title: 'Click Action',
-  isRelevant: () => true,
-  isRelevantDescription: 'Always relevant.',
+  isRelevant: (options) =>
+    options.showArrows &&
+    options.arrowsPosition !== GALLERY_CONSTS.arrowsPosition.MOUSE_CURSOR,
+  isRelevantDescription:
+    'set "Arrows Position" to anything but "Mouse Cursor" (ON_GALLERY or OUTSIDE_GALLERY)',
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.itemClick.NOTHING,
   options: createOptions('itemClick'),
--- packages/lib/src/settings/options/videoPlay.js
+++ packages/lib/src/settings/options/videoPlay.js
@@ -5,8 +5,26 @@ import { createOptions } from '../utils/utils';
 export default {
   title: 'Play Video On',
   description: `Choose when video items will start playing: On hover, on click or autoplay`,
-  isRelevant: () => true,
-  isRelevantDescription: 'Always relevant.',
+  isRelevant: (options, option) => {
+    // specific isRelevant function
+    const isVideoPlayAbleToPlay = (options) => {
+      return options['showArrows'] && options['arrowsPosition'] !== 2;
+    };
+    // Distribution of the specific isRelevant functions
+    const videoPlayOptions = {
+      AUTO: () => {
+        return true;
+      },
+      HOVER: isVideoPlayAbleToPlay,
+      ON_CLICK: isVideoPlayAbleToPlay,
+    };
+    // specific option isRelevant : general video play isRelevant (AUTO always true)
+    return option
+      ? videoPlayOptions[option](options)
+      : Object.values(videoPlayOptions).some((val) => val(options));
+  },
+  isRelevantDescription:
+    'in case of HOVER or ON_CLICK - set "Arrows Position" to anything but "Mouse Cursor" (ON_GALLERY or OUTSIDE_GALLERY)',
   options: createOptions('videoPlay'),
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.videoPlay.HOVER,
