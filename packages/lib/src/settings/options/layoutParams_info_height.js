3ec305323dbfcfd977de23fd182f45c4df60c9c4
talreu <talreu@wix.com>
2022-08-14 14:26:04 +0300

    changing isRelevant condition

--- packages/lib/src/settings/options/layoutParams_info_height.js
+++ packages/lib/src/settings/options/layoutParams_info_height.js
@@ -7,9 +7,10 @@ export default {
   isRelevantDescription:
     'First set a Vertical gallery ("Scroll Direction" as "Vertical").\nThen set "Layout Orientation" to "Columns".\nThen set "Max Group Size" to "1".\nThen set "Texts Placement" to "Show Below" or "Show Above".',
   isRelevant: (options) =>
-    options.isVertical &&
+  ((options.isVertical &&
+    options.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL) ||
+    options.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL) &&
     options.groupSize === 1 &&
-    options.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL &&
     GALLERY_CONSTS.hasExternalVerticalPlacement(options.titlePlacement),
   type: INPUT_TYPES.NUMBER,
   default: 200,
