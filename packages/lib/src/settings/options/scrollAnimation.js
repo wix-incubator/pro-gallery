e45962cdc61b63a0c3f574c8357629870963057c
Yonatan Hattav <yhattav@gmail.com>
2022-08-30 12:47:38 +0300

    remove just in slideshow and thumbnails

--- packages/lib/src/settings/options/scrollAnimation.js
+++ packages/lib/src/settings/options/scrollAnimation.js
@@ -7,17 +7,10 @@ export default {
   description: `Choose the type of animation to be used when items appear while scrolling vertically through the gallery`,
   isRelevantDescription:
     'To enable "Scroll Animation" either set a Vertical gallery ("Scroll Direction" as "Vertical")\nor set a Horizontal gallery ("Scroll Direction" as "Horizontal") with "Slide Animation" set to "Scroll".',
-  isRelevant: (sp) => {
-    const isSingleVerticalItemRendered = sp.layoutParams.repeatingGroupTypes
-      ? sp.layoutParams.repeatingGroupTypes === '1'
-      : sp.groupTypes === '1';
-    return (
-      sp.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL ||
-      (sp.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL &&
-        sp.slideAnimation === GALLERY_CONSTS.slideAnimations.SCROLL &&
-        !isSingleVerticalItemRendered)
-    );
-  },
+  isRelevant: (sp) =>
+    sp.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL ||
+    (sp.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL &&
+      sp.slideAnimation === GALLERY_CONSTS.slideAnimations.SCROLL),
   options: createOptions('scrollAnimations'),
   type: INPUT_TYPES.OPTIONS,
   default: GALLERY_CONSTS.scrollAnimations.NO_EFFECT,
