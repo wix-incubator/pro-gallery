0e280d37fd1a984cef14aca775e479628ec4f0db
Yonatan Hattav <yhattav@gmail.com>
2022-10-23 16:53:22 +0300

    create files for layoutParams_structure

--- packages/lib/src/settings/options/layoutParams_structure_responsiveMode.js
+++ packages/lib/src/settings/options/layoutParams_structure_responsiveMode.js
@@ -5,13 +5,13 @@ import { createOptions } from '../utils/utils';
 export default {
   title: 'Responsive Type',
   isRelevant: (options) =>
-    options.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL &&
+    options.scrollDirection === GALLERY_CONSTS.scrollDirection.VERTICAL && //NEW STYPEPARAMS METHOD use new sps
     options.isVertical,
   isRelevantDescription:
     'Set a Vertical gallery ("Scroll Direction" as "Vertical") and set "Layout Orientation" to "Columns".',
   type: INPUT_TYPES.OPTIONS,
-  default: GALLERY_CONSTS.gridStyle.FIT_TO_SCREEN,
-  options: createOptions('gridStyle'),
+  default: GALLERY_CONSTS.gridStyle.FIT_TO_SCREEN, //NEW STYPEPARAMS METHOD this is not a relevant type here
+  options: createOptions('gridStyle'), //NEW STYPEPARAMS METHOD use responsiveMode new ENUM
   description: `Choose between adjusting the number of columns addording to the container
   size or setting it manually and keep it fixed.`,
   alert: 'This option will disable the responsive feature of the gallery!',
