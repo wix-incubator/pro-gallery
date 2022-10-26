32fcb7df9159cd09562c79657dba6112478829d9
Yonatan Hattav <yhattav@gmail.com>
2022-10-26 14:20:10 +0300

    restore scrollDirection unintended change

--- packages/lib/src/settings/options/layoutParams_structure_scrollDirection.js
+++ packages/lib/src/settings/options/layoutParams_structure_scrollDirection.js
@@ -1,15 +1,16 @@
 //NEW STYPEPARAMS METHOD
 import { INPUT_TYPES } from '../utils/constants';
+import { default as GALLERY_CONSTS } from '../../common/constants';
 import { createOptions } from '../utils/utils';
 import optionsMap from '../../core/helpers/optionsMap';
-import { flatV4DefaultOptions } from '../../common/v4DefaultOptions';
+
 export default {
   title: 'Scroll Direction',
   isRelevant: () => true,
   isRelevantDescription: 'Always relevant.',
   type: INPUT_TYPES.OPTIONS,
   default:
-    flatV4DefaultOptions[optionsMap.layoutParams.structure.scrollDirection],
+    GALLERY_CONSTS[optionsMap.layoutParams.structure.scrollDirection].VERTICAL,
   options: createOptions(optionsMap.layoutParams.structure.scrollDirection),
   description: `Toggle between two types of galleries. Vertical and Horizontal. Notice that many options are available only for a specific scroll direction.`,
 };
