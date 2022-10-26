a8948aea7e9edabf2b45d17f3b8ff93a636fc039
Yonatan Hattav <yhattav@gmail.com>
2022-10-26 10:51:35 +0300

    Sp behaviour params item (#960)

---
+++ packages/lib/src/settings/options/behaviourParams_item_overlay_sizeUnits.js
@@ -0,0 +1,19 @@
+//NEW STYPEPARAMS METHOD
+import { INPUT_TYPES } from '../utils/constants';
+import { default as GALLERY_CONSTS } from '../../common/constants';
+import { createOptions } from '../utils/utils';
+import optionsMap from '../../core/helpers/optionsMap';
+
+export default {
+  title: 'Overlay Size Type',
+  description: `choose pixel or percent`,
+  isRelevantDescription: 'Set "Hover Effect" to anything but "Never Show".',
+  isRelevant: (options) =>
+    options[optionsMap.behaviourParams.item.overlay.hoveringBehaviour] !==
+    GALLERY_CONSTS[optionsMap.behaviourParams.item.overlay.hoveringBehaviour]
+      .NEVER_SHOW,
+  type: INPUT_TYPES.OPTIONS,
+  default:
+    GALLERY_CONSTS[optionsMap.behaviourParams.item.overlay.sizeUnits].PERCENT,
+  options: createOptions(optionsMap.behaviourParams.item.overlay.sizeUnits),
+};
--- packages/lib/src/settings/options/behaviourParams_item_secondaryMedia_trigger.js
+++ packages/lib/src/settings/options/behaviourParams_item_secondaryMedia_trigger.js
@@ -3,13 +3,14 @@ import { default as GALLERY_CONSTS } from '../../common/constants';
 import { createOptions } from '../utils/utils';
 import arrowsPosition from './arrowsPosition';
 import layoutParams_navigationArrows_mouseCursorContainerMaxWidth from './layoutParams_navigationArrows_mouseCursorContainerMaxWidth';
+import optionsMap from '../../core/helpers/optionsMap';
 
 export default {
   title: 'Second Media Trigger',
   isRelevant: (options) =>
     !(
       arrowsPosition.isRelevant(options) &&
-      options.arrowsPosition === GALLERY_CONSTS.arrowsPosition.MOUSE_CURSOR &&
+      options.arrowsPosition === GALLERY_CONSTS.arrowsPosition.MOUSE_CURSOR && //NEW STYPEPARAMS METHOD chnge to new sps
       options.layoutParams.navigationArrows.mouseCursorContainerMaxWidth ===
         100 &&
       layoutParams_navigationArrows_mouseCursorContainerMaxWidth.isRelevant(
@@ -19,7 +20,10 @@ export default {
   isRelevantDescription:
     'Not relevant when arowsPosition is MOUSE_CURSOR and mouseCursorContainerMaxWidth is 100',
   type: INPUT_TYPES.OPTIONS,
-  default: GALLERY_CONSTS.secondaryMediaTrigger.OFF,
-  options: createOptions('secondaryMediaTrigger'),
+  default:
+    GALLERY_CONSTS[optionsMap.behaviourParams.item.secondaryMedia.trigger].OFF, //NEW STYPEPARAMS METHOD need one source
+  options: createOptions(
+    optionsMap.behaviourParams.item.secondaryMedia.trigger
+  ),
   description: `Select the triggering action that will show the second media.`,
 };
