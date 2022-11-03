a3b38b107612af2bbb326a7141848831026e5328
Yonatan Hattav <yhattav@gmail.com>
2022-11-03 17:02:03 +0200

    add newSPs to the existing files of new sps

--- packages/lib/src/settings/options/behaviourParams_item_secondaryMedia_trigger.js
+++ packages/lib/src/settings/options/behaviourParams_item_secondaryMedia_trigger.js
@@ -1,22 +1,11 @@
 import { INPUT_TYPES } from '../utils/constants';
 import { default as GALLERY_CONSTS } from '../../common/constants';
 import { createOptions } from '../utils/utils';
-import arrowsPosition from './arrowsPosition';
-import layoutParams_navigationArrows_mouseCursorContainerMaxWidth from './layoutParams_navigationArrows_mouseCursorContainerMaxWidth';
 import optionsMap from '../../core/helpers/optionsMap';
 
 export default {
   title: 'Second Media Trigger',
-  isRelevant: (options) =>
-    !(
-      arrowsPosition.isRelevant(options) &&
-      options.arrowsPosition === GALLERY_CONSTS.arrowsPosition.MOUSE_CURSOR && //NEW STYPEPARAMS METHOD chnge to new sps
-      options.layoutParams.navigationArrows.mouseCursorContainerMaxWidth ===
-        100 &&
-      layoutParams_navigationArrows_mouseCursorContainerMaxWidth.isRelevant(
-        options
-      )
-    ),
+  isRelevant: () => true,
   isRelevantDescription:
     'Not relevant when arowsPosition is MOUSE_CURSOR and mouseCursorContainerMaxWidth is 100',
   type: INPUT_TYPES.OPTIONS,
--- packages/lib/src/settings/options/layoutParams_structure_galleryRatio_value.js
+++ packages/lib/src/settings/options/layoutParams_structure_galleryRatio_value.js
@@ -1,12 +1,23 @@
 import { default as GALLERY_CONSTS } from '../../common/constants';
+import optionsMap from '../../core/helpers/optionsMap';
 import { INPUT_TYPES } from '../utils/constants';
 
 export default {
   title: 'Gallery Ratio',
   description: `This option allows horizontally scrolled galleries to link their height to the width of the gallery by the provided ratio`,
-  isRelevant: (options) =>
-    options.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL, //NEW STYPEPARAMS METHOD replace to new sp scrollDirection
-
+  isRelevant: (options) => {
+    if (options.newSPs) {
+      return (
+        options[optionsMap.layoutParams.structure.scrollDirection] ===
+        GALLERY_CONSTS[optionsMap.layoutParams.structure.scrollDirection]
+          .HORIZONTAL
+      );
+    } else {
+      return (
+        options.scrollDirection === GALLERY_CONSTS.scrollDirection.HORIZONTAL //NEW STYPEPARAMS METHOD replace to new sp scrollDirection
+      );
+    }
+  },
   isRelevantDescription:
     'Set a Horizontal gallery ("Scroll Direction" as "Horizontal")',
   default: 0,
