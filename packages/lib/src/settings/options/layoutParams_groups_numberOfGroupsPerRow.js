dfef06c1fa2505c65425c684a944ea604817dc33
Yonatan Hattav <yhattav@gmail.com>
2022-11-03 16:59:54 +0200

    fix numberOfGroupsPerRow option relevancy

--- packages/lib/src/settings/options/layoutParams_groups_numberOfGroupsPerRow.js
+++ packages/lib/src/settings/options/layoutParams_groups_numberOfGroupsPerRow.js
@@ -2,15 +2,23 @@
 import { INPUT_TYPES } from '../utils/constants';
 // import { default as GALLERY_CONSTS } from '../../common/constants';
 import optionsMap from '../../core/helpers/optionsMap';
+import { GALLERY_CONSTS } from '../..';
 
 export default {
   title: 'Groups per Row',
-  isRelevant: (options) =>
-    options[optionsMap.layoutParams.structure.galleryLayout] === 'VERTICAL' && // NEW STYPEPARAMS METHOD CHANGE CONST // Change to the isRelevant of layoutOrientation
-    options[optionsMap.layoutParams.structure.layoutOrientation] ===
-      'HORIZONTAL' &&
-    options[optionsMap.layoutParams.structure.responsiveMode] ===
-      'SET_ITEMS_PER_ROW',
+  isRelevant: (options) => {
+    return (
+      options[optionsMap.layoutParams.structure.scrollDirection] ===
+        GALLERY_CONSTS[optionsMap.layoutParams.structure.scrollDirection]
+          .VERTICAL &&
+      options[optionsMap.layoutParams.structure.layoutOrientation] ===
+        GALLERY_CONSTS[optionsMap.layoutParams.structure.layoutOrientation]
+          .HORIZONTAL &&
+      options[optionsMap.layoutParams.structure.responsiveMode] ===
+        GALLERY_CONSTS[optionsMap.layoutParams.structure.responsiveMode]
+          .SET_ITEMS_PER_ROW
+    );
+  },
   isRelevantDescription:
     'Set a Vertical gallery ("Scroll Direction" as "Vertical"), set "Layout Orientation" to "Rows" and set "Responsive Type" to "Set Items Per Row".',
   type: INPUT_TYPES.NUMBER,
