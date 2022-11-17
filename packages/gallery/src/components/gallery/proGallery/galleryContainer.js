de9d6bd0e94ec284c92bd98f887c8d99087eb261
Yonatan Hattav <yhattav@gmail.com>
2022-11-17 09:28:49 +0200

    dummy commit as a fix to release in v4

--- packages/gallery/src/components/gallery/proGallery/galleryContainer.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainer.js
@@ -64,7 +64,7 @@ export class GalleryContainer extends React.Component {
       isInHover: false,
       isInViewport: true,
     };
-
+    //dummy
     this.state = initialState;
     this.layoutCss = [];
 
