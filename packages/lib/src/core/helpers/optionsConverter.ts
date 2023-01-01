f73e3821649d65e399ddf26b98ef3d7b966fd256
IzaacAyelin <izaaca@wix.com>
2023-01-01 13:17:30 +0200

    Revert "add flat styles in convertion (#964)"
    This reverts commit be4c11ad1a32fbcbd12192074137b6e3365191af.

--- packages/lib/src/core/helpers/optionsConverter.ts
+++ packages/lib/src/core/helpers/optionsConverter.ts
@@ -21,7 +21,7 @@ import {
 function extendNestedOptionsToIncludeOldAndNew(nestedOptions) {
   let flatOptions = flattenObject(nestedOptions);
   let populatedFlatOptions = addOldOptions(addMigratedOptions(flatOptions));
-  return { ...flatToNested(populatedFlatOptions), ...populatedFlatOptions };
+  return flatToNested(populatedFlatOptions);
 }
 
 function addMigratedOptions(flatOptions) {
