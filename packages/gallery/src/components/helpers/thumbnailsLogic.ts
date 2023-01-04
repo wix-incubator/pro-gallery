1ffb24db35f51ea35f7f77fbf97d39d2cb6388fd
Nir Naor <nirnaori@gmail.com>
2023-01-04 13:34:26 +0200

    bring back margin

--- packages/gallery/src/components/helpers/thumbnailsLogic.ts
+++ packages/gallery/src/components/helpers/thumbnailsLogic.ts
@@ -6,9 +6,9 @@ import {
 } from 'pro-gallery-lib';
 import { CSSProperties } from 'react';
 
-// function getNumberOrZeroIfNaN(value: number) {
-//   return isNaN(value) ? 0 : value;
-// }
+function getNumberOrZeroIfNaN(value: number) {
+  return isNaN(value) ? 0 : value;
+}
 function calculateActiveIndexOffset({
   activeIndex,
   prevActiveIndex,
@@ -99,7 +99,8 @@ export function getThumbnailsData({
   const nirMargin = options[optionsMap.layoutParams.thumbnails.margin];
   const thumbnailSizeWithSpacing =
     options[optionsMap.layoutParams.thumbnails.size] +
-    options[optionsMap.layoutParams.thumbnails.spacing] * 2;
+    getNumberOrZeroIfNaN(nirMargin) * 2;
+  options[optionsMap.layoutParams.thumbnails.spacing] * 2;
   const horizontalThumbnails =
     thumbnailAlignment ===
       GALLERY_CONSTS[optionsMap.layoutParams.thumbnails.alignment].BOTTOM ||
