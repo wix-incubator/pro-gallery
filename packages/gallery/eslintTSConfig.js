27a5aa49dc7e0cf152fe70147aed41efc19428a3
Nir Naor <nirnaori@gmail.com>
2023-01-04 11:26:39 +0200

    real disable.

--- packages/gallery/eslintTSConfig.js
+++ packages/gallery/eslintTSConfig.js
@@ -3,5 +3,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
-  rules: {},
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
 };
