9eb8efd3b1a557a818870e620970de9c7fdd05ce
Shmuel Hizmi <48323313+shmuelhizmi@users.noreply.github.com>
2023-01-22 14:42:12 +0200

    3d options pr (#1071)
    * update ts version
    
    * 3d options
    
    * Revert "update ts version"
    
    This reverts commit ff1095eb4dfcfcf7544489587baf20a5547a7d72.
    
    * fix typescript crash

---
+++ packages/lib/src/settings/options/behaviourParams_item_threeDimensionalScene_keepPosterAfterObjectLoad.ts
@@ -0,0 +1,8 @@
+import { INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: `Show preview poster should be kept after object load`,
+  isRelevant: () => true,
+  type: INPUT_TYPES.BOOLEAN,
+  default: false,
+};
