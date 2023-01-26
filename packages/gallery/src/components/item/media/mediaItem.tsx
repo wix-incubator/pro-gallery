e83a495f689c8c2981af5ded61e87ab6e7c4504c
Guy Sopher <guyso@wix.com>
2023-01-26 11:59:14 +0200

    remove useMemo

--- packages/gallery/src/components/item/media/mediaItem.tsx
+++ packages/gallery/src/components/item/media/mediaItem.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useMemo } from 'react';
-import { optionsMap, GALLERY_CONSTS, isEditMode } from 'pro-gallery-lib';
-import { VideoPlayButton } from './playButton';
-import { Options, Settings, utils } from 'pro-gallery-lib';
-import ImageItem from '../imageItem';
+import React, { useMemo } from "react";
+import { optionsMap, GALLERY_CONSTS, isEditMode } from "pro-gallery-lib";
+import { VideoPlayButton } from "./playButton";
+import { Options, Settings, utils } from "pro-gallery-lib";
+import ImageItem from "../imageItem";
 
 export type MediaBaseProps = {
   calculatedAlt: string;
@@ -32,61 +32,38 @@ export type MediaBaseProps = {
 };
 
 export type MediaProps<T extends Record<string, any>> = MediaBaseProps & {
-  MediaImplementation: React.ComponentType<
-    MediaImplementationProps<Omit<T, 'MediaImplementation'>>
-  >;
+  MediaImplementation: React.ComponentType<MediaImplementationProps<Omit<T, "MediaImplementation">>>;
 } & T;
 
 export type MediaImplementationProps<T = {}> = T &
   MediaBaseProps & {
     thumbnail: JSX.Element;
-    thumbnailWithOverride(props: ImageItem['props']): JSX.Element;
+    thumbnailWithOverride(props: ImageItem["props"]): JSX.Element;
     placeholder: JSX.Element;
   };
 
-export default function MediaItem<T extends Record<string, any>>(
-  props: MediaProps<T>
-): JSX.Element {
-  const {
-    hasLink,
-    options,
-    imageDimensions,
-    showPlayButton,
-    MediaImplementation,
-    enableImagePlaceholder,
-  } = props;
-  const {
-    behaviourParams_item_clickAction: clickAction,
-    behaviourParams_item_video_playTrigger: playTrigger,
-  } = options;
+export default function MediaItem<T extends Record<string, any>>(props: MediaProps<T>): JSX.Element {
+  const { hasLink, options, imageDimensions, showPlayButton, MediaImplementation, enableImagePlaceholder } = props;
+  const { behaviourParams_item_clickAction: clickAction, behaviourParams_item_video_playTrigger: playTrigger } =
+    options;
 
-  const isMediaPlayable = useMemo(() => {
+  const isMediaPlayable = /* useMemo( */ () => {
     if (utils.isSSR()) {
       return false;
     }
 
     if (
-      playTrigger ===
-        GALLERY_CONSTS[optionsMap.behaviourParams.item.video.playTrigger]
-          .HOVER ||
-      playTrigger ===
-        GALLERY_CONSTS[optionsMap.behaviourParams.item.video.playTrigger].AUTO
+      playTrigger === GALLERY_CONSTS[optionsMap.behaviourParams.item.video.playTrigger].HOVER ||
+      playTrigger === GALLERY_CONSTS[optionsMap.behaviourParams.item.video.playTrigger].AUTO
     ) {
       return true;
-    } else if (
-      clickAction ===
-      GALLERY_CONSTS[optionsMap.behaviourParams.item.clickAction].NOTHING
-    ) {
+    } else if (clickAction === GALLERY_CONSTS[optionsMap.behaviourParams.item.clickAction].NOTHING) {
       return true;
-    } else if (
-      clickAction ===
-        GALLERY_CONSTS[optionsMap.behaviourParams.item.clickAction].LINK &&
-      !hasLink
-    ) {
+    } else if (clickAction === GALLERY_CONSTS[optionsMap.behaviourParams.item.clickAction].LINK && !hasLink) {
       return true;
     }
     return false;
-  }, [hasLink, playTrigger, clickAction]);
+  }; /* , [hasLink, playTrigger, clickAction]) */
 
   const createThumbnail = (propsOverrides: any = {}) =>
     enableImagePlaceholder ? (
@@ -95,7 +72,7 @@ export default function MediaItem<T extends Record<string, any>>(
         imageDimensions={imageDimensions}
         id={props.idx}
         overlay={showPlayButton && !isMediaPlayable && <VideoPlayButton />}
-        extraClasses={props.placeholderExtraClasses.join(' ')}
+        extraClasses={props.placeholderExtraClasses.join(" ")}
         {...propsOverrides}
       />
     ) : (
