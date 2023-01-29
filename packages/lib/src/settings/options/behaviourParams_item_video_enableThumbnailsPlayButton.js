69155df70e167888bb1f0099aad8699c716e983d
Yonatan Hattav <yhattav@gmail.com>
2023-01-29 15:48:39 +0200

    with files

---
+++ packages/lib/src/settings/options/behaviourParams_item_video_enableThumbnailsPlayButton.js
@@ -0,0 +1,10 @@
+import { INPUT_TYPES } from '../utils/constants';
+
+export default {
+  title: 'Video Play Button on thumbnails',
+  isRelevant: () => true,
+  isRelevantDescription: 'Always relevant.',
+  type: INPUT_TYPES.BOOLEAN,
+  default: false, //one source
+  description: `Show video play button on thumbnails in a thumbnail gallery`,
+};
