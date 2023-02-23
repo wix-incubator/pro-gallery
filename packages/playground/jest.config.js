6046f4a4a62063f4a8241f0188a791cf5f9b6081
shmuelhizmi <shmuelhizmi1@gmail.com>
2023-02-23 14:10:51 +0200

    fix jest

---
+++ packages/playground/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['<rootDir>/src/**/*.test.(js|jsx|ts|tsx)'],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+    },
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/tsconfig.json',
+        diagnostics: false,
+      },
+    },
+  };
+  
