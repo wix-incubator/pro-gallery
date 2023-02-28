ed42d19b3964e67c783057d58a17ee0d104d63c7
shmuelhizmi <shmuelhizmi1@gmail.com>
2023-02-28 13:28:58 +0200

    fix jest on gallery package

---
+++ packages/gallery/jest.config.js
@@ -0,0 +1,21 @@
+const tsJest = ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }];
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$':
+      '<rootDir>/tests/drivers/mocks/stylesImportMock.js',
+  },
+  reporters: ['default', '<rootDir>/tests/drivers/customReporter.js'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/dist',
+    '<rootDir>/target',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/drivers/mocks/jest-setup.js'],
+  transform: {
+    '^.+\\.tsx?$': tsJest,
+    '^.+\\.jsx?$': tsJest,
+  },
+};
