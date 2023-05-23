8695712656203d4719532a67aadf8c36c57a378e
hadasze <hadasz@wix.com>
2023-05-23 14:53:59 +0300

    adding 'TILT' hover animation option

---
+++ packages/lib/src/settings/options/behaviourParams_item_content_tiltAngleValue.js
@@ -0,0 +1,14 @@
+import { INPUT_TYPES } from '../utils/constants';
+import { default as GALLERY_CONSTS } from '../../common/constants';
+import optionsMap from '../../core/helpers/optionsMap';
+
+export default {
+  title: 'Angle of tilt',
+  description: `Set the angle of tilt`,
+  type: INPUT_TYPES.NUMBER,
+  isRelevant: (styleParams) =>
+    styleParams.itemClick ===
+    GALLERY_CONSTS[optionsMap.behaviourParams.item.content.hoverAnimation].TILT,
+  isRelevantDescription: 'Set the item click action to "TILT".',
+  default: -4,
+};
