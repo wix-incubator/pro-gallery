c85c453445b91c1f93ed9758eb636507e657f51b
Guy Sopher <guyso@wix.com>
2023-06-08 17:07:32 +0300

    fix

--- packages/playground/src/constants/options.test.js
+++ packages/playground/src/constants/options.test.js
@@ -6,8 +6,8 @@ it('should return SP object from url', () => {
     '?layoutParams_structure_galleryLayout=1&behaviourParams_item_overlay_hoveringBehaviour=ALWAYS_SHOW';
   const options = JSON.stringify(getOptionsFromUrl(locationSearch));
   const expected = JSON.stringify({
-    behaviourParams_item_overlay_hoveringBehaviour: 'ALWAYS_SHOW',
     layoutParams_structure_galleryLayout: 1,
+    behaviourParams_item_overlay_hoveringBehaviour: 'ALWAYS_SHOW',
   });
   expect(options).to.equal(expected);
 });
