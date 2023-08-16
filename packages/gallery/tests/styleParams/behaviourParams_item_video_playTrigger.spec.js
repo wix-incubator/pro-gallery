9c464fe643b77bb29202fb04a16fccea0d0e5ec0
IzaacAyelin <izaaca@wix.com>
2023-08-16 16:44:37 +0300

    timer

--- packages/gallery/tests/styleParams/behaviourParams_item_video_playTrigger.spec.js
+++ packages/gallery/tests/styleParams/behaviourParams_item_video_playTrigger.spec.js
@@ -25,7 +25,7 @@ describe('options - behaviourParams_item_video_playTrigger', () => {
         GALLERY_CONSTS[optionsMap.layoutParams.structure.galleryLayout].GRID,
     });
     driver.mount.proGallery(initialProps);
-    await driver.update(200);
+    await driver.update(500);
     const galleryVideoItems = driver.find.hook('video_container-video-player-element');
     expect(galleryVideoItems.length).to.be.greaterThan(0);
     driver.detach.proGallery();
