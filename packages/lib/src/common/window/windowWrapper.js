f32c5375ad6b04d11712ea1bf5c4cd2246e25103
IzaacAyelin <izaaca@wix.com>
2023-08-27 11:03:58 +0300

    partial mock window properties

--- packages/lib/src/common/window/windowWrapper.js
+++ packages/lib/src/common/window/windowWrapper.js
@@ -1,8 +1,15 @@
-import WindowMock from './window.mock';
+import WindowMock, { dimsMock, scrollMock } from './window.mock';
 
 class WindowWrapper {
   constructor() {
-    this.reset();
+    this.initProxyWindow = this.initProxyWindow.bind(this);
+    if (this.windowIsAvailable()) {
+      // this will wrap the real window with partial mock for the dimensions
+      // once the gallery is mounted we will switch from the mocked properties to the real values
+      this.initProxyWindow();
+    } else {
+      this.initMockWindow();
+    }
   }
 
   windowIsAvailable() {
@@ -13,12 +20,42 @@ class WindowWrapper {
     }
   }
 
-  reset() {
-    this.isMock = !this.windowIsAvailable();
-    this.window = this.isMock ? WindowMock : window;
-    if (this.isMock) {
-      this.window.mockInstanceId = Math.floor(Math.random() * 100000);
-    }
+  initProxyWindow() {
+    const handler = {
+      get: function (target, property) {
+        let targetObj = target[property];
+        const dimsAndScrollMock = {
+          ...dimsMock,
+          ...scrollMock,
+        };
+        if (
+          Object.keys(dimsAndScrollMock).includes(property) &&
+          !this.isGalleryMounted
+        ) {
+          return dimsAndScrollMock[property];
+        } else if (typeof targetObj == 'function') {
+          return (...args) => target[property].apply(target, args);
+        } else {
+          return targetObj;
+        }
+      }.bind(this),
+    };
+    // eslint-disable-next-line no-undef
+    this.window = new Proxy(window, handler);
+  }
+  initMockWindow() {
+    this.window = WindowMock;
+    this.window.mockInstanceId = Math.floor(Math.random() * 100000);
+  }
+  initFinalWindow() {
+    this.window = window;
+  }
+
+  get isGalleryMounted() {
+    return this._isGalleryMounted;
+  }
+  set isGalleryMounted(isGalleryMounted) {
+    this._isGalleryMounted = isGalleryMounted;
   }
 }
 
