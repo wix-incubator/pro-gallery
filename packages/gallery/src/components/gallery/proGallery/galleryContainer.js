3730f6b8dd446bd7061109326f71d8917a8eb57d
yhattav <yonatanh@wix.com>
2024-02-07 12:34:59 +0200

    ass prerender to significant props

--- packages/gallery/src/components/gallery/proGallery/galleryContainer.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainer.js
@@ -192,8 +192,9 @@ export class GalleryContainer extends React.Component {
     };
 
     const getSignificantProps = (props) => {
-      const { id, options, container, items, isInDisplay } = props;
-      return { id, options, container, items, isInDisplay };
+      const { id, options, container, items, isInDisplay, isPrerenderMode } =
+        props;
+      return { id, options, container, items, isInDisplay, isPrerenderMode };
     };
 
     if (this.reCreateGalleryTimer) {
