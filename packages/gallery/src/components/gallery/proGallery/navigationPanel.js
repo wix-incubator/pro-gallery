e6ac1a25742c0f76cb1f155a7804bbbbdb49a2fa
Galpittel <galpi@wix.com>
2024-03-14 12:12:01 +0200

    Renaming ENTER_KEY const

--- packages/gallery/src/components/gallery/proGallery/navigationPanel.js
+++ packages/gallery/src/components/gallery/proGallery/navigationPanel.js
@@ -8,6 +8,8 @@ import {
   clearGalleryItems,
 } from '../../helpers/thumbnailsLogic';
 
+const ENTER_KEY = 'Enter';
+
 class NavigationPanel extends React.Component {
   constructor(props) {
     super(props);
@@ -51,7 +53,6 @@ class NavigationPanel extends React.Component {
       activeIndexOffsetMemory: this.activeIndexOffsetMemory,
       prevActiveIndex: this.prevActiveIndex,
     });
-    const enterKey = 'Enter';
 
     this.prevActiveIndex = activeIndex;
     this.activeIndexOffsetMemory = activeIndexOffsetMemory;
@@ -113,7 +114,7 @@ class NavigationPanel extends React.Component {
                 style={itemStyle}
                 onClick={() => this.scrollToThumbnail(idx)}
                 onKeyDown={(e) => {
-                  if (e.key === enterKey) {
+                  if (e.key === ENTER_KEY) {
                     this.scrollToThumbnail(idx);
                   }
                 }}
