2a6f6ced62323c2bd96a3835de3efe916e61d0a4
liatvi <liatvi@wix.com>
2024-04-25 13:50:37 +0300

    trigger hover when without overlay and no action ON MOBILE (#1213)
    * trigger hover when without overlay and no action ON MOBILE
    
    * fix lint

--- packages/gallery/src/components/item/itemView.js
+++ packages/gallery/src/components/item/itemView.js
@@ -73,6 +73,8 @@ class ItemView extends React.Component {
     this.onBlur = this.onBlur.bind(this);
     this.checkIfCurrentHoverChanged =
       this.checkIfCurrentHoverChanged.bind(this);
+    this.shouldHoverWithoutOverlayAndClickOnMobile =
+      this.shouldHoverWithoutOverlayAndClickOnMobile.bind(this);
   }
 
   //----------------------------------------| ACTIONS |-------------------------------------------//
@@ -204,6 +206,11 @@ class ItemView extends React.Component {
       this.shouldShowSecondMediaOnMobile()
     ) {
       setTimeout(this.handleHoverClickOnMobile(e), 0);
+    } else if (this.shouldHoverWithoutOverlayAndClickOnMobile()) {
+      this.props.actions.eventsListener(
+        GALLERY_CONSTS.events.HOVER_SET,
+        this.props.idx
+      );
     } else {
       this.handleGalleryItemAction(e);
     }
@@ -321,6 +328,15 @@ class ItemView extends React.Component {
       }
     }
     return false;
+  }
+
+  shouldHoverWithoutOverlayAndClickOnMobile() {
+    return (
+      utils.isMobile() &&
+      this.props.options.behaviourParams.item.video.playTrigger ===
+        GALLERY_CONSTS.videoPlay.HOVER &&
+      this.props.options.itemClick === GALLERY_CONSTS.itemClick.NOTHING
+    );
   }
 
   isHighlight() {
