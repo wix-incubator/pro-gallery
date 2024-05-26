3a482e7317c4c4962ee19173ae1c41dffcecc0eb
noam-heller1 <121542318+noam-heller1@users.noreply.github.com>
2024-05-26 10:01:42 +0300

    TEST Interaction for mobile (#1239)
    * interaction test
    
    * remove test temporary

--- packages/gallery/src/components/gallery/proGallery/galleryContainer.js
+++ packages/gallery/src/components/gallery/proGallery/galleryContainer.js
@@ -763,6 +763,7 @@ export class GalleryContainer extends React.Component {
         case GALLERY_CONSTS.events.HOVER_SET:
         case GALLERY_CONSTS.events.LOAD_MORE_CLICKED:
         case GALLERY_CONSTS.events.ITEM_ACTION_TRIGGERED:
+        case GALLERY_CONSTS.events.ITEM_CLICKED:
           this.setState({ firstUserInteractionExecuted: true });
           break;
       }
