1dc5f66132091b6c14a72ed693f7ab5cbab9d8b0
IzaacAyelin <izaaca@wix.com>
2025-05-17 23:38:04 +0300

    prevent video iframe auto playing in ssr

--- packages/gallery/src/components/item/videos/IframeVideoPlayer.tsx
+++ packages/gallery/src/components/item/videos/IframeVideoPlayer.tsx
@@ -5,17 +5,29 @@ interface Props {
   dimensions: any;
 }
 const IframePlayer: React.FC<Props> = ({ url, dimensions }) => {
-  return (
-    <iframe
-      data-hook="video-iframe-placeholder"
-      className="gallery-item-content video-iframe-placeholder"
-      title="pro-gallery-video"
-      style={{ ...dimensions }}
-      allow={'fullscreen'}
-      src={url}
-      allowFullScreen
-      loading="lazy"
-    />
-  );
+  const src = url;
+  const commonProps: any = {
+    'data-hook': 'video-iframe-placeholder',
+    className: 'gallery-item-content video-iframe-placeholder',
+    title: 'pro-gallery-video',
+    style: dimensions,
+    allow: 'fullscreen',
+    allowFullScreen: true,
+    loading: 'lazy',
+  };
+  if (url.includes('youtube.com') || url.includes('youtu.be') || url.includes('vimeo.com')) {
+    return <iframe {...commonProps} src={url + '&autoplay=0&muted=1'} />;
+  } else {
+    return (
+      <iframe
+        {...commonProps}
+        srcDoc={`
+      <video controls preload='metadata'>
+      <source src='${src}' type='video/mp4'>
+    </video>
+    `}
+      />
+    );
+  }
 };
 export default IframePlayer;
