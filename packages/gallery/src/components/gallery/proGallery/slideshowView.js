71730668d660a20bc546d0a5eb74cfb2435f982a
vydmantask <vydmantask@wix.com>
2025-06-23 09:10:09 +0300

    Simplify solution

--- packages/gallery/src/components/gallery/proGallery/slideshowView.js
+++ packages/gallery/src/components/gallery/proGallery/slideshowView.js
@@ -89,8 +89,7 @@ class SlideshowView extends React.Component {
     }
     return (
       this.isAllItemsLoaded(props) &&
-      (this.scrollPositionAtTheAndOfTheGallery(props) >= Math.floor(this.getScrollElementWidth(props)) ||
-        this.isLastItemByState())
+      this.scrollPositionAtTheAndOfTheGallery(props) >= Math.floor(this.getScrollElementWidth(props))
     );
   }
 
@@ -112,16 +111,9 @@ class SlideshowView extends React.Component {
     return !this.props.options.behaviourParams_gallery_horizontal_loop && this.isScrollEnd();
   }
 
-  isLastItem(props = this.props) {
-    const activeIndex = props?.activeIndex ?? this.state.activeIndex;
+  isLastItem() {
+    const activeIndex = this.state.activeIndex;
     return !this.props.options.behaviourParams_gallery_horizontal_loop && activeIndex >= this.props.totalItemsCount - 1;
-  }
-
-  isLastItemByState() {
-    return (
-      !this.props.options.behaviourParams_gallery_horizontal_loop &&
-      this.state.activeIndex >= this.props.totalItemsCount - 1
-    );
   }
 
   //__________________________________Slide show loop functions_____________________________________________
