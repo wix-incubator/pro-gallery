9b30f7ee2a1bb88e1c0149951c9775d48302daa8
itaisinai <itaisi@wix.com>
2025-11-17 14:44:04 +0200

    switch to use options

--- packages/lib/src/settings/options/layoutParams_thumbnails_ratio.js
+++ packages/lib/src/settings/options/layoutParams_thumbnails_ratio.js
@@ -5,12 +5,16 @@ export default {
   title: 'Thumbnail Ratio',
   isRelevant: (options) => options[optionsMap.layoutParams.thumbnails.enable],
   isRelevantDescription: 'Enable thumbnails to see this option.',
-  type: INPUT_TYPES.NUMBER,
+  type: INPUT_TYPES.OPTIONS,
   default: 1,
-  min: 0.1,
-  max: 10,
-  step: 0.01,
-  description: `Set the aspect ratio (width/height) of thumbnails. A ratio of 1 creates square thumbnails (1:1). 
-  For example, a ratio of 0.75 creates 3:4 thumbnails (portrait), and 1.333 creates 4:3 thumbnails (landscape).
-  This allows thumbnails to match the main gallery aspect ratio.`,
+  get options() {
+    return [
+      { value: 16 / 9, title: '16:9' },
+      { value: 4 / 3, title: '4:3' },
+      { value: 1, title: '1:1' },
+      { value: 3 / 4, title: '3:4' },
+      { value: 9 / 16, title: '9:16' },
+    ];
+  },
+  description: `Set the aspect ratio (width/height) of thumbnails. This allows thumbnails to match the main gallery aspect ratio.`,
 };
